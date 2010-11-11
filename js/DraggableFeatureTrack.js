0b50fc818d6acc5bfa739722c2375faa985d17d8
Nomi Harris <nlharris@lbl.gov>
2010-11-11 15:03:46 -0800

    Can now grab and drag features to the annotation track to create annotation features without pre-selecting them--just grab and go. This is done by making features draggable on mouseover.  I wanted to make this happen on mousedown (clicking on the feature you want to drag), but that seemed to be too late already, so I had to do it on mouseover.  It only happens on the first mouseover of each feature, so it shouldn't cause much of a slowdown, but if it does turn out to be an issue (e.g. when there are a lot of features loaded) I will revisit it.

--- js/DraggableFeatureTrack.js
+++ js/DraggableFeatureTrack.js
@@ -2,10 +2,70 @@
 function DraggableFeatureTrack(trackMeta, url, refSeq, browserParams) {
     FeatureTrack.call(this, trackMeta, url, refSeq, browserParams);
     thisObj = this;
+    this.dragging = false;
+    this.currentSelection = null;
 }
 
 // Inherit from FeatureTrack
 DraggableFeatureTrack.prototype = new FeatureTrack();
+
+DraggableFeatureTrack.prototype.setDragging = function(onOrOff) {
+//        console.log("setDragging(" + onOrOff + ")");
+        this.dragging = onOrOff;
+}
+
+DraggableFeatureTrack.prototype.isDragging = function() {
+//        console.log("isDragging: " + this.dragging);
+        return this.dragging;
+}
+
+DraggableFeatureTrack.prototype.setCurrentSelection = function(selected) {
+        console.log("setCurrentSelection " + selected); // DEL
+        this.currentSelection = selected;
+}
+
+DraggableFeatureTrack.prototype.getCurrentSelection = function() {
+        return this.currentSelection;
+}
+
+/* I would think that you could add the draggable to the feature at mousedown, but it seems to be already too late--
+ * had to add at mouseover.
+ */
+DraggableFeatureTrack.prototype.onMouseDown = function(event) {
+    var elem = (event.currentTarget || event.srcElement);
+    if (!elem.feature) elem = elem.parentElement;
+    var track = thisObj;
+        console.log("DFT.onMouseDown: started dragging " + elem.feature); // DEL
+        DraggableFeatureTrack.prototype.setCurrentSelection(elem.feature);
+        // This only works the SECOND time you try to drag a feature.
+//	event.stopPropagation();
+//	track.makeDraggableAndDroppable(elem);
+        DraggableFeatureTrack.prototype.setDragging(true);
+}
+
+DraggableFeatureTrack.prototype.onMouseOver = function(event) {
+    if (DraggableFeatureTrack.prototype.isDragging()) {
+//            console.log("DFT.onMouseOver: already dragging"); // DEL
+            return;
+    }
+
+    event = event || window.event;
+    var track = thisObj;
+    if (event.shiftKey) return;
+    var elem = (event.currentTarget || event.srcElement);
+    // !! What if we don't do this?
+    if (!elem.feature) elem = elem.parentElement;
+    if (!elem.feature) return; //shouldn't happen; just bail if it does
+//    console.log("DFT: mouseover on draggablefeature " + elem.feature + ", elem.className = " + elem.className + ", event = " + event);  // DEL
+        // Make this feature draggable & droppable
+	track.makeDraggableAndDroppable(elem);
+}
+
+DraggableFeatureTrack.prototype.onMouseUp = function(event) {
+        DraggableFeatureTrack.prototype.setDragging(false);
+//    console.log("DFT.onMouseup: stopped dragging");  // DEL
+//        this.currentSelection = null;
+}
 
 DraggableFeatureTrack.prototype.onFeatureClick = function(event) {
     var track = thisObj;
@@ -17,11 +77,12 @@ DraggableFeatureTrack.prototype.onFeatureClick = function(event) {
     if (!elem.feature) elem = elem.parentElement;
     if (!elem.feature) return; //shouldn't happen; just bail if it does
 
+    // For debugging, but also to let a click on a feature potentially bring up other info (e.g. a web page)
     var fields = thisObj.fields;
 //    console.log("DFT.onFeatureClick: fields = " + fields + ", fields[end] = " + fields["end"]);
 
     var feat = elem.feature;
-    console.log("DFT: user clicked on draggablefeature\nstart: " + feat[fields["start"]] +
+    console.log("DFT: user clicked on draggablefeature " + elem.feature +"\nstart: " + feat[fields["start"]] +
 	  ", end: " + feat[fields["end"]] +
 	  ", strand: " + feat[fields["strand"]] +
 	  ", label: " + feat[fields["name"]] +
@@ -33,26 +94,30 @@ DraggableFeatureTrack.prototype.onFeatureClick = function(event) {
 
 /* If elem is selected, deselect it.  Otherwise, select it. 
  * (Right now, selection is fake--"selected" elements get a red border and become draggable, but
- * are not added to a selection list.)
+ * are not added to a selection list.  Should add to or remove from currentSelection.)
  */
 DraggableFeatureTrack.prototype.toggleSelection = function(elem) {
     if (elem.style.border == "") {  // !! What if it had a border set by its style?
 	elem.style.border = "3px solid red";
-	// Make it draggable
-	this.makeDraggable(elem);
-        this.makeDroppable(elem);
+//	// Make it draggable  // Now happens elsewhere
+//	this.makeDraggable(elem);
+//        this.makeDroppable(elem);
     }
     // !! Else, need to take it off the "selected" list
     else 
 	elem.style.border = "";
-
-//    console.log("toggleSelection: now elem.style.border = " + elem.style.border);
 }
 
 // Make this DraggableFeatureTrack draggable
-DraggableFeatureTrack.prototype.makeDraggable = function(elem) {
+DraggableFeatureTrack.prototype.makeDraggableAndDroppable = function(elem) {
+        // Check whether we've already done it--look at class name and see if it includes "draggable-feature"
+        if (DraggableFeatureTrack.prototype.hasString(elem.className, "draggable-feature")) {
+//                console.log("makeDraggable: feature = " + elem.feature + ", className = " + elem.className + "--already has draggable-feature");
+                return;
+        }
+
     dojo.addClass(elem, "draggable-feature");
-//    console.log("makeDraggable: elem = " + elem + ", elem.className = " + elem.className + ", elem.id = " + elem.id);  // DEL
+    console.log("makeDraggable: feature = " + elem.feature + ", className = " + elem.className);
     $(".draggable-feature").draggable({
 	    helper:'clone',
 		//	    containment: 'gridtrack',  // Need containment?  (Don't seem to)
@@ -62,19 +127,19 @@ DraggableFeatureTrack.prototype.makeDraggable = function(elem) {
     });
     // If a selected feature is being dragged, we'll handle the drag here--don't want it to go to the whole-canvas-drag handler.
     $(".draggable-feature").bind("mousedown", function(evt) {
+//        $(".draggable-feature").draggable().trigger(evt); // ??
+//        console.log("makeDraggable: evt.stopPropagation.  elem.feature = " + elem.feature + ", currentSelection = " + DraggableFeatureTrack.prototype.getCurrentSelection());  // DEL
 	evt.stopPropagation();
     });
-}
 
-/* Make dragged feature droppable in Annot row */
-DraggableFeatureTrack.prototype.makeDroppable = function(elem) {
     var fields = this.fields;
-	
     $("#track_Annotations").droppable({
        drop: function(ev, ui) {
-    	var pos = $(ui.helper).offset();  // Need?
+//        console.log("makeDroppable: stopped dragging");  // DEL
+        DraggableFeatureTrack.prototype.setDragging(false);
     	// Clone the dragged feature
     	var newAnnot=$(ui.draggable).clone();
+//        console.log("makeDroppable: feat = " + elem.feature + ", currentSelection = " + DraggableFeatureTrack.prototype.getCurrentSelection());  // DEL
     	// Change its class to the appropriate annot class
     	DraggableFeatureTrack.prototype.setAnnotClassNameForFeature(newAnnot);
 
@@ -83,18 +148,12 @@ DraggableFeatureTrack.prototype.makeDroppable = function(elem) {
     	// Restore border of annot to its default (don't want selection border)
     	newAnnot.css({"border": null});
 
-        // This was what we did before actual creation of annotation object on server--don't need anymore
-    	// Find the right block to put the new annot in
-//     	var startField = fields["start"];      	// The field that specifies the start base of this feature
-//  	console.log("Before calling findBlock, startfield = " + fields["start"]);
-//    	var block = DraggableFeatureTrack.prototype.findBlock($(ui.draggable)[0], $(this).children(), startField);
-//    	newAnnot.appendTo(block); 
-
     	var track = this.track;
     	var features = this.track.features;
-    	var feat = elem.feature;
+//    	var feat = elem.feature;
+        var feat = DraggableFeatureTrack.prototype.getCurrentSelection();
     	var responseFeatures;
-
+	
     	dojo.xhrPost( {
 		// "http://10.0.1.24:8080/ApolloWeb/Login?username=foo&password=bar" to login
 	    	postData: '{ "features": [{ "location": { "fmax": ' + feat[fields["end"]] + ', "fmin": ' + feat[fields["start"]] + ', "strand": ' + feat[fields["strand"]] + ' }, "type": { "cv": {"name": "SO"}, "name": "gene" }}], "operation": "add_feature" }',
@@ -109,11 +168,11 @@ DraggableFeatureTrack.prototype.makeDroppable = function(elem) {
 	    	features.add(featureArray, responseFeatures[0].uniquename);
 	    	track.hideAll();
 	    	track.changed();
-	    	console.log("DFT: responseFeatures[0].uniquename = " + responseFeatures[0].uniquename);
+//	    	console.log("DFT: responseFeatures[0].uniquename = " + responseFeatures[0].uniquename);
 	    },
 	    // The ERROR function will be called in an error case.
 	    error: function(response, ioArgs) { // 
-	    	console.log("API didn't work!")
+	    	console.log("Error creating annotation--maybe you forgot to log into the server?");
 	    	console.error("HTTP status code: ", ioArgs.xhr.status); //
 	    	//dojo.byId("replace").innerHTML = 'Loading the ressource from the server did not work'; //  
 	    	return response; // 
@@ -141,27 +200,20 @@ DraggableFeatureTrack.prototype.makeDroppable = function(elem) {
 	     */
 
 //	console.log("itemDragged: " + newAnnot); //  + ", pos.left = " + pos.left + ", pos.top = " + pos.top + ", width = " + ui.draggable.width());
-//	    console.log("newAnnot: " + newAnnot);
-//	    console.log("pos: " + pos);
     }
     });
 }
 
-/* Find and return the block that this feature goes in */
-/* (No longer needed) */
-// DraggableFeatureTrack.prototype.findBlock = function(feature, blocks, startField) {
-//     var feat = feature.feature;
-//     var startBase = feat[startField];
-//     // !! What if it's on minus strand?  (Seems ok)
-//     // !! Can we safely skip the first few blocks, since they're always blank?
-//     for (i = 0; i < blocks.length; i++) {
-// //            console.log("Block " + i + ": end = " + blocks[i].endBase);
-//             if (startBase <= blocks[i].endBase) {
-//                     console.log("Feature with startBase=" + startBase + " goes in block " + i + ", which has endBase=" + blocks[i].endBase);
-//                     return blocks[i];
-//             }
-//     }
-// }
+/* Check whether an array includes the string in question */
+DraggableFeatureTrack.prototype.hasString = function(array, string) {
+        if (array == null)
+                return false;
+        var arr2str = array.toString();
+        if (arr2str.search(string) >= 0)
+                return true;
+        return false;
+}
+
 
 /* Change feature class name to annot class name */
 DraggableFeatureTrack.prototype.setAnnotClassNameForFeature = function(feature) {
