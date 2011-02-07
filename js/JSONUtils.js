e8fd625292bdd1bcfbfff305dd72cbb17fae8ca5
Gregg Helt <gregghelt@gmail.com>
2011-02-06 23:23:31 -0800

    added JSONUtils.createApolloFeature for taking a JBrowse JSON feature and making the corresponding ApolloEditorService JSON feature, including subfeature children (at least for DAS and other non-lazy-loading subfeatures)

--- js/JSONUtils.js
+++ js/JSONUtils.js
@@ -17,3 +17,66 @@ JSONUtils.createJsonFeature = function(fmin, fmax, strand, cv, cvterm) {
 	var feature = { "location": { "fmin": fmin, "fmax": fmax, "strand": strand }, "type": { "cv": {"name": cv }, "name": cvterm }};
 	return feature;
 }
+
+/** 
+*  creates a feature in ApolloEditorService JSON format
+*  takes as argument a feature in JBrowse JSON format, and an array specifying order of fields
+*  ApoloEditorService format:
+*    { 
+*       "location" : { "fmin": fmin, "fmax": fmax, "strand": strand }, 
+*       "type": { "cv": { "name":, cv },   // typical cv name: "SO" (Sequence Ontology)
+*                 "name": cvterm },        // typical name: "transcript"
+*       "children": { __recursive ApolloEditorService feature__ }
+*    }
+*                 
+*    fields example: ["start", "end", "strand", "id", "subfeatures"]
+*
+*    type handling
+*    if specified_type arg present, it determines type name
+*    else if fields has a "type" field, use that to determine type name
+*    else don't include type 
+*
+*    ignoring JBrowse ID / name fields for now
+*    currently, for features with lazy-loaded children, ignores children 
+*/
+JSONUtils.createApolloFeature = function(jfeature, fields, subfields, specified_type)   {
+    var afeature = new Object();
+    afeature.location = { "fmin": jfeature[fields["start"]], "fmax": jfeature[fields["end"]], "strand": jfeature[fields["strand"]] };
+
+    var typename;
+    if (specified_type)  { typename = specified_type; }
+    else if (fields["type"])  { typename = jfeature[fields["type"]]; }
+    if (typename)  {
+	afeature.type = { "cv": { "name": "SO" }};
+	afeature.type.name = typename;
+    }
+    console.log("subfeatures: " + fields["subfeatures"]);
+    if (fields["subfeatures"])  {
+	console.log("found subfeatures field");
+	var subfeats = jfeature[fields["subfeatures"]];
+	console.log(subfeats);
+	// GAH TODO: get this working for lazy-loaded subfeatures 
+	// lazy-loaded subfeatures will only have single-valued index into LazyArray, rather than an actual array
+	//    currently detecting for this and bailing on children if no array found
+	if (subfeats && subfeats.length > 0 && (subfeats[0] instanceof Array))  {
+	    afeature.children = new Array();
+	    //	for (var i=0; i<subfeats.length; i++)  {
+	    for (i in subfeats) {
+		var subfeat = subfeats[i];
+		if (subfields)  {
+		    afeature.children[i] = JSONUtils.createApolloFeature(subfeat, subfields); 
+		}
+		else  {
+		    afeature.children[i] = JSONUtils.createApolloFeature(subfeat, fields, fields); 
+		}
+	    }
+	}
+    }
+    return afeature;
+}
+
+/*
+JSONUtils.createJBrowseFeature = function(apollo_feature, fields)  {
+    
+}
+*/
