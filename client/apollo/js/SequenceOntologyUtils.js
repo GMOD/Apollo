40cdae5bb777eb26de9ac5fb4092d3dd0ad41b47
Gregg Helt <gregghelt@gmail.com>
2013-08-17 20:49:33 -0700

    Added flattened terms for detecting gene features and transcript features.

--- client/apollo/js/SequenceOntologyUtils.js
+++ client/apollo/js/SequenceOntologyUtils.js
@@ -21,7 +21,7 @@ SequenceOntologyUtils.neverHasCDS = {
     protein_hmm_match: true, 
     alignment: true, 
     repeat: true
-}
+};
             
 SequenceOntologyUtils.neverHasExons = {
     match: true, 
@@ -37,7 +37,262 @@ SequenceOntologyUtils.neverHasExons = {
     protein_hmm_match: true, 
     alignment: true, 
     repeat: true
-}
+};
+
+
+/** 
+ *  flattened Sequence Ontology for Gene
+ *     Gene and is-a descendants
+ */
+SequenceOntologyUtils.geneTerms = {
+    gene: true, 
+    gene_cassette: true, 
+    gene_rearranged_at_DNA_level: true, 
+    gene_silenced_by_DNA_methylation: true, 
+    gene_silenced_by_DNA_modification: true, 
+    gene_silenced_by_RNA_interference: true, 
+    gene_silenced_by_histone_deacetylation: true, 
+    gene_silenced_by_histone_methylation: true, 
+    gene_silenced_by_histone_modification: true, 
+    gene_with_dicistronic_mRNA: true, 
+    gene_with_dicistronic_primary_transcript: true, 
+    gene_with_dicistronic_transcript: true, 
+    gene_with_edited_transcript: true, 
+    gene_with_mRNA_recoded_by_translational_bypass: true, 
+    gene_with_mRNA_with_frameshift: true, 
+    gene_with_non_canonical_start_codon: true, 
+    gene_with_polyadenylated_mRNA: true, 
+    gene_with_polycistronic_transcript: true, 
+    gene_with_recoded_mRNA: true, 
+    gene_with_start_codon_CUG: true, 
+    gene_with_stop_codon_read_through: true, 
+    gene_with_stop_codon_redefined_as_pyrrolysine: true, 
+    gene_with_stop_codon_redefined_as_selenocysteine: true, 
+    gene_with_trans_spliced_transcript: true, 
+    gene_with_transcript_with_translational_frameshift: true, 
+    kinetoplast_gene: true, 
+    leucoplast_gene: true, 
+    lincRNA_gene: true, 
+    maternally_imprinted_gene: true, 
+    maxicircle_gene: true, 
+    miRNA_gene: true, 
+    minicircle_gene: true, 
+    mt_gene: true, 
+    ncRNA_gene: true, 
+    negatively_autoregulated_gene: true, 
+    nuclear_gene: true, 
+    nucleomorph_gene: true, 
+    paternally_imprinted_gene: true, 
+    piRNA_gene: true, 
+    plasmid_gene: true, 
+    plastid_gene: true, 
+    positional_candidate_gene: true, 
+    positively_autoregulated_gene: true, 
+    post_translationally_regulated_gene: true, 
+    predicted_gene: true, 
+    proplastid_gene: true, 
+    protein_coding_gene: true, 
+    proviral_gene: true, 
+    rRNA_gene: true, 
+    recombinationally_inverted_gene: true, 
+    recombinationally_rearranged_gene: true, 
+    recombinationally_rearranged_vertebrate_immune_system_gene: true, 
+    rescue_gene: true, 
+    retrogene: true, 
+    scRNA_gene: true, 
+    silenced_gene: true, 
+    snoRNA_gene: true, 
+    stRNA_gene: true, 
+    tRNA_gene: true, 
+    telomerase_RNA_gene: true, 
+    tmRNA_gene: true, 
+    transgene: true, 
+    translationally_regulated_gene: true, 
+    transposable_element_gene: true, 
+    wild_type_rescue_gene: true
+};
+
+/** 
+ *  flattened Sequence Ontology for Transcript
+ *  Transcript and is-a descendants (including mRNA)
+ */
+SequenceOntologyUtils.transcriptTerms = {
+    ARIA: true, 
+    ARRET: true, 
+    C_D_box_snoRNA: true, 
+    C_D_box_snoRNA_primary_transcript: true, 
+    CsrB_RsmB_RNA: true, 
+    DsrA_RNA: true, 
+    GcvB_RNA: true, 
+    H_ACA_box_snoRNA: true, 
+    H_ACA_box_snoRNA_primary_transcript: true, 
+    MicF_RNA: true, 
+    OxyS_RNA: true, 
+    RNA_6S: true, 
+    RNase_MRP_RNA: true, 
+    RNase_P_RNA: true, 
+    RRE_RNA: true, 
+    RprA_RNA: true, 
+    SRP_RNA: true, 
+    SRP_RNA_primary_transcript: true, 
+    TERRA: true, 
+    U11_snRNA: true, 
+    U12_snRNA: true, 
+    U14_snoRNA: true, 
+    U14_snoRNA_primary_transcript: true, 
+    U1_snRNA: true, 
+    U2_snRNA: true, 
+    U3_snoRNA: true, 
+    U4_snRNA: true, 
+    U4atac_snRNA: true, 
+    U5_snRNA: true, 
+    U6_snRNA: true, 
+    U6atac_snRNA: true, 
+    Y_RNA: true, 
+    aberrant_processed_transcript: true, 
+    alanine_tRNA_primary_transcript: true, 
+    alanyl_tRNA: true, 
+    alternatively_spliced_transcript: true, 
+    anti_ARRET: true, 
+    antisense_RNA: true, 
+    antisense_lncRNA: true, 
+    antisense_primary_transcript: true, 
+    arginine_tRNA_primary_transcript: true, 
+    arginyl_tRNA: true, 
+    asparagine_tRNA_primary_transcript: true, 
+    asparaginyl_tRNA: true, 
+    aspartic_acid_tRNA_primary_transcript: true, 
+    aspartyl_tRNA: true, 
+    capped_mRNA: true, 
+    capped_primary_transcript: true, 
+    class_II_RNA: true, 
+    class_I_RNA: true, 
+    consensus_mRNA: true, 
+    cysteine_tRNA_primary_transcript: true, 
+    cysteinyl_tRNA: true, 
+    dicistronic_mRNA: true, 
+    dicistronic_primary_transcript: true, 
+    dicistronic_transcript: true, 
+    edited_mRNA: true, 
+    edited_transcript: true, 
+    edited_transcript_by_A_to_I_substitution: true, 
+    enhancerRNA: true, 
+    enzymatic_RNA: true, 
+    exemplar_mRNA: true, 
+    glutamic_acid_tRNA_primary_transcript: true, 
+    glutamine_tRNA_primary_transcript: true, 
+    glutaminyl_tRNA: true, 
+    glutamyl_tRNA: true, 
+    glycine_tRNA_primary_transcript: true, 
+    glycyl_tRNA: true, 
+    guide_RNA: true, 
+    histidine_tRNA_primary_transcript: true, 
+    histidyl_tRNA: true, 
+    intronic_lncRNA: true, 
+    isoleucine_tRNA_primary_transcript: true, 
+    isoleucyl_tRNA: true, 
+    large_subunit_rRNA: true, 
+    leucine_tRNA_primary_transcript: true, 
+    leucyl_tRNA: true, 
+    lincRNA: true, 
+    lnc_RNA: true, 
+    lysine_tRNA_primary_transcript: true, 
+    lysyl_tRNA: true, 
+    mRNA: true, 
+    mRNA_recoded_by_codon_redefinition: true, 
+    mRNA_recoded_by_translational_bypass: true, 
+    mRNA_with_frameshift: true, 
+    mRNA_with_minus_1_frameshift: true, 
+    mRNA_with_minus_2_frameshift: true, 
+    mRNA_with_plus_1_frameshift: true, 
+    mRNA_with_plus_2_frameshift: true, 
+    mature_transcript: true, 
+    methionine_tRNA_primary_transcript: true, 
+    methionyl_tRNA: true, 
+    methylation_guide_snoRNA: true, 
+    methylation_guide_snoRNA_primary_transcript: true, 
+    miRNA: true, 
+    miRNA_primary_transcript: true, 
+    mini_exon_donor_RNA: true, 
+    monocistronic_mRNA: true, 
+    monocistronic_primary_transcript: true, 
+    monocistronic_transcript: true, 
+    ncRNA: true, 
+    nc_primary_transcript: true, 
+    phenylalanine_tRNA_primary_transcript: true, 
+    phenylalanyl_tRNA: true, 
+    piRNA: true, 
+    polyadenylated_mRNA: true, 
+    polycistronic_mRNA: true, 
+    polycistronic_primary_transcript: true, 
+    polycistronic_transcript: true, 
+    pre_edited_mRNA: true, 
+    primary_transcript: true, 
+    processed_transcript: true, 
+    proline_tRNA_primary_transcript: true, 
+    prolyl_tRNA: true, 
+    protein_coding_primary_transcript: true, 
+    pseudouridylation_guide_snoRNA: true, 
+    pyrrolysine_tRNA_primary_transcript: true, 
+    pyrrolysyl_tRNA: true, 
+    rRNA: true, 
+    rRNA_16S: true, 
+    rRNA_18S: true, 
+    rRNA_21S: true, 
+    rRNA_23S: true, 
+    rRNA_25S: true, 
+    rRNA_28S: true, 
+    rRNA_5S: true, 
+    rRNA_5_8S: true, 
+    rRNA_cleavage_RNA: true, 
+    rRNA_cleavage_snoRNA_primary_transcript: true, 
+    rRNA_large_subunit_primary_transcript: true, 
+    rRNA_primary_transcript: true, 
+    rRNA_small_subunit_primary_transcript: true, 
+    rasiRNA: true, 
+    recoded_mRNA: true, 
+    regional_centromere_outer_repeat_transcript: true, 
+    ribozyme: true, 
+    scRNA: true, 
+    scRNA_primary_transcript: true, 
+    selenocysteine_tRNA_primary_transcript: true, 
+    selenocysteinyl_tRNA: true, 
+    serine_tRNA_primary_transcript: true, 
+    seryl_tRNA: true, 
+    siRNA: true, 
+    small_regulatory_ncRNA: true, 
+    small_subunit_rRNA: true, 
+    snRNA: true, 
+    snRNA_primary_transcript: true, 
+    snoRNA: true, 
+    snoRNA_primary_transcript: true, 
+    spot_42_RNA: true, 
+    stRNA: true, 
+    stRNA_primary_transcript: true, 
+    tRNA: true, 
+    tRNA_primary_transcript: true, 
+    tasiRNA: true, 
+    tasiRNA_primary_transcript: true, 
+    telomerase_RNA: true, 
+    telomeric_transcript: true, 
+    threonine_tRNA_primary_transcript: true, 
+    threonyl_tRNA: true, 
+    tmRNA: true, 
+    tmRNA_primary_transcript: true, 
+    trans_spliced_mRNA: true, 
+    trans_spliced_transcript: true, 
+    transcript: true, 
+    transcript_bound_by_nucleic_acid: true, 
+    transcript_bound_by_protein: true, 
+    transcript_with_translational_frameshift: true, 
+    tryptophan_tRNA_primary_transcript: true, 
+    tryptophanyl_tRNA: true, 
+    tyrosine_tRNA_primary_transcript: true, 
+    tyrosyl_tRNA: true, 
+    valine_tRNA_primary_transcript: true, 
+    valyl_tRNA: true, 
+    vault_RNA: true
+};
 
 /** 
  *  flattened Sequence Ontology for UTR
@@ -58,7 +313,7 @@ SequenceOntologyUtils.utrTerms = {
     noncoding_region_of_exon: true,  /* part_of exon */
     five_prime_coding_exon_noncoding_region: true,  /* part_of exon */
     three_prime_coding_exon_noncoding_region: true  /* part_of exon */
-}
+};
 
 /**
  *  flattened Sequence Ontology for CDS
@@ -84,7 +339,7 @@ SequenceOntologyUtils.cdsTerms = {
     coding_region_of_exon: true, /* part_of exon */
     five_prime_coding_exon_coding_region: true, /* part_of exon */
     three_prime_coding_exon_coding_region: true /* part_of exon */
-} 
+}; 
 
 /**
  *  flattened Sequence Ontology for exon
@@ -113,16 +368,16 @@ SequenceOntologyUtils.exonTerms = {
         five_prime_coding_exon_noncoding_region: true,
         three_prime_coding_exon_noncoding_region: true 
     */
-}
+};
 
 SequenceOntologyUtils.startCodonTerms = {
     start_codon: true, 
     non_canonical_start_codon: true 
-}
+};
 
 SequenceOntologyUtils.stopCodonTerms = {
     stop_codon: true
-}
+};
 
 /* not yet complete */
 SequenceOntologyUtils.spliceTerms = {
@@ -136,7 +391,7 @@ SequenceOntologyUtils.spliceTerms = {
     non_canonical_five_prime_splice_site: true, 
     non_canonical_three_prime_splice_site: true, 
     non_canonical_splice_site: true 
-}
+};
 
 /* not yet complete? */
 SequenceOntologyUtils.intronTerms = {
@@ -150,7 +405,7 @@ SequenceOntologyUtils.intronTerms = {
     autocatalytically_spliced_intron: true, 
     endonuclease_spliced_intron: true, 
     mobile_intron: true
-}
+};
 
 return SequenceOntologyUtils;
 });
