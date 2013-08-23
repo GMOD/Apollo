6995bde06f0db4e8cd7e169963a5c5eb0f69ec46
Gregg Helt <gregghelt@gmail.com>
2013-08-23 07:42:06 -0700

    Revised DraggableAlignments to improve rendering. Copied and modified _drawMismatches from Alignments superclassk. Should really refactor in Alignments so don't have to do cut-and-paste, or DraggableAlignments changes should get moved over to Alignments. By default rendering of SNVs (mismatch, insertion, deletion) for DraggableAlignments is now handled in _drawMismatches as non-feature divs, rendering of match regions is handled as normal subfeature divs, and skips are not rendered. Also, drawing residues for mismatches is turned off by default, can turn on using config.showMismatchResidues, but will still not draw residues unless read alignments feature div height is tall enough to fit the residues font.

--- client/apollo/js/View/Track/DraggableAlignments.js
+++ client/apollo/js/View/Track/DraggableAlignments.js
@@ -1,11 +1,13 @@
 define([
-           'dojo/_base/declare',
+           'dojo/_base/declare', 
+           'dojo/_base/array',
            'JBrowse/View/Track/Alignments',
            'WebApollo/View/Track/DraggableHTMLFeatures', 
            'JBrowse/Util', 
        ],
        function(
            declare,
+           array, 
            AlignmentsTrack,
            DraggableTrack, 
            Util
@@ -29,23 +31,93 @@ return declare([ DraggableTrack, AlignmentsTrack ], {
                 style: {
                     className: "bam-read", 
                     renderClassName: null, 
-                    arrowheadClass: "arrowhead", 
+                    arrowheadClass: null, 
 	            centerChildrenVertically: false, 
 	            showSubfeatures: true, 
-	            showMismatches: false, 
+	            showMismatches: true, 
 		    showLabels: false, 
+                    showMismatchResidues: false,  // when rendering mismatches, whether to render residues text
                     subfeatureClasses: {
 	                M: "cigarM", 
-		        D: "cigarD",
+//		        D: "cigarD",
+		        D: null, // not rendering deletions as subfeats, relying on drawMismatches instead
 		        N: "cigarN",
 		        E: "cigarEQ",  /* "=" converted to "E" in BAM/LazyFeature subfeature construction */
 		        X: "cigarX", 
-		        I: "cigarI"
+//		        I: "cigarI"
+		        I: null // not rendering insertions as subfeats, relying on drawMismatches instead
                     }
                 }
             }
         );
         return thisConfig;
+    }, 
+
+    /**
+     * draw base-mismatches on the feature
+     */
+    _drawMismatches: function( feature, featDiv, scale, displayStart, displayEnd ) {
+        var featLength = displayEnd - displayStart;
+        // recall: scale is pixels/basepair
+        // if ( featLength*scale > 1 && scale >= 1) {  // alternatively, also require zoomed in scale min (1px/bp  in this case)
+        if ( featLength*scale > 1) {     
+            var mismatches = this._getMismatches( feature );
+            var charSize = this.getCharacterMeasurements();
+            var drawChars = (this.config.style.showMismatchResidues && 
+                             (scale >= charSize.w) && 
+                             (charSize.h <= (this.glyphHeight + this.glyphHeightPad))
+                             );
+            array.forEach( mismatches, function( mismatch ) {
+                var start = feature.get('start') + mismatch.start;
+                // GAH: _MismatchesMixin._getMismatches is creating insertions with length = 1, 
+                //   but length of insertion should really by 0, since JBrowse internally uses zero-interbase coordinates
+                //   fixing here for now, since changing in _MismatchesMixin could have unexpected consequences
+                // if (mismatch.type == 'insertion') { mismatch.length = 0; }
+                var end = start + mismatch.length;
+
+                // if the feature has been truncated to where it doesn't cover
+                // this mismatch anymore, just skip this mismatch
+                if ( end <= displayStart || start >= displayEnd )
+                    return;
+
+                var base = mismatch.base;
+                var mDisplayStart = Math.max( start, displayStart );
+                var mDisplayEnd = Math.min( end, displayEnd );
+                var mDisplayWidth = mDisplayEnd - mDisplayStart;
+                var overall = dojo.create('span',  {
+                    className: 'align_'+mismatch.type + ' ' + mismatch.type + ' base_'+base.toLowerCase(),
+                    style: {
+                        position: 'absolute',
+                        left: 100 * ( mDisplayStart - displayStart)/featLength + '%',
+                        width: scale*mDisplayWidth>1 ? 100 * mDisplayWidth/featLength + '%' : '1px'
+                    }
+                }, featDiv );
+                overall.mismatch = mismatch;
+
+                // give the mismatch a mouseover if not drawing a character with the mismatch base
+                if( ! drawChars ) {
+                    if (mismatch.type == 'deletion') { overall.title = mismatch.length; }
+                    else  { overall.title = base; }
+                }
+
+                if( drawChars && mismatch.length <= 20 ) {
+                    for( var i = 0; i<mismatch.length; i++ ) {
+                        var basePosition = start + i;
+                        if( basePosition >= mDisplayStart && basePosition <= mDisplayEnd ) {
+                            dojo.create('span',{
+                                            className: 'base base_'+base.toLowerCase(),
+                                            style: {
+                                                position: 'absolute',
+                                                width: scale+'px',
+                                                left: (basePosition-mDisplayStart)/mDisplayWidth*100 + '%'
+                                            },
+                                            innerHTML: base
+                                        }, overall );
+                        }
+                    }
+                }
+            }, this );
+        }
     }
 
 } );
