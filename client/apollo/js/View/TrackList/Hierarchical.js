6bb7f608cde0494ed1d2bd4d328164d0d309b012
Colin <colin.diesh@gmail.com>
2014-02-10 11:51:59 -0600

    Complete the modified hierarchical track selector to remove the 'Annotations' and 'DNA' tracks

--- client/apollo/js/View/TrackList/Hierarchical.js
+++ client/apollo/js/View/TrackList/Hierarchical.js
@@ -5,9 +5,15 @@ define([
 function(declare,Hierarchical) {
 	return declare('WebApollo.View.TrackList.Hierarchical',Hierarchical,
 	{
+		// Subclass method for track selector to remove webapollo specific tracks
 		addTracks: function( tracks, inStartup ) {
-			console.log(tracks);
-			alert('Hello World');
+			for(i=0;i<tracks.length;i++) {
+				if(tracks[i].label=="Annotations" || tracks[i].label=="DNA") {
+					tracks.splice(i,1);
+					i=0;//restart for safety
+				}
+			}
+			//call superclass
 			this.inherited(arguments);
 		}
 	});
