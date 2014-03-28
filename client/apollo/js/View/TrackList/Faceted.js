2cc62dd8e90a3e6f1f999430e4655edf75138c78
Colin <colin.diesh@gmail.com>
2014-03-28 16:04:00 -0500

    Fix crazy whitespace in Faceted tracklist mod

--- client/apollo/js/View/TrackList/Faceted.js
+++ client/apollo/js/View/TrackList/Faceted.js
@@ -7,14 +7,14 @@ function(declare,Faceted) {
 	{
 		// Subclass method for track selector to remove webapollo specific tracks
 		constructor: function(args) {
-            //Remove the tracks from the metadata descriptions
-            for(var index in args.trackMetaData.identIndex) {
-                if(args.trackMetaData.identIndex[index]["track type"]=="WebApollo/View/Track/AnnotTrack" || 
-                   args.trackMetaData.identIndex[index]["track type"]=="WebApollo/View/Track/AnnotSequenceTrack") {
-                    delete args.trackMetaData.identIndex[index];
-                    args.trackMetaData.facetIndexes.itemCount--;
-                }   
-            }
+			//Remove the tracks from the metadata descriptions
+			for(var index in args.trackMetaData.identIndex) {
+				if(args.trackMetaData.identIndex[index]["track type"]=="WebApollo/View/Track/AnnotTrack" || 
+				   args.trackMetaData.identIndex[index]["track type"]=="WebApollo/View/Track/AnnotSequenceTrack") {
+					delete args.trackMetaData.identIndex[index];
+					args.trackMetaData.facetIndexes.itemCount--;
+				}
+			}
 		}
 	});
 });
