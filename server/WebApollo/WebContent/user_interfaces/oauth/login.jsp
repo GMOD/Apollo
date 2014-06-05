b972dbfb907b604f91b9ff567389d5329e74f9c4
Stephen Ficklin <spficklin@gmail.com>
2014-06-05 14:18:09 -0700

    Added support for OAuth authentication

---
+++ server/WebApollo/WebContent/user_interfaces/oauth/login.jsp
@@ -0,0 +1,66 @@
+<%@ page import="javax.xml.parsers.DocumentBuilder" %>
+<%@ page import="javax.xml.parsers.DocumentBuilderFactory" %>
+<%@ page import="javax.xml.parsers.ParserConfigurationException" %>
+<%@ page import="java.io.FileInputStream" %>
+<%@ page import="org.w3c.dom.Document" %>
+<%@ page import="org.w3c.dom.Node" %>
+<% 
+
+String Client_ID = null;
+String Client_Secret = null;
+String Auth_URL = null;
+String Uname_Field = null;
+
+// get the oauth settings from the configuration file 
+String config_path = getServletContext().getRealPath("/") + "config/oauth.xml";
+
+FileInputStream fstream = new FileInputStream(config_path);
+DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+DocumentBuilder db = dbf.newDocumentBuilder();
+Document doc = db.parse(fstream);
+Client_ID = doc.getElementsByTagName("client_id").item(0).getFirstChild().getNodeValue();
+Client_Secret = doc.getElementsByTagName("client_secret").item(0).getFirstChild().getNodeValue();
+Auth_URL = doc.getElementsByTagName("auth_url").item(0).getFirstChild().getNodeValue();
+Uname_Field = doc.getElementsByTagName("uname_field").item(0).getFirstChild().getNodeValue();
+%> 
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Login</title>
+<script>
+  var context;
+  $(document).ready(function() {
+    var pathname = location.pathname;
+    context = /^\/([^\/]+)\//.exec(pathname)[1];
+    $("#login_button").click(function() {
+      
+      login();
+    });
+    $("#sign_in_image").attr("src", "/" + context + "/images/sign_in_green.png");
+  });
+  
+  function login() {
+    var auth_url = "<%=Auth_URL%>?" +
+       "scope=<%=Uname_Field%>&" +
+       "response_type=code&" +
+       "redirect_uri=" + encodeURIComponent("http://localhost:8080/WebApollo/Login?operation=login&forceRedirect=true") + "&" +
+       "client_id=<%=Client_ID%>";
+    window.location = auth_url;
+  };
+  
+  function setMessage(message) {
+      $("#message").text(message);
+  };
+  
+</script>
+</head>
+<body>
+<div>
+<button id="login_button"><img id="sign_in_image" /></button>
+<div id="message">
+</div>
+</div>
+</body>
+</html>
---
+++ server/WebApollo/src/org/bbop/apollo/web/user/oauth/OauthUserAuthentication.java
@@ -0,0 +1,156 @@
+package org.bbop.apollo.web.user.oauth;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.bbop.apollo.web.user.UserAuthenticationException;
+import org.bbop.apollo.web.user.UserAuthentication;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.xml.sax.SAXException;
+
+import java.util.Arrays;
+
+import com.google.api.client.auth.oauth2.AuthorizationCodeFlow;
+import com.google.api.client.auth.oauth2.BearerToken;
+import com.google.api.client.auth.oauth2.ClientParametersAuthentication;
+import com.google.api.client.auth.oauth2.TokenResponse;
+import com.google.api.client.auth.openidconnect.IdToken;
+import com.google.api.client.http.GenericUrl;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson.JacksonFactory;
+
+public class OauthUserAuthentication implements UserAuthentication {
+	
+	private String Client_ID = null;
+	private String Client_Secret = null;
+	private String Auth_URL = null;
+	private String Token_URL = null;
+	private String Uname_Field = null;
+	
+	public OauthUserAuthentication() {
+		try {
+			URL resource = getClass().getResource("/");
+			String config_path = resource.getPath() + "/../../config/oauth.xml";
+			
+			FileInputStream fstream = new FileInputStream(config_path);
+			DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+			DocumentBuilder db = dbf.newDocumentBuilder();
+			Document doc = db.parse(fstream);
+			Node client_id_node = doc.getElementsByTagName("client_id").item(0);
+			if (client_id_node != null) {
+				Client_ID = client_id_node.getTextContent();
+			}
+			Node client_secret_node = doc.getElementsByTagName("client_secret").item(0);
+			if (client_secret_node != null) {
+				Client_Secret = client_secret_node.getTextContent();
+			}
+			Node auth_url_node = doc.getElementsByTagName("auth_url").item(0);
+			if (auth_url_node != null) {
+				Auth_URL = auth_url_node.getTextContent();
+			}
+			Node token_url_node = doc.getElementsByTagName("token_url").item(0);
+			if (token_url_node != null) {
+				Token_URL = token_url_node.getTextContent();
+			}
+			Node auth_uname_field = doc.getElementsByTagName("uname_field").item(0);
+			if (auth_uname_field != null) {
+				Uname_Field = auth_uname_field.getTextContent();
+			}
+		} 
+		catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		//Node hibernateConfigNode = doc.getElementsByTagName("hibernate_config").item(0);
+		catch (ParserConfigurationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+		catch (SAXException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+		catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void generateUserLoginPage(HttpServlet servlet, HttpServletRequest request,
+			HttpServletResponse response) throws ServletException {
+		InputStream in = servlet.getServletContext().getResourceAsStream("/user_interfaces/opauth/login.html");
+		BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+		String line;
+		try {
+			while ((line = reader.readLine()) != null) {
+				response.getOutputStream().println(line);
+			}
+			in.close();
+		} catch (IOException e) {
+			throw new ServletException(e);
+		}
+	}
+
+	@Override
+	public String validateUser(HttpServletRequest request,
+			HttpServletResponse response) throws UserAuthenticationException {
+		
+		String username = null; 
+		
+		String redirect_uri = "http://localhost:8080/WebApollo/Login?operation=login&forceRedirect=true";
+
+		
+		// get the authorization code
+		String code = request.getParameter("code");
+		
+		HttpTransport http_transport = new NetHttpTransport();
+		JsonFactory json_factory = new JacksonFactory();
+
+	    AuthorizationCodeFlow codeFlow = new AuthorizationCodeFlow.Builder(
+	        BearerToken.authorizationHeaderAccessMethod(),
+	        http_transport,
+	        json_factory,
+	        new GenericUrl(Token_URL),
+	        new ClientParametersAuthentication(Client_ID, Client_Secret), Client_ID, Token_URL).setScopes(Arrays.asList("email")).build();
+	    try {
+			TokenResponse token_response = codeFlow.newTokenRequest(code)
+				.setRedirectUri(redirect_uri).setScopes(Arrays.asList(Uname_Field)).execute();
+			IdToken id_token = IdToken.parse(json_factory, token_response.get("id_token").toString());
+			username = id_token.getPayload().get(Uname_Field).toString();
+		} 
+	    catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	    return username;
+
+	}
+
+	@Override
+	public String getUserLoginPageURL() {
+		return "user_interfaces/oauth/login.jsp";
+	}
+
+	@Override
+	public String getAddUserURL() {
+		return "user_interfaces/oauth/addUser.jsp";
+	}
+
+}
