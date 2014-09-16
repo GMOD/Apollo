f76d8684ebf2d4d93e9938b81a02d5d43141de1d
Nathan Dunn <ndunn@me.com>
2014-09-16 08:59:31 -0700

    moved config.xml to sample_config.xml

--- build.sh
+++ build.sh
@@ -23,6 +23,13 @@ else
   echo "jbrowse installed"
 fi
 
+if [ -e "$APOLLO_ROOT_DIRECTORY/config.xml" ]; then
+    # will either do a force copy
+    cp  $APOLLO_ROOT_DIRECTORY/config.xml $APOLLO_WEBAPP_DIRECTORY/config/config.xml
+else
+   echo "No config.xml found, not copying."
+fi
+
 if [ -e "$APOLLO_ROOT_DIRECTORY/config.properties" ]; then
     # will either do a force copy
     cp  $APOLLO_ROOT_DIRECTORY/config.properties $APOLLO_WEBAPP_DIRECTORY/config/config.properties
