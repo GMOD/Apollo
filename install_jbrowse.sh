76724b1b9a1e7f99c36bc31a867a8640c1c98c8f
Nathan Dunn <ndunn@me.com>
2014-09-17 16:01:00 -0700

    added jbrowse install script

--- install_jbrowse.sh
+++ install_jbrowse.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #https://github.com/cmdcolin/jbrowse/blob/master/.travis.yml
 APOLLO_ROOT_DIRECTORY=`pwd`
-APOLLO_WEBAPP_DIRECTORY="$APOLLO_ROOT_DIRECTORY/src/main/webapp"
+APOLLO_WEBAPP_DIRECTORY="$APOLLO_ROOT_DIRECTORY/web-app"
 APOLLO_JBROWSE_DIRECTORY="$APOLLO_WEBAPP_DIRECTORY/jbrowse"
 JBROWSE_GITHUB="https://github.com/GMOD/jbrowse"
 if [ ! -d "$APOLLO_JBROWSE_DIRECTORY" ]; then
@@ -23,9 +23,16 @@ else
   echo "jbrowse installed"
 fi
 
-if [ -e "$APOLLO_ROOT_DIRECTORY/config.properties" ]; then
-    # will either do a force copy
-    cp  $APOLLO_ROOT_DIRECTORY/config.properties $APOLLO_WEBAPP_DIRECTORY/config/config.properties
-else
-   echo "No config.properties found, not copying."
-fi
+#if [ -e "$APOLLO_ROOT_DIRECTORY/config.xml" ]; then
+#    # will either do a force copy
+#    cp  $APOLLO_ROOT_DIRECTORY/config.xml $APOLLO_WEBAPP_DIRECTORY/config/config.xml
+#else
+#   echo "No config.xml found, not copying."
+#fi
+
+#if [ -e "$APOLLO_ROOT_DIRECTORY/config.properties" ]; then
+#    # will either do a force copy
+#    cp  $APOLLO_ROOT_DIRECTORY/config.properties $APOLLO_WEBAPP_DIRECTORY/config/config.properties
+#else
+#   echo "No config.properties found, not copying."
+#fi
