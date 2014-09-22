b31ba4ab3bbe0fe33abb4c5d30d56fbf3d8b72b8
Nathan Dunn <ndunn@me.com>
2014-09-22 15:58:29 -0700

    updated build script

--- build.sh
+++ build.sh
@@ -5,35 +5,39 @@ APOLLO_BUILD_DIRECTORY=$APOLLO_ROOT_DIRECTORY
 APOLLO_WEBAPP_DIRECTORY="$APOLLO_ROOT_DIRECTORY/src/main/webapp"
 APOLLO_JBROWSE_DIRECTORY="$APOLLO_WEBAPP_DIRECTORY/jbrowse"
 JBROWSE_GITHUB="https://github.com/GMOD/jbrowse"
+
 if [ ! -d "$APOLLO_BUILD_DIRECTORY/jbrowse-github" ]; then
   echo "No jbrowse directory found at $APOLLO_JBROWSE_DIRECTORY, installing locally from $JBROWSE_GITHUB"
   cd $APOLLO_BUILD_DIRECTORY
   git clone --recursive $JBROWSE_GITHUB jbrowse-github
+
+  cd $APOLLO_BUILD_DIRECTORY/jbrowse-github
+  cp -r $APOLLO_ROOT_DIRECTORY/client/apollo plugins/WebApollo
+  rm -rf $APOLLO_JBROWSE_DIRECTORY
+  if [[ $1 == release ]]; then
+      echo "Using release jbrowse"
+      ulimit -n 1000
+      make -f build/Makefile release
+      mv JBrowse-dev $APOLLO_JBROWSE_DIRECTORY
+  elif [[ $1 == debug ]]; then
+      ulimit -n 1000
+      make -f build/Makefile release
+      echo "Using debug jbrowse"
+      mv JBrowse-dev-dev $APOLLO_JBROWSE_DIRECTORY
+  else
+      cp -R .  $APOLLO_JBROWSE_DIRECTORY
+  fi
+
+  cd $APOLLO_JBROWSE_DIRECTORY
+  ./setup.sh
+  cd $APOLLO_ROOT_DIRECTORY
+
 else 
   echo "jbrowse installed"
 fi
 
 
-cd $APOLLO_BUILD_DIRECTORY/jbrowse-github
-cp -r $APOLLO_ROOT_DIRECTORY/client/apollo plugins/WebApollo
-rm -rf $APOLLO_JBROWSE_DIRECTORY
-if [[ $1 == release ]]; then
-    echo "Using release jbrowse"
-	ulimit -n 1000
-	make -f build/Makefile release
-    mv JBrowse-dev $APOLLO_JBROWSE_DIRECTORY
-elif [[ $1 == debug ]]; then
-	ulimit -n 1000
-	make -f build/Makefile release
-    echo "Using debug jbrowse"
-    mv JBrowse-dev-dev $APOLLO_JBROWSE_DIRECTORY
-else
-    cp -R .  $APOLLO_JBROWSE_DIRECTORY
-fi
 
-cd $APOLLO_JBROWSE_DIRECTORY
-./setup.sh
-cd $APOLLO_ROOT_DIRECTORY
 
 if [ -e "$APOLLO_ROOT_DIRECTORY/config.xml" ]; then
     # will either do a force copy
