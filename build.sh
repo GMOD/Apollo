a692d7c768b5e0f8d155c3f16f1f8c8f6146aadf
Nathan Dunn <ndunn@me.com>
2014-09-30 13:54:51 -0700

    configured blat

--- build.sh
+++ build.sh
@@ -68,6 +68,11 @@ fi
 
 
 
+if [ -e "$APOLLO_ROOT_DIRECTORY/blat_config.xml" ]; then
+    cp  $APOLLO_ROOT_DIRECTORY/blat_config.xml $APOLLO_WEBAPP_DIRECTORY/config/blat_config.xml
+else
+   echo "No blat_config.xml found, not copying."
+fi
 
 if [ -e "$APOLLO_ROOT_DIRECTORY/config.xml" ]; then
     cp  $APOLLO_ROOT_DIRECTORY/config.xml $APOLLO_WEBAPP_DIRECTORY/config/config.xml
