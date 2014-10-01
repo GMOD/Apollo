88bf98ac112a101e5ad2bea57c120715cb9c8bb2
Colin <colin.diesh@gmail.com>
2014-10-01 11:00:17 -0500

    Avoid some rare cases where git clean doesn't work correctly

--- build.sh
+++ build.sh
@@ -40,7 +40,10 @@ fi
 
 if [ ! -d "$APOLLO_JBROWSE_DIRECTORY" ]; then
   cd "$APOLLO_JBROWSE_GITHUB"
+  rm -rf JBrowse-$JBROWSE_VERSION-dev
+  rm -rf JBrowse-$JBROWSE_VERSION
   git checkout -- release-notes.txt
+  git checkout -- src/JBrowse/Browser.js
   git fetch
   git checkout $JBROWSE_RELEASE
   cp -r $APOLLO_ROOT_DIRECTORY/client/apollo plugins/WebApollo
@@ -68,11 +71,6 @@ fi
 
 
 
-if [ -e "$APOLLO_ROOT_DIRECTORY/blat_config.xml" ]; then
-    cp  $APOLLO_ROOT_DIRECTORY/blat_config.xml $APOLLO_WEBAPP_DIRECTORY/config/blat_config.xml
-else
-   echo "No blat_config.xml found, not copying."
-fi
 
 if [ -e "$APOLLO_ROOT_DIRECTORY/config.xml" ]; then
     cp  $APOLLO_ROOT_DIRECTORY/config.xml $APOLLO_WEBAPP_DIRECTORY/config/config.xml
