d3ad0921ec003bd8c74e74725822d989d59c0763
Nathan Dunn <ndunn@me.com>
2014-10-02 14:23:55 -0700

    fixed scripts to work with maven

--- tools/data/update_sequence_alterations.sh
+++ tools/data/update_sequence_alterations.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 
-webapollo_dir=$WEBAPOLLO_DIR
-
 function print_usage {
 	cat << END
 usage: $(basename $0)
 	-i <input_database> || -d <annotation_directory>
-	-w <webapollo_installation_dir> -H
-	[-h]
-
 	i: input JE database to update
 	d: directory containing JE databases to update (will process every
 	   database in the directory - overrides "-i" option
@@ -24,26 +19,18 @@ function update {
 	if [ $update_history ]; then
 		args="$args -H"
 	fi
-	java -cp "$(dirname $0)/classes.jar:$webapollo_dir/WEB-INF/classes:$webapollo_dir/WEB-INF/lib/*" org.bbop.apollo.web.tools.UpdateSequenceAlterations $args
+    mvn -q exec:java -Dexec.mainClass="org.bbop.apollo.web.tools.UpdateSequenceAlterations" -Dexec.args="$args"
 }
 
 while getopts "i:d:w:Hh" opt; do
 	case $opt in
 		i) input_db=$OPTARG;;
 		d) annotation_dir=$OPTARG;;
-		w) webapollo_dir=$OPTARG;;
 		H) update_history=1;;
 		h) print_usage;;
 	esac
 done
 
-if [ -z "$webapollo_dir" ]; then
-	echo "Missing -w <webapollo_installation_dir> or \$WEBAPOLLO_DIR environment variable"
-	exit 1
-elif [ ! -d "$webapollo_dir" ]; then
-	echo "Cannot access webapollo installation directory: $webapollo_dir"
-	exit 1
-fi
 
 echo "Remember to shut down the WebApollo instance before running this tool!"
 
