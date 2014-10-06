714b2686885382cd087db47039bf38ce26a64853
Colin <colin.diesh@gmail.com>
2014-10-06 16:18:18 -0500

    Allow all tracks mode when no -T tracks_file is specified

--- tools/data/export_annotations_to_gff3.pl
+++ tools/data/export_annotations_to_gff3.pl
@@ -9,6 +9,8 @@ use Getopt::Long qw(:config no_ignore_case bundling);
 use IO::File;
 use LWP::UserAgent;
 use JSON;
+use URI::Escape;
+
 
 my $annotation_track_prefix = "Annotations-";
 my $username;
@@ -44,7 +46,6 @@ sub parse_options {
     die "Missing required parameter: username\n" if !$username;
     die "Missing required parameter: password\n" if !$password;
     die "Missing required parameter: url\n" if !$url;
-    die "Missing required parameter: tracks_file\n" if !$tracks_file;
     $url = "http://$url" if $url !~ m%http://%;
 }
 
@@ -59,7 +60,7 @@ usage: $progname
     --username|-u <username>
     --password|-p <password>
     [--track_prefix|-P <annotation track prefix>]
-    --tracks_file|-T <tracks file>
+    [--tracks_file|-T <tracks file>]
 
     U: URL to WebApollo instance
     u: username to access WebApollo
@@ -67,6 +68,7 @@ usage: $progname
     P: annotation track prefix
        [default: "$annotation_track_prefix"]
     T: tracks file
+       [default: "all_tracks"]
     o: output file
 END
 }
@@ -74,11 +76,24 @@ END
 
 
 sub parse_tracks_file {
-   open my $fh, '<', $tracks_file or die $!;
-   while (my $line=<$fh>) {
-       chomp($line);
-       push(@tracks,$line);
+   if($tracks_file) {
+       open my $fh, '<', $tracks_file or die $!;
+       while (my $line=<$fh>) {
+           chomp($line);
+           push(@tracks,$line);
+       }
    }
+}
+
+
+sub escape_hash {
+    
+    my %hash =@_;
+    my @pairs;
+    for my $key (keys %hash) {
+        push @pairs, join "=", map { uri_escape($_) } $key, $hash{$key};
+    }
+    return join "&", @pairs;
 }
 
 
@@ -121,20 +136,33 @@ sub logout {
 
 sub send_gff3_request {
     my @arr;
-    foreach my $i (@tracks) {
-        push(@arr,$annotation_track_prefix.$i);
+    if($tracks_file) {
+        foreach my $i (@tracks) {
+            push(@arr,$annotation_track_prefix.$i);
+        }
     }
-    my $request = {
-            tracks => \@arr,
+    my %requesthash = (
+            tracks => $tracks_file?\@arr:"all_tracks",
             adapter => "GFF3",
             operation => "write",
             options => "output=display&format=text"
-    };
+    );
+    my $request=\%requesthash;
+
     my $req = new HTTP::Request();
-    $req->method("POST");
-    $req->uri("$url/IOService;jsessionid=$session_id");
-    $req->content($json->encode($request));
+    if($tracks_file) {
+        $req->method("POST");
+        $req->uri("$url/IOService;jsessionid=$session_id");
+        $req->content($json->encode($request));
+    }
+    else {
+        my $urlrequest="$url/IOService;jsessionid=$session_id?".escape_hash(%requesthash);
+        print $urlrequest."\n";
+        $req->method("GET");
+        $req->uri($urlrequest);
+    }
     my $res = $ua->request($req);
+    print $res->content();
     if ($res->is_success()) {
         my $fh;
         if($output) {
