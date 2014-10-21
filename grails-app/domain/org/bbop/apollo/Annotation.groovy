1bb25b30bef798683231fe204cfa5174eec562fe
Nathan Dunn <ndunn@me.com>
2014-10-21 10:27:38 -0700

    minor changes

--- grails-app/domain/org/bbop/apollo/Annotation.groovy
+++ grails-app/domain/org/bbop/apollo/Annotation.groovy
@@ -1,5 +1,8 @@
 package org.bbop.apollo
 
+/**
+ * I think this should be a Feature . . . and go away?
+ */
 class Annotation {
 
     static constraints = {
--- grails-app/domain/org/bbop/apollo/FeatureRelationship.groovy
+++ grails-app/domain/org/bbop/apollo/FeatureRelationship.groovy
@@ -30,18 +30,18 @@ class FeatureRelationship {
         if ( !(other instanceof FeatureRelationship) ) return false;
         FeatureRelationship castOther = ( FeatureRelationship ) other;
 
-        return ( (this.getType()==castOther.getType()) || ( this.getType()!=null && castOther.getType()!=null && this.getType().equals(castOther.getType()) ) ) && ( (this.getObjectFeature()==castOther.getObjectFeature()) || ( this.getObjectFeature()!=null && castOther.getObjectFeature()!=null && this.getObjectFeature().equals(castOther.getObjectFeature()) ) ) && ( (this.getSubjectFeature()==castOther.getSubjectFeature()) || ( this.getSubjectFeature()!=null && castOther.getSubjectFeature()!=null && this.getSubjectFeature().equals(castOther.getSubjectFeature()) ) ) && (this.getRank()==castOther.getRank());
+        return ( (this.type==castOther.type) || ( this.type!=null && castOther.type!=null && this.type.equals(castOther.type) ) ) && ( (this.getObjectFeature()==castOther.getObjectFeature()) || ( this.getObjectFeature()!=null && castOther.getObjectFeature()!=null && this.getObjectFeature().equals(castOther.getObjectFeature()) ) ) && ( (this.getSubjectFeature()==castOther.getSubjectFeature()) || ( this.getSubjectFeature()!=null && castOther.getSubjectFeature()!=null && this.getSubjectFeature().equals(castOther.getSubjectFeature()) ) ) && (this.getRank()==castOther.getRank());
     }
 
     public int hashCode() {
         int result = 17;
 
 
-        result = 37 * result + ( getType() == null ? 0 : this.getType().hashCode() );
-        result = 37 * result + ( getObjectFeature() == null ? 0 : this.getObjectFeature().hashCode() );
-        result = 37 * result + ( getSubjectFeature() == null ? 0 : this.getSubjectFeature().hashCode() );
+        result = 37 * result + ( type == null ? 0 : this.type.hashCode() );
+        result = 37 * result + ( objectFeature == null ? 0 : this.objectFeature.hashCode() );
+        result = 37 * result + ( subjectFeature == null ? 0 : this.subjectFeature.hashCode() );
 
-        result = 37 * result + this.getRank();
+        result = 37 * result + this.rank;
 
 
         return result;
