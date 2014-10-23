23f7f2dd37a6b10b5687a8ea0a2ae4d3974cc01c
Nathan Dunn <ndunn@me.com>
2014-10-23 12:01:43 -0700

    began to add tests

--- test/unit/org/bbop/apollo/AnnotationEditorControllerSpec.groovy
+++ test/unit/org/bbop/apollo/AnnotationEditorControllerSpec.groovy
@@ -1,5 +1,6 @@
 package org.bbop.apollo
 
+import grails.test.mixin.Mock
 import grails.test.mixin.TestFor
 import spock.lang.Specification
 
@@ -7,6 +8,7 @@ import spock.lang.Specification
  * See the API for {@link grails.test.mixin.web.ControllerUnitTestMixin} for usage instructions
  */
 @TestFor(AnnotationEditorController)
+@Mock([Sequence,Genome,Feature])
 class AnnotationEditorControllerSpec extends Specification {
 
     def setup() {
@@ -15,6 +17,16 @@ class AnnotationEditorControllerSpec extends Specification {
     def cleanup() {
     }
 
-    void "test something"() {
+    void "get_features"() {
+
+        when: "converts properly"
+        Sequence sequence = new Sequence(name: "chromosome7").save()
+        params.operation="get_features"
+        params.track="chromosome7"
+        def jsonReturn = controller.handleOperation(params)
+        then:
+        println jsonReturn
+        assert jsonReturn != null
+
     }
 }
