e07514fae05f0f1be0850fd6fe3819b4fdcd9fe4
Nathan Dunn <ndunn@me.com>
2014-11-07 13:08:29 -0800

    added test back as ignore

--- test/unit/org/bbop/apollo/AnnotationEditorControllerSpec.groovy
+++ test/unit/org/bbop/apollo/AnnotationEditorControllerSpec.groovy
@@ -94,65 +94,65 @@ class AnnotationEditorControllerSpec extends Specification {
     }
 
     // TODO: move to integration tests
+    @Ignore
+    void "add_transcript"() {
 
-//    void "add_transcript"() {
-//
-//        when: "we send JSON towards our controller to add a transcript"
-//        Organism organism = new Organism(
-//                genus: "Danio"
-//                ,species: "rerio"
-//                ,abbreviation: "ZF"
-//                ,commonName: "Zebrafish"
-//        ).save(failOnError: true)
-//        Sequence sequence = new Sequence(name:  "Annotations-Group1.1",organism: organism).save(failOnError: true)
-//        JSONBuilder builder = new JSONBuilder()
-////        JSON json = builder.build
-//        String jsonString = ('{ "track": "Annotations-Group1.1", "features": [{"location":{"fmin":88364,"fmax":88550,"strand":-1},"type":{"cv":{"name":"sequence"},"name":"mRNA"},"name":"geneid_mRNA_CM000054.5_3","children":[{"location":{"fmin":88364,"fmax":88550,"strand":-1},"type":{"cv":{"name":"sequence"},"name":"exon"}}]}], "operation": "add_transcript" }' )
-//        JSONElement rootJsonObject = JSON.parse(jsonString)
-//        println rootJsonObject
-//
-////        def map = {
-////            track: "Annotations-Group1.1"
-////            features:
-////            [{
-////                 location {
-////                     fmin 88364
-////                     fmax 88550
-////                     strand - 1
-////                 }
-////                 type {
-////                     cv { name "sequence" }
-////                     name "mRNA"
-////                 }
-////                 name "geneid_mRNA_CM000054.5_3"
-////                 children [
-////                         {
-////                             location {
-////                         fmin 88364
-////                         fmax 88550
-////                         strand - 1
-////                         }
-////                     type {
-////                         cv {
-////                             name "sequence"
-////                         }
-////                         name "exon"
-////                     }
-////                 }]
-////             }]
-////            operation "add_transcript"
-////        }
-//
-////        JSON jsonObject = map as JSON
-////        println "MAP: " + jsonObject
-//
-//        params.data = jsonString
-//
-//        controller.addTranscript()
-//
-//        then: "It should add a transcript"
-//        assert true
-//
-//    }
+        when: "we send JSON towards our controller to add a transcript"
+        Organism organism = new Organism(
+                genus: "Danio"
+                ,species: "rerio"
+                ,abbreviation: "ZF"
+                ,commonName: "Zebrafish"
+        ).save(failOnError: true)
+        Sequence sequence = new Sequence(name:  "Annotations-Group1.1",organism: organism).save(failOnError: true)
+        JSONBuilder builder = new JSONBuilder()
+//        JSON json = builder.build
+        String jsonString = ('{ "track": "Annotations-Group1.1", "features": [{"location":{"fmin":88364,"fmax":88550,"strand":-1},"type":{"cv":{"name":"sequence"},"name":"mRNA"},"name":"geneid_mRNA_CM000054.5_3","children":[{"location":{"fmin":88364,"fmax":88550,"strand":-1},"type":{"cv":{"name":"sequence"},"name":"exon"}}]}], "operation": "add_transcript" }' )
+        JSONElement rootJsonObject = JSON.parse(jsonString)
+        println rootJsonObject
+
+//        def map = {
+//            track: "Annotations-Group1.1"
+//            features:
+//            [{
+//                 location {
+//                     fmin 88364
+//                     fmax 88550
+//                     strand - 1
+//                 }
+//                 type {
+//                     cv { name "sequence" }
+//                     name "mRNA"
+//                 }
+//                 name "geneid_mRNA_CM000054.5_3"
+//                 children [
+//                         {
+//                             location {
+//                         fmin 88364
+//                         fmax 88550
+//                         strand - 1
+//                         }
+//                     type {
+//                         cv {
+//                             name "sequence"
+//                         }
+//                         name "exon"
+//                     }
+//                 }]
+//             }]
+//            operation "add_transcript"
+//        }
+
+//        JSON jsonObject = map as JSON
+//        println "MAP: " + jsonObject
+
+        params.data = jsonString
+
+        controller.addTranscript()
+
+        then: "It should add a transcript"
+        assert true
+
+    }
 
 }
