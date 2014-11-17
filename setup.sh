0be8d5b3da3266a73535c34a614db4b265533131
Nathan Dunn <ndunn@me.com>
2014-11-17 14:29:20 -0800

    reverted rest of a43773f6882f8932afee11138758398060f73c97 build

--- setup.sh
+++ setup.sh
@@ -17,13 +17,11 @@ if ! ( perl -MExtUtils::MakeMaker -e 1 >/dev/null 2>&1); then
     echo "WARNING: Your Perl installation does not seem to include a complete set of core modules.  Attempting to cope with this, but if installation fails please make sure that at least ExtUtils::MakeMaker is installed.  For most users, the best way to do this is to use your system's package manager: apt, yum, fink, homebrew, or similar.";
 fi;
 ( set -x;
+  bin/cpanm -v --notest -l extlib DBI DBD::Pg Crypt::PBKDF2 LWP::UserAgent JSON < /dev/null;
   cd src/main/webapp/jbrowse;
-  bin/cpanm -v --notest -l ../../../../extlib DBI DBD::Pg Crypt::PBKDF2 LWP::UserAgent JSON < /dev/null;
-  bin/cpanm -v --notest -l ../../../../extlib DBI DBD::Pg Crypt::PBKDF2 LWP::UserAgent JSON < /dev/null;
   bin/cpanm -v --notest -l ../../../../extlib/ --installdeps .< /dev/null;
   bin/cpanm -v --notest -l ../../../../extlib/ --installdeps .< /dev/null;
   set -e;
-  bin/cpanm -v --notest -l ../../../../extlib DBI DBD::Pg Crypt::PBKDF2 LWP::UserAgent JSON < /dev/null;
   bin/cpanm -v --notest -l ../../../../extlib/ --installdeps .< /dev/null;
   cd -;
   cp -r src/main/webapp/jbrowse/bin/ bin;
