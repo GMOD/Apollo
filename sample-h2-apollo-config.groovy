3be89bbb6dc575e148a5083a05dd1045ad31dd43
Nathan Dunn <ndunn@icebox.lbl.gov>
2014-12-11 14:23:43 -0800

    updated sample

--- sample-h2-apollo-config.groovy
+++ sample-h2-apollo-config.groovy
@@ -1,6 +1,10 @@
 grails {
-    dataSource.username = "web_apollo_user"
-    dataSource.password = "web_apollo_user"
-    dataSource.driverClassName = "org.h2.Driver"
-    dataSource.url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
+//    dataSource.username = "web_apollo_user"
+//    dataSource.password = "web_apollo_user"
+//    dataSource.driverClassName = "org.h2.Driver"
+//    dataSource.url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
+
+	apollo.datastore.directory = "/opt/apollo/annotations"
+	apollo.jbrowse.data.directory="/opt/jbrowse/data"
+
 }
