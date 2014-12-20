1226b16e895e0770ea9286ea9cd0858d6e65ecb7
Nathan Dunn <ndunn@me.com>
2014-12-19 16:20:28 -0800

    showing the bar when closed

--- src/gwt/org/bbop/apollo/gwt/client/MainPanel.java
+++ src/gwt/org/bbop/apollo/gwt/client/MainPanel.java
@@ -1,5 +1,7 @@
 package org.bbop.apollo.gwt.client;
 
+import com.google.gwt.canvas.client.Canvas;
+import com.google.gwt.canvas.dom.client.Context2d;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.logical.shared.SelectionEvent;
@@ -52,6 +54,8 @@ public class MainPanel extends Composite {
     ListBox organismList;
     @UiField
     ListBox sequenceList;
+    @UiField
+    FlowPanel westPanel;
 
     public MainPanel() {
         initWidget(ourUiBinder.createAndBindUi(this));
@@ -62,8 +66,24 @@ public class MainPanel extends Composite {
         rootUrl = dictionary.get("rootUrl");
         frame.setUrl(rootUrl + "/jbrowse/?loc=Group1.3%3A14865..15198&tracks=DNA%2CAnnotations%2COfficial%20Gene%20Set%20v3.2%2CGeneID%2CCflo_OGSv3.3&highlight=");
 
+//        westPanel.setVisible(true);
+
         DataGenerator.populateOrganismList(organismList);
         DataGenerator.populateSequenceList(sequenceList);
+
+//        Canvas canvas = Canvas.createIfSupported();
+////        canvas.setHeight("300px");
+//        westPanel.add(canvas);
+//        Context2d context2d = canvas.getContext2d();
+//        context2d.beginPath();
+////        context2d.moveTo(20,180);
+////        context2d.rotate(-Math.PI/2.0);
+//
+//        context2d.rotate(-Math.PI / 5.0);
+//        context2d.setTextAlign("right");
+//        context2d.fillRect(40,40, 20, 20);
+//        context2d.fillText("adfas",10,20);
+//        context2d.closePath();
     }
 
 
@@ -104,11 +124,25 @@ public class MainPanel extends Composite {
         }
 
         if (toggleOpen) {
+            // close
             mainSplitPanel.setWidgetSize(eastDockPanel, 20);
             dockOpenClose.setIcon(IconType.CARET_LEFT);
+//            detailTabs.setVisible(false);
+//            westPanel.setVisible(true);
+//            westPanel.setWidth("20px");
+//            eastDockPanel.addWest(westPanel,20);
+//            westPanel.setPixelSize(20,1000);
+//            eastDockPanel.forceLayout();
         } else {
+            // open
             mainSplitPanel.setWidgetSize(eastDockPanel, 500);
             dockOpenClose.setIcon(IconType.CARET_RIGHT);
+//            detailTabs.setVisible(true);
+//            westPanel.setVisible(false);
+//            westPanel.setPixelSize(0,1000);
+//            eastDockPanel.remove(westPanel);
+//            westPanel.setWidth("1px");
+//            eastDockPanel.forceLayout();
         }
 
         mainSplitPanel.animate(400);
