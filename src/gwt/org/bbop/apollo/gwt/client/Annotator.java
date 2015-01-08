394fa52ef49b53c1404f93fb50c0c0564e47ab47
Nathan Dunn <ndunn@me.com>
2015-01-07 17:12:03 -0800

    added some new files to get the websockets to work

--- src/gwt/org/bbop/apollo/gwt/client/Annotator.java
+++ src/gwt/org/bbop/apollo/gwt/client/Annotator.java
@@ -4,6 +4,10 @@ import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.dom.client.Style;
 import com.google.gwt.i18n.client.Dictionary;
 import com.google.gwt.user.client.ui.*;
+//import org.realityforge.gwt.websockets.client.WebSocket;
+//import org.realityforge.gwt.websockets.client.WebSocketListenerAdapter;
+
+import javax.annotation.Nonnull;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
@@ -23,6 +27,32 @@ public class Annotator implements EntryPoint {
         RootLayoutPanel rp = RootLayoutPanel.get();
         rp.add(mainPanel);
         rp.setWidgetTopHeight(mainPanel, 0, Style.Unit.PX, 100, Style.Unit.PCT);
+
+//        final WebSocket webSocket = WebSocket.newWebSocketIfSupported();
+//        if ( null != webSocket )
+//        {
+//            webSocket.setListener( new WebSocketListenerAdapter()
+//            {
+//                @Override
+//                public void onOpen( @Nonnull final WebSocket webSocket )
+//                {
+//                    // After we have connected we can send
+//                    webSocket.send( "Hello!" );
+//                }
+//
+//                @Override
+//                public void onMessage( @Nonnull final WebSocket webSocket, @Nonnull final String data )
+//                {
+//                    // After we receive a message back we can close the socket
+//                    webSocket.close();
+//                }
+//            } );
+//            webSocket.connect( "ws://"+rootUrl + "/ws/" );
+//        }
+
+
+
+
 
 
         // Focus the cursor on the name field when the app loads
