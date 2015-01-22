045766d25a7a2491ae0b25fe50dc072cdec87d9d
Nathan Dunn <ndunn@me.com>
2015-01-22 10:20:37 -0800

    added eventbus to annotator

--- src/gwt/org/bbop/apollo/gwt/client/Annotator.java
+++ src/gwt/org/bbop/apollo/gwt/client/Annotator.java
@@ -7,10 +7,14 @@ import com.google.gwt.event.shared.EventBus;
 import com.google.gwt.event.shared.SimpleEventBus;
 import com.google.gwt.i18n.client.Dictionary;
 import com.google.gwt.user.client.ui.*;
+import org.bbop.apollo.gwt.client.dto.OrganismInfo;
+import org.bbop.apollo.gwt.client.event.OrganismChangeEvent;
+import org.bbop.apollo.gwt.client.event.OrganismChangeEventHandler;
 //import org.realityforge.gwt.websockets.client.WebSocket;
 //import org.realityforge.gwt.websockets.client.WebSocketListenerAdapter;
 
 import javax.annotation.Nonnull;
+import java.util.List;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
@@ -32,76 +36,13 @@ public class Annotator implements EntryPoint {
         rp.add(mainPanel);
         rp.setWidgetTopHeight(mainPanel, 0, Style.Unit.PX, 100, Style.Unit.PCT);
 
-//        final WebSocket webSocket = WebSocket.newWebSocketIfSupported();
-//        if ( null != webSocket )
-//        {
-//            webSocket.setListener( new WebSocketListenerAdapter()
-//            {
-//                @Override
-//                public void onOpen( @Nonnull final WebSocket webSocket )
-//                {
-//                    // After we have connected we can send
-//                    webSocket.send( "Hello!" );
-//                }
-//
-//                @Override
-//                public void onMessage( @Nonnull final WebSocket webSocket, @Nonnull final String data )
-//                {
-//                    // After we receive a message back we can close the socket
-//                    webSocket.close();
-//                }
-//            } );
-//            webSocket.connect( "ws://"+rootUrl + "/ws/" );
-//        }
 
-
-
-
-
-
-        // Focus the cursor on the name field when the app loads
-//        nameField.setFocus(true);
-//        nameField.selectAll();
-//
-//        nameField.addChangeHandler(new ChangeHandler() {
-//
-//            @Override
-//            public void onChange(ChangeEvent event) {
-//                String url = "/apollo/annotator/search";
-//                RequestBuilder builder = new RequestBuilder(RequestBuilder.POST, URL.encode(url));
-//                JSONObject jsonObject = new JSONObject();
-//                jsonObject.put("query", new JSONString("pax6a"));
-//                builder.setRequestData("data=" + jsonObject.toString());
-//                builder.setHeader("Content-type", "application/x-www-form-urlencoded");
-//                RequestCallback requestCallback = new RequestCallback() {
-//                    @Override
-//                    public void onResponseReceived(Request request, Response response) {
-//                        JSONValue returnValue = JSONParser.parseStrict(response.getText());
-//                        JSONObject jsonObject = returnValue.isObject();
-//                        String queryString = jsonObject.get("query").isString().stringValue();
-//
-//                        // TODO: use proper array parsing
-//                        String resultString = jsonObject.get("result").isString().stringValue();
-//                        resultString = resultString.replace("[", "");
-//                        resultString = resultString.replace("]", "");
-//                        searchResult.setText(" asdflkj asdflkjdas fsearch for " + queryString + " yields [" + resultString + "]");
-//                    }
-//
-//                    @Override
-//                    public void onError(Request request, Throwable exception) {
-//                        Window.alert("ow");
-//                    }
-//                };
-//                try {
-//                    builder.setCallback(requestCallback);
-//                    builder.send();
-//                } catch (RequestException e) {
-//                    // Couldn't connect to server
-//                    Window.alert(e.getMessage());
-//                }
-//
-//            }
-//        });
+        eventBus.addHandler(OrganismChangeEvent.TYPE, new OrganismChangeEventHandler() {
+            @Override
+            public void onOrganismChanged(OrganismChangeEvent organismChangeEvent) {
+//                List<OrganismInfo> organismInfoList = organismChangeEvent.getOrganismInfoList();
+            }
+        });
 
     }
 
