a4cfa08a843bce86a05c5b736b719ca69e0a0a28
Colin <colin.diesh@gmail.com>
2015-02-12 01:28:35 -0600

    Allow getting the JBrowse directory from multiple places for install_jbrowse.sh

--- install_jbrowse.sh
+++ install_jbrowse.sh
@@ -17,7 +17,16 @@ if ! ( perl -MExtUtils::MakeMaker -e 1 >/dev/null 2>&1); then
     echo "WARNING: Your Perl installation does not seem to include a complete set of core modules.  Attempting to cope with this, but if installation fails please make sure that at least ExtUtils::MakeMaker is installed.  For most users, the best way to do this is to use your system's package manager: apt, yum, fink, homebrew, or similar.";
 fi;
 ( set -x;
-  cd src/jbrowse;
+  dir=""
+  if [ -d "$1" ]; then
+    dir=$1;
+  elif [ -d src/jbrowse ]; then
+   dir=src/jbrowse;
+  elif [ -d src/main/webapp/jbrowse ]; then
+   dir=src/main/webapp/jbrowse;
+  fi;
+  echo $dir
+  cd $dir
   chmod +x bin/cpanm
   bin/cpanm -v --notest -l ../../extlib DBI DBD::Pg Crypt::PBKDF2 LWP::UserAgent JSON Mozilla::CA < /dev/null;
   bin/cpanm -v --notest -l ../../extlib DBI DBD::Pg Crypt::PBKDF2 LWP::UserAgent JSON Mozilla::CA < /dev/null;
