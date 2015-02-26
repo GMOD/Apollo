8c959862fb6e0089216ce9b9b3d2d44413d319cb
Nathan Dunn <ndunn@me.com>
2015-02-25 22:50:26 -0800

    added new service spec

---
+++ test/integration/org/bbop/apollo/FeatureServiceIntegrationSpec.groovy
@@ -0,0 +1,52 @@
+package org.bbop.apollo
+
+import grails.converters.JSON
+import grails.test.spock.IntegrationSpec
+import org.codehaus.groovy.grails.web.json.JSONArray
+import org.codehaus.groovy.grails.web.json.JSONObject
+
+class FeatureServiceIntegrationSpec extends IntegrationSpec {
+    
+    def featureService
+
+    def setup() {
+        Sequence sequence = new Sequence(
+                length: 3
+                ,refSeqFile: "adsf"
+                ,seqChunkPrefix: "asdf"
+                ,seqChunkSize: 3
+                ,start: 5
+                ,end: 8
+                ,sequenceDirectory: "asdfadsf"
+                ,name: "Group-1.10"
+        ).save(failOnError: true)
+    }
+
+    def cleanup() {
+    }
+
+    void "convert JSON to Features"(){
+
+        given: "a set string and existing sequence, when we have a complicated mRNA as JSON"
+        String jsonString = "{ \"track\": \"Annotations-Group1.10\", \"features\": [{\"location\":{\"fmin\":1216824,\"fmax\":1235616,\"strand\":1},\"type\":{\"cv\":{\"name\":\"sequence\"},\"name\":\"mRNA\"},\"name\":\"GB40856-RA\",\"children\":[{\"location\":{\"fmin\":1235534,\"fmax\":1235616,\"strand\":1},\"type\":{\"cv\":{\"name\":\"sequence\"},\"name\":\"exon\"}},{\"location\":{\"fmin\":1216824,\"fmax\":1216850,\"strand\":1},\"type\":{\"cv\":{\"name\":\"sequence\"},\"name\":\"exon\"}},{\"location\":{\"fmin\":1224676,\"fmax\":1224823,\"strand\":1},\"type\":{\"cv\":{\"name\":\"sequence\"},\"name\":\"exon\"}},{\"location\":{\"fmin\":1228682,\"fmax\":1228825,\"strand\":1},\"type\":{\"cv\":{\"name\":\"sequence\"},\"name\":\"exon\"}},{\"location\":{\"fmin\":1235237,\"fmax\":1235396,\"strand\":1},\"type\":{\"cv\":{\"name\":\"sequence\"},\"name\":\"exon\"}},{\"location\":{\"fmin\":1235487,\"fmax\":1235616,\"strand\":1},\"type\":{\"cv\":{\"name\":\"sequence\"},\"name\":\"exon\"}},{\"location\":{\"fmin\":1216824,\"fmax\":1235534,\"strand\":1},\"type\":{\"cv\":{\"name\":\"sequence\"},\"name\":\"CDS\"}}]}], \"operation\": \"add_transcript\" }"
+
+        when: "we parse it"
+        JSONObject jsonObject = JSON.parse(jsonString) as JSONObject
+
+        then: "is is a valid object"
+        assert jsonObject!=null
+        JSONArray jsonArray = jsonObject.getJSONArray(FeatureStringEnum.FEATURES.value)
+        assert jsonArray.size()==1
+        JSONObject mRNAJsonObject = jsonArray.getJSONObject(0)
+        JSONArray childArray = jsonArray.getJSONObject(0).getJSONArray(FeatureStringEnum.CHILDREN.value)
+        assert childArray.size()==7
+
+        when: "we convert it to a feature"
+        Feature feature = service.convertJSONToFeature(mRNAJsonObject,Sequence.first())
+
+        then: "it should convert it to the same feature"
+        assert feature!=null
+        feature.ontologyId == MRNA.ontologyId
+
+    }
+}
--- test/unit/org/bbop/apollo/FeatureServiceSpec.groovy
+++ test/unit/org/bbop/apollo/FeatureServiceSpec.groovy
@@ -4,7 +4,6 @@ import grails.converters.JSON
 import grails.test.mixin.Mock
 import grails.test.mixin.TestFor
 import org.bbop.apollo.sequence.Strand
-import org.codehaus.groovy.grails.web.json.JSONArray
 import org.codehaus.groovy.grails.web.json.JSONObject
 import spock.lang.Specification
 
@@ -51,38 +50,4 @@ class FeatureServiceSpec extends Specification {
         assert ontologyId == Exon.ontologyId
     }
     
-    void "convert JSON to Features"(){
-        
-        given: "a set string and existing sequence, when we have a complicated mRNA as JSON"
-        String jsonString = "{ \"track\": \"Annotations-Group1.10\", \"features\": [{\"location\":{\"fmin\":1216824,\"fmax\":1235616,\"strand\":1},\"type\":{\"cv\":{\"name\":\"sequence\"},\"name\":\"mRNA\"},\"name\":\"GB40856-RA\",\"children\":[{\"location\":{\"fmin\":1235534,\"fmax\":1235616,\"strand\":1},\"type\":{\"cv\":{\"name\":\"sequence\"},\"name\":\"exon\"}},{\"location\":{\"fmin\":1216824,\"fmax\":1216850,\"strand\":1},\"type\":{\"cv\":{\"name\":\"sequence\"},\"name\":\"exon\"}},{\"location\":{\"fmin\":1224676,\"fmax\":1224823,\"strand\":1},\"type\":{\"cv\":{\"name\":\"sequence\"},\"name\":\"exon\"}},{\"location\":{\"fmin\":1228682,\"fmax\":1228825,\"strand\":1},\"type\":{\"cv\":{\"name\":\"sequence\"},\"name\":\"exon\"}},{\"location\":{\"fmin\":1235237,\"fmax\":1235396,\"strand\":1},\"type\":{\"cv\":{\"name\":\"sequence\"},\"name\":\"exon\"}},{\"location\":{\"fmin\":1235487,\"fmax\":1235616,\"strand\":1},\"type\":{\"cv\":{\"name\":\"sequence\"},\"name\":\"exon\"}},{\"location\":{\"fmin\":1216824,\"fmax\":1235534,\"strand\":1},\"type\":{\"cv\":{\"name\":\"sequence\"},\"name\":\"CDS\"}}]}], \"operation\": \"add_transcript\" }"
-        Sequence sequence = new Sequence(
-                length: 3
-                ,refSeqFile: "adsf"
-                ,seqChunkPrefix: "asdf"
-                ,seqChunkSize: 3
-                ,start: 5
-                ,end: 8
-                ,sequenceDirectory: "asdfadsf"
-                ,name: "Group-1.10"
-        ).save(failOnError: true)
-
-        when: "we parse it"
-        JSONObject jsonObject = JSON.parse(jsonString) as JSONObject
-        
-        then: "is is a valid object"
-        assert jsonObject!=null 
-        JSONArray jsonArray = jsonObject.getJSONArray(FeatureStringEnum.FEATURES.value)
-        assert jsonArray.size()==1
-        JSONObject mRNAJsonObject = jsonArray.getJSONObject(0)
-        JSONArray childArray = jsonArray.getJSONObject(0).getJSONArray(FeatureStringEnum.CHILDREN.value)
-        assert childArray.size()==7
-
-        when: "we convert it to a feature"
-        Feature feature = service.convertJSONToFeature(mRNAJsonObject,sequence)
-       
-        then: "it should convert it to the same feature"
-        assert feature!=null
-        feature.ontologyId == MRNA.ontologyId
-        
-    }
 }
