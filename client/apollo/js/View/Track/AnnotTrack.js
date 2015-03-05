c86905b495bcfe9071f330f9e1fb0796d6538fb3
Colin <colin.diesh@gmail.com>
2015-03-05 16:15:34 -0600

    Allow features without any subfeatures to be added to the User-created annotations track. See #81

--- client/apollo/js/View/Track/AnnotTrack.js
+++ client/apollo/js/View/Track/AnnotTrack.js
@@ -1075,13 +1075,14 @@ var AnnotTrack = declare( DraggableFeatureTrack,
             }
             else {
                 var children = dragfeat.get("subfeatures");
-				if(!children) {
-                    alert("This element cannot be annotated as a gene. Please choose a different type of annotation");
-                    return;
+                if(children) {
+                    for (var j = 0; j < children.length; ++j) {
+                        subfeatures.push(children[j]);
+                    }
                 }
-                for (var j = 0; j < children.length; ++j) {
-                    subfeatures.push(children[j]);
-                 }
+                else {
+                    subfeatures.push(dragfeat);
+                }
                 if (!parentFeature) {
                     parentFeature = dragfeat;
                 }
