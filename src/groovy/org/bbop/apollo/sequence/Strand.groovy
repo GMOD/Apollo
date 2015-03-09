8c09bb1746ff5e7ab982fdf6cb80e5a5fb4fd350
Nathan Dunn <ndunn@me.com>
2015-03-09 09:36:55 -0700

    updated strand object

--- src/groovy/org/bbop/apollo/sequence/Strand.groovy
+++ src/groovy/org/bbop/apollo/sequence/Strand.groovy
@@ -5,16 +5,22 @@ package org.bbop.apollo.sequence
  */
 enum Strand {
 
-    POSITIVE(1)
-    , NEGATIVE(-1)
+    POSITIVE(1,"+")
+    , NEGATIVE(-1,"-")
 
     Integer value
+    String display
 
-    public Strand(Integer value) {
+    public Strand(Integer value,String display) {
         this.value = value
+        this.display = display
     }
 
     public getValue() {
         return this.value
+    }
+
+    String getDisplay() {
+        return display
     }
 }
