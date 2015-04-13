2ef80c033cd437eb95a790efbe34cc2a56ecf19b
Nathan Dunn <ndunn@me.com>
2015-04-13 13:47:47 -0700

    cleaned up config

--- sample-mysql-apollo-config.groovy
+++ sample-mysql-apollo-config.groovy
@@ -3,5 +3,4 @@ grails {
     dataSource.password = "<CHANGEME>"
     dataSource.driverClassName = "com.mysql.jdbc.Driver"
     dataSource.dialect = "org.hibernate.dialect.MySQL5InnoDBDialect"
-    dataSource.url = "jdbc:mysql://localhost:3306/apollo"
 }
