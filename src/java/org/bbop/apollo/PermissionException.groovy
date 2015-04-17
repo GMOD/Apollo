4099cf7feab85b9c7e433c4b6e3fbd06515bc377
Nathan Dunn <ndunn@me.com>
2015-04-17 08:02:07 -0700

    permission exception

---
+++ src/java/org/bbop/apollo/PermissionException.groovy
@@ -0,0 +1,10 @@
+package org.bbop.apollo
+
+/**
+ * Created by ndunn on 10/28/14.
+ */
+class PermissionException extends Exception{
+    def PermissionException(String gString) {
+        super(gString)
+    }
+}
