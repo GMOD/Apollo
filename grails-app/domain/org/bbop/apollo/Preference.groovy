78499cfca4f520a9fce30591cd0767cbc48d53d0
Nathan Dunn <ndunn@me.com>
2015-04-21 09:42:42 -0700

    #295 - added hooks for domain in preference

--- grails-app/domain/org/bbop/apollo/Preference.groovy
+++ grails-app/domain/org/bbop/apollo/Preference.groovy
@@ -4,9 +4,11 @@ class Preference {
 
     static constraints = {
         name nullable: true ,blank: false
+        domain nullable: true ,blank: false
         preferencesString nullable: true, blank: false
     }
 
     String name
+    String domain  // if we want to filter for a user / group domain
     String preferencesString // can embed JSONObject
 }
