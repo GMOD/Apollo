656bf71ee923d1e795b10b5a362536427bf686f0
Nathan Dunn <ndunn@me.com>
2015-04-21 18:18:18 -0700

    added min and max to sequence pref

--- grails-app/domain/org/bbop/apollo/UserOrganismPreference.groovy
+++ grails-app/domain/org/bbop/apollo/UserOrganismPreference.groovy
@@ -7,11 +7,15 @@ class UserOrganismPreference extends UserPreference{
         currentOrganism nullable: false
 //        defaultSequence nullable: true, blank: false
         sequence nullable: true, blank: false
+        min nullable: true, blank: false
+        max nullable: true, blank: false
     }
 
     Organism organism
 //    String defaultSequence
     Boolean currentOrganism
     Sequence sequence
+    Integer min
+    Integer max
 
 }
