345d85d3eef134e5c3375ddcbdb0b04107b30b6f
Colin <colin.diesh@gmail.com>
2015-04-24 21:31:35 -0500

    Cleanup configs and fill up sample h2 file. See #286

--- sample-h2-apollo-config.groovy
+++ sample-h2-apollo-config.groovy
@@ -1,8 +1,53 @@
-grails {
-//    dataSource.username = "web_apollo_user"
-//    dataSource.password = "web_apollo_user"
-//    dataSource.driverClassName = "org.h2.Driver"
-//    dataSource.url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
-	apollo.jbrowse.data.directory="/opt/apollo/jbrowse/data"
+// default username/password on h2 are given here. see docs for adjusting
+dataSource {
+    pooled = true
+    jmxExport = true
+    driverClassName = "org.h2.Driver"
+    username = "sa"
+    password = ""
+}
+// environment specific settings
+environments {
+    development {
+        dataSource {
+            dbCreate = "update" // one of 'create', 'create-drop', 'update', 'validate', ''
+            url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
+        }
+    }
+    test {
+        dataSource {
+            dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
+            url = "jdbc:h2:mem:testDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
+        }
+    }
 
+    //note: not necessarily recommended to use h2 in production mode. see precautions
+    production {
+        dataSource {
+            dbCreate = "update"
+            //note: production mode uses file instead of mem database
+            //url = "jdbc:h2:/opt/apollo/h2/prodDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
+            url = "jdbc:h2:prodDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
+            properties {
+               // See http://grails.org/doc/latest/guide/conf.html#dataSource for documentation
+               jmxEnabled = true
+               initialSize = 5
+               maxActive = 50
+               minIdle = 5
+               maxIdle = 25
+               maxWait = 10000
+               maxAge = 10 * 60000
+               timeBetweenEvictionRunsMillis = 5000
+               minEvictableIdleTimeMillis = 60000
+               validationQuery = "SELECT 1"
+               validationQueryTimeout = 3
+               validationInterval = 15000
+               testOnBorrow = true
+               testWhileIdle = true
+               testOnReturn = false
+               jdbcInterceptors = "ConnectionState"
+               defaultTransactionIsolation = java.sql.Connection.TRANSACTION_READ_COMMITTED
+            }
+        }
+    }
 }
--- sample-postgres-apollo-config.groovy
+++ sample-postgres-apollo-config.groovy
@@ -1,54 +1,49 @@
-grails {
-
-
-    environments {
-        development {
-            dataSource.dbCreate = "update" // one of 'create', 'create-drop', 'update', 'validate', ''
-            dataSource.username = "<CHANGEME>"
-            dataSource.password = "<CHANGEME>"
-            dataSource.driverClassName = "org.postgresql.Driver"
-            dataSource.dialect = org.hibernate.dialect.PostgresPlusDialect
-            dataSource.url = "jdbc:postgresql://localhost/apollo"
-        }
-        test {
-            dataSource.dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
-            dataSource.username = "<CHANGEME>"
-            dataSource.password = "<CHANGEME>"
-            dataSource.driverClassName = "org.postgresql.Driver"
-            dataSource.dialect = org.hibernate.dialect.PostgresPlusDialect
-            dataSource.url = "jdbc:postgresql://localhost/apollo-test"
-        }
-        production {
-            dataSource{
-                dbCreate = "update" // one of 'create', 'create-drop', 'update', 'validate', ''
-                username = "<CHANGEME>"
-                password = "<CHANGEME>"
-                driverClassName = "org.postgresql.Driver"
-                dialect = org.hibernate.dialect.PostgresPlusDialect
-                url = "jdbc:postgresql://localhost/apollo-production"
-                properties {
-                    // See http://grails.org/doc/latest/guide/conf.html#dataSource for documentation
-                    jmxEnabled = true
-                    initialSize = 5
-                    maxActive = 50
-                    minIdle = 5
-                    maxIdle = 25
-                    maxWait = 10000
-                    maxAge = 10 * 60000
-                    timeBetweenEvictionRunsMillis = 5000
-                    minEvictableIdleTimeMillis = 60000
-                    validationQuery = "SELECT 1"
-                    validationQueryTimeout = 3
-                    validationInterval = 15000
-                    testOnBorrow = true
-                    testWhileIdle = true
-                    testOnReturn = false
-                    jdbcInterceptors = "ConnectionState"
-                    defaultTransactionIsolation = java.sql.Connection.TRANSACTION_READ_COMMITTED
-                }
+environments {
+    development {
+        dataSource.dbCreate = "update" // one of 'create', 'create-drop', 'update', 'validate', ''
+        dataSource.username = "<CHANGEME>"
+        dataSource.password = "<CHANGEME>"
+        dataSource.driverClassName = "org.postgresql.Driver"
+        dataSource.dialect = org.hibernate.dialect.PostgresPlusDialect
+        dataSource.url = "jdbc:postgresql://localhost/apollo"
+    }
+    test {
+        dataSource.dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
+        dataSource.username = "<CHANGEME>"
+        dataSource.password = "<CHANGEME>"
+        dataSource.driverClassName = "org.postgresql.Driver"
+        dataSource.dialect = org.hibernate.dialect.PostgresPlusDialect
+        dataSource.url = "jdbc:postgresql://localhost/apollo-test"
+    }
+    production {
+        dataSource{
+            dbCreate = "update" // one of 'create', 'create-drop', 'update', 'validate', ''
+            username = "<CHANGEME>"
+            password = "<CHANGEME>"
+            driverClassName = "org.postgresql.Driver"
+            dialect = org.hibernate.dialect.PostgresPlusDialect
+            url = "jdbc:postgresql://localhost/apollo-production"
+            properties {
+                // See http://grails.org/doc/latest/guide/conf.html#dataSource for documentation
+                jmxEnabled = true
+                initialSize = 5
+                maxActive = 50
+                minIdle = 5
+                maxIdle = 25
+                maxWait = 10000
+                maxAge = 10 * 60000
+                timeBetweenEvictionRunsMillis = 5000
+                minEvictableIdleTimeMillis = 60000
+                validationQuery = "SELECT 1"
+                validationQueryTimeout = 3
+                validationInterval = 15000
+                testOnBorrow = true
+                testWhileIdle = true
+                testOnReturn = false
+                jdbcInterceptors = "ConnectionState"
+                defaultTransactionIsolation = java.sql.Connection.TRANSACTION_READ_COMMITTED
             }
         }
     }
-
 }
 
