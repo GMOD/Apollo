f7bc3dd5d1c5af4f0f6e436977ba594511fe7951
Nathan Dunn <ndunn@me.com>
2015-04-29 12:21:00 -0700

    #223 - refactored feature_event

--- grails-app/domain/org/bbop/apollo/FeatureEvent.groovy
+++ grails-app/domain/org/bbop/apollo/FeatureEvent.groovy
@@ -1,5 +1,7 @@
 package org.bbop.apollo
 
+import org.bbop.apollo.history.FeatureOperation
+
 //import org.bbop.apollo.history.FeatureOperation
 
 class FeatureEvent {
@@ -8,20 +10,37 @@ class FeatureEvent {
     Date dateCreated
     Date lastUpdated
     User editor
-    String uniqueName // from feature
-    String operation
+    FeatureOperation operation
     Boolean current
 
+    String uniqueName // from original top-level feature
+    String originalJsonCommand
+    String previousJsonObject1
+    String previousJsonObject2
+
+    // TODO: remove
     String newFeaturesJsonArray
     String oldFeaturesJsonArray
 
+
     static constraints = {
         editor nullable: true
+        originalJsonCommand nullable: true, blank: false
+        previousJsonObject1 nullable: true, blank: false
+        previousJsonObject2 nullable: true, blank: false
+
+
+        // TODO: remove
         newFeaturesJsonArray nullable: true
         oldFeaturesJsonArray nullable: true
     }
 
     static mapping = {
+        originalJsonCommand type: "text"
+        previousJsonObject1 type: "text"
+        previousJsonObject2 type: "text"
+
+        // TODO: remove
         newFeaturesJsonArray type: "text"
         oldFeaturesJsonArray type: "text"
     }
