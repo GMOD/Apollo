25898ae4a577a1c0bf5b40617391759f1ec6b834
Nathan Dunn <ndunn@me.com>
2015-05-13 17:25:16 -0700

    #348 - still seems to run properly after remove extra code

--- src/main/webapp/config/track_name_comparator.js
+++
@@ -1,8 +0,0 @@
-/* Function for comparing the annotation track names for sorting in the
-   selection table.  You can customize this to provide your own sorting
-   function.  Note that the function must be called "track_name_comparator"
-   and return -1 for "less than", 0 for equal, and 1 for "greater than".
-*/
-function track_name_comparator(a, b) {
-	return a < b ? -1 : a > b ? 1 : 0;
-};
