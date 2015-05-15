7c840a879c47717278681630221f5a6108227e30
Nathan Dunn <ndunn@me.com>
2015-05-15 15:08:45 -0700

    updated organism script to be more uniform

--- docs/web_services/examples/groovy/add_organism.groovy
+++ docs/web_services/examples/groovy/add_organism.groovy
@@ -11,21 +11,21 @@ String usageString = "add_organism.groovy <options>" +
         "Example: " +
         "./add_organism.groovy -name LargeBees -url http://localhost:8080/apollo/ -directory /opt/apollo/yeast -username ndunn@me.com -password supersecret"
 
-def cli = new CliBuilder(usage:'add_organism.groovy <options>')
+def cli = new CliBuilder(usage: 'add_organism.groovy <options>')
 cli.setStopAtNonOption(true)
-cli.url('URL to WebApollo instance',required:true,args: 1)
-cli.name('organism common name',required: true,args: 1)
-cli.directory('jbrowse data directory',required: true,args: 1)
-cli.blatdb('blatdb directory',args: 1)
-cli.genus('genus',args: 1)
-cli.species('species',args: 1)
-cli.username('username',required: true,args: 1)
-cli.password('password',required:true,args: 1)
+cli.url('URL to WebApollo instance', required: true, args: 1)
+cli.name('organism common name', required: true, args: 1)
+cli.directory('jbrowse data directory', required: true, args: 1)
+cli.blatdb('blatdb directory', args: 1)
+cli.genus('genus', args: 1)
+cli.species('species', args: 1)
+cli.username('username', required: true, args: 1)
+cli.password('password', required: true, args: 1)
 OptionAccessor options
 try {
     options = cli.parse(args)
 
-    if( !(options?.url && options?.name && options?.directory && options?.username && options?.password)){
+    if (!(options?.url && options?.name && options?.directory && options?.username && options?.password)) {
         return
     }
 
@@ -34,24 +34,29 @@ try {
     return
 }
 
+
+URL url = new URL(options.url)
+
 def argumentsArray = [
-    commonName: options.name,
-    directory: options.directory,
-    username: options.username,
-    password: options.password,
-    blatdb: options.blatdb?:null,
-    genus: options.genus?:null,
-    species: options.species?:null
+        commonName: options.name,
+        directory : options.directory,
+        username  : options.username,
+        password  : options.password,
+        blatdb    : options.blatdb ?: null,
+        genus     : options.genus ?: null,
+        species   : options.species ?: null
 ]
 
 println "arguments array = ${argumentsArray}"
 
 def client = new RESTClient(options.url)
 
+String fullPath = "${url.path}/organism/addOrganism"
+
 def resp = client.post(
-    contentType: 'text/javascript',
-    path: '/apollo/organism/addOrganism',
-    body : argumentsArray
+        contentType: 'text/javascript',
+        path: fullPath,
+        body: argumentsArray
 )
 
 assert resp.status == 200  // HTTP response code; 404 means not found, etc.
