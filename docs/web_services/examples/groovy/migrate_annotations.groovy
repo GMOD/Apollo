a42e4c592a21f4c89b605cbec2af7fcbeb10bcf2
Deepak Unni <deepak.unni3@gmail.com>
2015-05-20 15:59:26 -0500

    Getting features via webservices

--- docs/web_services/examples/groovy/migrate_annotations.groovy
+++ docs/web_services/examples/groovy/migrate_annotations.groovy
@@ -1,9 +1,11 @@
 #!/usr/bin/env groovy
+
 import groovyx.net.http.HTTPBuilder
 import groovyx.net.http.Method
 import groovyx.net.http.RESTClient
-import org.json.JSONObject
-import static groovyx.net.http.ContentType.*
+import net.sf.json.JSONArray
+import net.sf.json.JSONObject
+
 
 @Grab(group = 'org.json', module = 'json', version = '20140107')
 @Grab(group = 'org.codehaus.groovy.modules.http-builder', module = 'http-builder', version = '0.7')
@@ -11,14 +13,15 @@ import static groovyx.net.http.ContentType.*
 String usageString = "migrate_annotations.groovy <options>" +
         "Example: \n" +
 //        "./migrate_annotations.groovy -sourceurl http://remoteserver:8080/apollo/ -destinationurl http://localhost:8080/apollo/ -name amel -username ndunn@me.com -password supersecret"
-        "./migrate_annotations.groovy -sourceurl http://localhost:8080/apollo/ -destinationurl newserver -username ndunn@me.com -password demo -organism Honey1"
+        "./migrate_annotations.groovy -sourceurl http://localhost:8080/apollo/ -source_organism amel -destinationurl newserver -destination_organism amel2  -username ndunn@me.com -password demo -organism Honey1"
 
 println "intro"
 def cli = new CliBuilder(usage: 'migrate_annotations.groovy <options>')
 cli.setStopAtNonOption(true)
 cli.sourceurl('URL of source WebApollo instance from which annotations are fetched', required: true, args: 1)
 cli.destinationurl('URL of destination WebApollo instance to which annotations are to be loaded', required: true, args: 1)
-cli.organism('organism common name', required: true, args: 1)
+cli.source_organism('source organism common name', required: true, args: 1)
+cli.destination_organism('destination organism common name', required: true, args: 1)
 cli.username('username', required: true, args: 1)
 cli.password('password', required: true, args: 1)
 OptionAccessor options
@@ -28,7 +31,7 @@ println "clie done"
 try {
     options = cli.parse(args)
 
-    if (!(options?.sourceurl && options?.destinationurl && options?.organism && options?.username && options?.password)) {
+    if (!(options?.sourceurl && options?.destinationurl && options?.source_organism && options?.destination_organism && options?.username && options?.password)) {
         return
     }
 } catch (e) {
@@ -40,9 +43,33 @@ def getFeaturesClient = new RESTClient(options.sourceurl)
 def getFeaturesResponse = getFeaturesClient.post(
         contentType: 'text/javascript',
         path: '/apollo/annotationEditor/getFeatures',
-        body: [operation: 'get_features', 'username': options.username, 'password': options.password, 'track': "Annotations-Group1.1", 'organism': options.name]
+        body: [operation: 'get_features', 'username': options.username, 'password': options.password, 'track': "Annotations-Group1.1", 'organism': options.source_organism]
 )
 
-println "GetFeaturesResponse status: ${getFeaturesResponse.status}"
-println "GetFeaturesResponse content: ${getFeaturesResponse.getData()}"
+assert getFeaturesResponse.status == 200
+println getFeaturesResponse.getData()
+if (getFeaturesResponse.getData().features.size() == 0) {
+    println "Couldn't establish session with ${options.sourceurl}"
+    return
+}
+
+count = 0
+JSONArray jsonArray = new JSONArray()
+for (def entity : getFeaturesResponse.getData().features) {
+    jsonArray.add(count, entity as JSONObject)
+    count += 1
+}
+
+println "JSONARRAY SIZE: ${jsonArray.size()}"
+
+def loadFeaturesClient = new RESTClient(options.destinationurl)
+def loadFeaturesResponse = loadFeaturesClient.post(
+        contentType: 'text/javascript',
+        path: '/apollo/annotationEditor/addFeature',
+        body: [ operation: 'add_feature', 'username' : options.username, 'password' : options.password, 'track' : "Annotations-Group1.1", 'organism' : options.destination_organism, 'features' : jsonArray ]
+)
+
+assert loadFeaturesResponse.status == 200
+println "loadFeaturesResponse content: ${loadFeaturesResponse.getData()}"
+
 
