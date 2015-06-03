e7bb298dbc0bd31b237fb61e9071f44f979b9260
Colin <colin.diesh@gmail.com>
2015-06-03 00:38:33 -0500

    Change test env to update from create-drop to avoid h2 errors

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -26,7 +26,7 @@ environments {
     test {
         dataSource {
             //dbCreate = "update"
-            dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
+            dbCreate = "update" // one of 'create', 'create-drop', 'update', 'validate', ''
             url = "jdbc:h2:mem:testDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
         }
     }
