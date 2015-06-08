4ede45e36dcd65b59868adef8526bf6fc0bbe326
Nathan Dunn <ndunn@me.com>
2015-06-08 12:18:00 -0700

    #255 - got get features to work via curl

--- docs/web_services/examples/shell/getFeatures.sh
+++ docs/web_services/examples/shell/getFeatures.sh
@@ -4,6 +4,11 @@ url=$1
 username=$2
 password=$3
 
-curl -b ${username}_cookes.txt -c ${username}_cookies.txt -H "Content-Type:application/json" -d "{'username': '${username}', 'password': '${password}'}" "${url}/Login?operation=login" 2> /dev/null
+username='demo'
+
+#curl -b ${username}_cookes.txt -c ${username}_cookies.txt -H "Content-Type:application/json" -d "{'username': '${username}', 'password': '${password}'}" "${url}/Login?operation=login" 2> /dev/null
 # curl -b cookies.txt -c cookies.txt -e  "http://localhost:8080" -H "Content-Type:application/json" -d "{'username': 'demo', 'password': 'demo'}" "http://localhost:8080/apollo/Login?operation=login"
-curl -b ${username}_cookies.txt -c ${username}_cookies.txt -e  "http://localhost:8080" --data '{ operation: "write", adapter: "GFF3", tracks: ["Annotations-scf1117875582023"], options: "output=file&format=gzip" }' http://localhost:8080/apollo/IOService
+#curl -b ${username}_cookies.txt -c ${username}_cookies.txt -e  "http://icebox.lbl.gov/WebApolloDemo/" -d "{ 'operation': 'get_features', 'tracks': ['Annotations-Group1.10']}" http://icebox.lbl.gov/WebApolloDemo/
+
+curl -b demo_cookies.txt -c demo_cookies.txt -H "Content-Type:application/json" -d "{'username': 'demo', 'password': 'demo'}" http://icebox.lbl.gov/WebApolloDemo/Login?operation=login
+curl -b demo_cookies.txt -c demo_cookies.txt -e "http://icebox.lbl.gov/WebApolloDemo/" --data "{ 'operation': 'get_features', 'track': 'Annotations-Group1.10'}" http://icebox.lbl.gov/WebApolloDemo/AnnotationEditorService
