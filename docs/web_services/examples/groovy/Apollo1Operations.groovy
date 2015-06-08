9aa04d935865f03d3c2f4e8168b3f3a0ea7e3cc5
Nathan Dunn <ndunn@me.com>
2015-06-08 14:52:07 -0700

    #254 - began to stub operations

--- docs/web_services/examples/groovy/Apollo1Operations.groovy
+++ docs/web_services/examples/groovy/Apollo1Operations.groovy
@@ -1,7 +1,12 @@
 
 import groovy.json.JsonSlurper
+import net.sf.json.JSONArray
+import net.sf.json.JSONObject
 
 @Grab(group = 'org.json', module = 'json', version = '20140107')
+@Grab(group='postgresql', module='postgresql', version='8.3-603.jdbc4')
+
+
 
 
 static def getFeature(url,track,cookieFile){
@@ -20,11 +25,21 @@ static def getFeature(url,track,cookieFile){
 
 static def doLogin(url, username, password,cookieFile) {
     String json = "{'username': '${username}', 'password': '${password}'}"
-    def process = ["curl","-c",cookieFile,"-H","Content-Type:application/json","-d",json,"${url}/Login?operation=login"].execute()
+    def process = ["curl", "-c", cookieFile, "-H", "Content-Type:application/json", "-d", json, "${url}/Login?operation=login"].execute()
     def response = process.text
-    if(process.exitValue()!=0){
+    if (process.exitValue() != 0) {
         println process.errorStream.text
     }
     def jsonResponse = new JsonSlurper().parseText(response)
     return jsonResponse
+}
+
+static def getUsers(url,username,password){
+    JSONArray usersArray = new JSONArray()
+//    ["psql","-U",username,"-p",password]
+    Class.forName("org.postgresql.Driver");
+
+
+
+    return usersArray
 }
---
+++ docs/web_services/examples/groovy/migrate_users1to2.groovy
@@ -0,0 +1,55 @@
+#!/usr/bin/env groovy
+evaluate(new File("./Apollo1Operations.groovy"))
+evaluate(new File("./Apollo2Operations.groovy"))
+
+import net.sf.json.JSONArray
+import net.sf.json.JSONObject
+
+
+@Grab(group = 'org.json', module = 'json', version = '20140107')
+@Grab(group = 'org.codehaus.groovy.modules.http-builder', module = 'http-builder', version = '0.7')
+
+String usageString = "migrate_users1to2.groovy <options>" +
+        "Example: \n" +
+        "./migrate_users1to2.groovy -username1 demo -password1 demo -databaseurl databaseurl -username2 ndunn@me.com -password2 demo  -organism Honey2 -destinationurl http://localhost:8080/WebApollo2/ "
+
+def cli = new CliBuilder(usage: 'migrate_users.groovy <options>')
+cli.setStopAtNonOption(true)
+cli.databaseurl('URL of WebApollo 1.0.x database from which users are fetched', required: true, args: 1)
+cli.destinationurl('URL of WebApollo 2.0.x instance to which users are to be loaded', required: true, args: 1)
+cli.username1('username1', required: true, args: 1)
+cli.password1('password1', required: true, args: 1)
+cli.username2('username2', required: true, args: 1)
+cli.password2('password2', required: true, args: 1)
+OptionAccessor options
+
+try {
+    options = cli.parse(args)
+
+    if (!(options?.destinationurl && options?.databaseurl && options?.username2 && options?.password2 && options?.username1 && options?.password1)) {
+        println "\n"+usageString
+        return
+    }
+} catch (e) {
+    println(e)
+    return
+}
+
+String cookieFile = "${options.username2}_cookies.txt"
+
+def responseArray = Apollo1Operations.getUsers(options.username1, options.password1,options.databaseurl)
+if (responseArray == null) {
+    println "Could not communicate with ${options.databaseurl}"
+    return
+}
+
+JSONObject newArray = new JSONObject()
+JSONArray addUsersArray = new JSONArray()
+
+def users = Apollo1Operations.getUsers(options.databaseurl,options.username1,options.username2)
+
+println users
+
+
+
+
