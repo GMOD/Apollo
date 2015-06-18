bf5acd3fc4fa9aa61c04b0e462a097b2a0bb1dcd
Colin <colin.diesh@gmail.com>
2015-06-18 12:58:01 -0500

    Remove unused scripts

--- tools/data/split_gff_by_source.pl
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/perl
-
-print "This is unused. The flatfile-to-json.pl --type argument can accept filters on the source column, e.g. --type exonerate:mRNA which removes the need for this script."
