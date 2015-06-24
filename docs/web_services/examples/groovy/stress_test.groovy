c7979b111a3a8188a7a08d3f3f50e882fa013c53
Nathan Dunn <ndunn@me.com>
2015-06-24 11:05:27 -0700

    #452 - handling a variable load as well

--- docs/web_services/examples/groovy/stress_test.groovy
+++ docs/web_services/examples/groovy/stress_test.groovy
@@ -12,7 +12,7 @@ import net.sf.json.JSONObject
 
 String usageString = "stress_test.groovy <options>" +
         "Example: \n" +
-        "./stress_test.groovy -iter 100 -concurrency 3 -username ndunn@me.com -password demo  -organism Honey2 -destinationurl http://localhost:8080/WebApollo2/"
+        "./stress_test.groovy -iter 100 -concurrency 3 -username ndunn@me.com -password demo  -organism Honey2 -destinationurl http://localhost:8080/WebApollo2/ -load 10"
 
 def cli = new CliBuilder(usage: 'stress_test.groovy <options>')
 cli.setStopAtNonOption(true)
@@ -20,16 +20,17 @@ cli.destinationurl('URL of WebApollo 2.0.x instance to which annotations are to 
 cli.organism('organism common name', required: true, args: 1)
 cli.username('username', required: true, args: 1)
 cli.password('password', required: true, args: 1)
-cli.concurrency('concurrency', required: false, args: 1)
+cli.concurrency('concurrent transaction', required: false, args: 1)
 cli.iter('iter', required: false, args: 1)
+cli.load('load level (0-10)', required: false, args: 1)
 
 OptionAccessor options
 
 try {
     options = cli.parse(args)
 
-    if (!(options?.destinationurl && options?.organism && options?.username && options?.password )) {
-        println "\n"+usageString
+    if (!(options?.destinationurl && options?.organism && options?.username && options?.password)) {
+        println "\n" + usageString
         return
     }
 } catch (e) {
@@ -38,27 +39,70 @@ try {
 }
 
 String sequenceName = "Annotations-Group1.10"
-int concurrency = options.concurrency ? Integer.parseInt(options.concurrency)  : 1
-int iter = options.iter ? Integer.parseInt(options.iter)  : 1
+int concurrency = options.concurrency ? Integer.parseInt(options.concurrency) : 1
+int iter = options.iter ? Integer.parseInt(options.iter) : 1
+int load = options.load ? Integer.parseInt(options.load) : 1
+load = load < 1 ? 1 : load
+load = load > 10 ? 10 : load
 
-JSONArray sampleFeaturesArray = SampleFeatures.getSampleFeatures()
+println "concurrency: ${concurrency}"
+println "iter: ${iter}"
+println "load: ${load}"
+
+
+JSONArray inputArray = SampleFeatures.getSampleFeatures()
+JSONArray sampleFeaturesArray = new JSONArray()
+for(int i = 0 ; i < load ; i++){
+   sampleFeaturesArray.add(inputArray.getJSONObject(i))
+}
 int sampleFeaturesSize = sampleFeaturesArray.size()
 
-for(int i = 0 ; i < (int) iter ; i++){
-    for(int j = 0 ; j < (int) concurrency ; j++){
-        JSONArray deleteArray = new JSONArray()
-        def response = Apollo2Operations.triggerAddTranscript(options.destinationurl, options.username, options.password, options.organism, sequenceName, sampleFeaturesArray)
-//        println "response ${response.features.uniqueName}"
-//        println "response ${response.features.collect{it.keySet().join(",")}}"
-        println "response ${response.features.collect{it.uniquename}}"
-        response.features.collect{it.uniquename}.each(){ uniquename ->
-            JSONObject jsonObject = new JSONObject()
-            jsonObject.put("uniquename",uniquename)
-            deleteArray.add(jsonObject)
+long startTime = System.currentTimeMillis()
+
+for (int i = 0; i < (int) iter; i++) {
+
+    def threads = []
+    for (int j = 0; j < (int) concurrency; j++) {
+
+        def thread = new Thread({
+            JSONArray deleteArray = new JSONArray()
+            def response = Apollo2Operations.triggerAddTranscript(options.destinationurl, options.username, options.password, options.organism, sequenceName, sampleFeaturesArray)
+            println "response ${response.features.collect { it.uniquename }}"
+            response.features.collect { it.uniquename }.each() { uniquename ->
+                JSONObject jsonObject = new JSONObject()
+                jsonObject.put("uniquename", uniquename)
+                deleteArray.add(jsonObject)
+            }
+            Apollo2Operations.triggerRemoveTranscript(options.destinationurl, options.username, options.password, options.organism, sequenceName, deleteArray)
+        })
+        threads << thread
+    }
+    threads.each { it.start() }
+
+    boolean threadsRunning = true
+    while(threadsRunning){
+        threadsRunning = false
+        threads.each { Thread it ->
+            if(it?.alive){
+                threadsRunning = true
+            }
         }
-        Apollo2Operations.triggerRemoveTranscript(options.destinationurl, options.username, options.password, options.organism, sequenceName, deleteArray)
+        sleep(50l)
     }
 }
+
+long totalTime = System.currentTimeMillis() - startTime
+int totalFeatures = iter * concurrency * load
+int totalTransactions = iter * concurrency
+
+System.out.println("total: "+totalTime/1000f)
+System.out.println("per iteration : "+totalTime/(iter*1000f))
+System.out.println("per transaction: "+totalTime/(totalTransactions*1000f))
+System.out.println("per feature: "+totalTime/(totalFeatures*1000f))
+
+
+
+
 
 //sequenceArray = options.sequence_names.tokenize(',')
 //for (String sequence in sequenceArray) {
