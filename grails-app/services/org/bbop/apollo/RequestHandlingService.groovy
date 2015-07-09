14a7568c82339c1f0d56ad8605c5213e58948de2
Nathan Dunn <ndunn@me.com>
2015-07-08 17:06:38 -0700

    #468 - fixed non-sensical stuff

--- grails-app/services/org/bbop/apollo/RequestHandlingService.groovy
+++ grails-app/services/org/bbop/apollo/RequestHandlingService.groovy
@@ -1461,12 +1461,10 @@ class RequestHandlingService {
                 feature = transcriptService.flipTranscriptStrand((Transcript) feature);
                 featureService.setLongestORF((Transcript) feature)
                 nonCanonicalSplitSiteService.findNonCanonicalAcceptorDonorSpliceSites((Transcript) feature)
-//                featureEventService.addNewFeatureEvent(FeatureOperation.FLIP_STRAND, feature, inputObject, permissionService.getActiveUser(inputObject))
                 featureEventService.addNewFeatureEventWithUser(FeatureOperation.FLIP_STRAND, transcriptService.getGene((Transcript) feature).name, feature.uniqueName, inputObject, featureService.convertFeatureToJSON((Transcript) feature), permissionService.getActiveUser(inputObject))
             } else {
+                // TODO: does this even make sense?
                 feature = featureService.flipStrand(feature)
-                featureService.setLongestORF((Transcript) feature)
-                nonCanonicalSplitSiteService.findNonCanonicalAcceptorDonorSpliceSites((Transcript) feature)
                 featureEventService.addNewFeatureEventWithUser(FeatureOperation.FLIP_STRAND, feature.name, feature.uniqueName, inputObject, featureService.convertFeatureToJSON(feature), permissionService.getActiveUser(inputObject))
             }
 
