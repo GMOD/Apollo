841b4826a5af2e661de29cc116e5d727692bf8aa
Eric Rasche <rasche.eric@gmail.com>
2015-09-29 16:08:22 -0500

    Try to clearly delineate function of variables

--- sample-docker-apollo-config.groovy
+++ sample-docker-apollo-config.groovy
@@ -22,17 +22,17 @@ environments {
             properties {
                 // See http://grails.org/doc/latest/guide/conf.html#dataSource for documentation
                 jmxEnabled = System.getenv("WEBAPOLLO_JMX_ENABLED").equals("true")
-                initialSize                   = System.getenv("WEBAPOLLO_CONF_INITIAL_SIZE")             ? System.getenv("WEBAPOLLO_CONF_INITIAL_SIZE").toInteger()             : 5
-                maxActive                     = System.getenv("WEBAPOLLO_CONF_MAX_ACTRIVE")              ? System.getenv("WEBAPOLLO_CONF_MAX_ACTRIVE").toInteger()              : 50
-                minIdle                       = System.getenv("WEBAPOLLO_CONF_MIN_IDLE")                 ? System.getenv("WEBAPOLLO_CONF_MIN_IDLE").toInteger()                 : 5
-                maxIdle                       = System.getenv("WEBAPOLLO_CONF_MAX_IDLE")                 ? System.getenv("WEBAPOLLO_CONF_MAX_IDLE").toInteger()                 : 25
-                maxWait                       = System.getenv("WEBAPOLLO_CONF_MAX_WAIT")                 ? System.getenv("WEBAPOLLO_CONF_MAX_WAIT").toInteger()                 : 10000
-                maxAge                        = System.getenv("WEBAPOLLO_CONF_MAX_AGE")                  ? System.getenv("WEBAPOLLO_CONF_MAX_AGE").toInteger()                  : 10 * 60000
-                timeBetweenEvictionRunsMillis = System.getenv("WEBAPOLLO_CONF_EVICTION_DELAY")           ? System.getenv("WEBAPOLLO_CONF_EVICTION_DELAY").toInteger()           : 5000
-                minEvictableIdleTimeMillis    = System.getenv("WEBAPOLLO_CONF_MIN_EVICTABLE_IDLE")       ? System.getenv("WEBAPOLLO_CONF_MIN_EVICTABLE_IDLE").toInteger()       : 60000
-                validationQuery               = System.getenv("WEBAPOLLO_CONF_VALIDATION_QUERY")         ? System.getenv("WEBAPOLLO_CONF_VALIDATION_QUERY").toInteger()         : "SELECT 1"
-                validationQueryTimeout        = System.getenv("WEBAPOLLO_CONF_VALIDATION_QUERY_TIMEOUT") ? System.getenv("WEBAPOLLO_CONF_VALIDATION_QUERY_TIMEOUT").toInteger() : 3
-                validationInterval            = System.getenv("WEBAPOLLO_CONF_VALIDATION_INTERVAL")      ? System.getenv("WEBAPOLLO_CONF_VALIDATION_INTERVAL").toInteger()      : 15000
+                initialSize                   = System.getenv("WEBAPOLLO_SERVER_INITIAL_SIZE")             ? System.getenv("WEBAPOLLO_SERVER_INITIAL_SIZE").toInteger()             : 5
+                maxActive                     = System.getenv("WEBAPOLLO_SERVER_MAX_ACTRIVE")              ? System.getenv("WEBAPOLLO_SERVER_MAX_ACTRIVE").toInteger()              : 50
+                minIdle                       = System.getenv("WEBAPOLLO_SERVER_MIN_IDLE")                 ? System.getenv("WEBAPOLLO_SERVER_MIN_IDLE").toInteger()                 : 5
+                maxIdle                       = System.getenv("WEBAPOLLO_SERVER_MAX_IDLE")                 ? System.getenv("WEBAPOLLO_SERVER_MAX_IDLE").toInteger()                 : 25
+                maxWait                       = System.getenv("WEBAPOLLO_SERVER_MAX_WAIT")                 ? System.getenv("WEBAPOLLO_SERVER_MAX_WAIT").toInteger()                 : 10000
+                maxAge                        = System.getenv("WEBAPOLLO_SERVER_MAX_AGE")                  ? System.getenv("WEBAPOLLO_SERVER_MAX_AGE").toInteger()                  : 10 * 60000
+                timeBetweenEvictionRunsMillis = System.getenv("WEBAPOLLO_SERVER_EVICTION_DELAY")           ? System.getenv("WEBAPOLLO_SERVER_EVICTION_DELAY").toInteger()           : 5000
+                minEvictableIdleTimeMillis    = System.getenv("WEBAPOLLO_SERVER_MIN_EVICTABLE_IDLE")       ? System.getenv("WEBAPOLLO_SERVER_MIN_EVICTABLE_IDLE").toInteger()       : 60000
+                validationQuery               = System.getenv("WEBAPOLLO_SERVER_VALIDATION_QUERY")         ? System.getenv("WEBAPOLLO_SERVER_VALIDATION_QUERY").toInteger()         : "SELECT 1"
+                validationQueryTimeout        = System.getenv("WEBAPOLLO_SERVER_VALIDATION_QUERY_TIMEOUT") ? System.getenv("WEBAPOLLO_SERVER_VALIDATION_QUERY_TIMEOUT").toInteger() : 3
+                validationInterval            = System.getenv("WEBAPOLLO_SERVER_VALIDATION_INTERVAL")      ? System.getenv("WEBAPOLLO_SERVER_VALIDATION_INTERVAL").toInteger()      : 15000
                 testOnBorrow = true
                 testWhileIdle = true
                 testOnReturn = false
