c5074c20c45d8bb731008967a721a817edc057e5
Nathan Dunn <ndunn@me.com>
2015-09-29 14:15:08 -0700

    #525 - merge functionality working properly now

--- src/gwt/org/bbop/apollo/gwt/client/BookmarkPanel.java
+++ src/gwt/org/bbop/apollo/gwt/client/BookmarkPanel.java
@@ -8,6 +8,7 @@ import com.google.gwt.cell.client.FieldUpdater;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.core.client.Scheduler;
 import com.google.gwt.dom.client.BrowserEvents;
+import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.KeyUpEvent;
 import com.google.gwt.json.client.JSONArray;
 import com.google.gwt.json.client.JSONObject;
@@ -89,19 +90,20 @@ public class BookmarkPanel extends Composite {
     AbsolutePanel absolutePanel;
 
 
+    private PickupDragController dragController ;
     public static ListDataProvider<BookmarkInfo> dataProvider = new ListDataProvider<>();
-    //    private static List<BookmarkInfo> trackInfoList = new ArrayList<>();
     private static List<BookmarkInfo> bookmarkInfoList = dataProvider.getList();
     private MultiSelectionModel<BookmarkInfo> selectionModel = new MultiSelectionModel<BookmarkInfo>();
-    private boolean trackSelectionFix; // this fixes the fact that firefox requires two clicks to select a CheckboxCell
 
     public BookmarkPanel() {
-//        exportStaticMethod();
-        trackSelectionFix = true;
 
         Widget rootElement = ourUiBinder.createAndBindUi(this);
+
         initWidget(rootElement);
 
+        dragController = new PickupDragController(absolutePanel, true);
+        FlowPanelDropController flowPanelDropController = new FlowPanelDropController( dragAndDropPanel);
+        dragController.registerDropController(flowPanelDropController);
         dataGrid.setWidth("100%");
         foldType.addItem("None");
         foldType.addItem("Exon");
@@ -116,53 +118,6 @@ public class BookmarkPanel extends Composite {
         // fix selected style: http://comments.gmane.org/gmane.org.google.gwt/70747
         dataGrid.setEmptyTableWidget(new Label("No bookmarks!"));
 
-
-//        Column<BookmarkInfo, Boolean> showColumn = new Column<BookmarkInfo, Boolean>(new CheckboxCell(true, false)) {
-//            @Override
-//            public Boolean getValue(BookmarkInfo track) {
-//                return track.getVisible();
-//            }
-//        };
-//        dataGrid.addCellPreviewHandler(new CellPreviewEvent.Handler<BookmarkInfo>() {
-//
-//            @Override
-//            public void onCellPreview(final CellPreviewEvent<BookmarkInfo> event) {
-//
-//                if (BrowserEvents.CLICK.equals(event.getNativeEvent().getType())) {
-//                    trackSelectionFix=false;
-//                    final BookmarkInfo value = event.getValue();
-//                    final Boolean state = !event.getDisplay().getSelectionModel().isSelected(value);
-//                    event.getDisplay().getSelectionModel().setSelected(value, state);
-//                    event.setCanceled(true);
-//                }
-//            }
-//        });
-//
-//        showColumn.setFieldUpdater(new FieldUpdater<BookmarkInfo, Boolean>() {
-//            /**
-//             * TODO: emulate . . underBookmarkList . . Create an external function in Annotrack to then call from here
-//             * a good example: http://www.springindepth.com/book/gwt-comet-gwt-dojo-cometd-spring-bayeux-jetty.html
-//             * uses DOJO publish mechanism (http://dojotoolkit.org/reference-guide/1.7/dojo/publish.html)
-//
-//             * @param index
-//             * @param trackInfo
-//             * @param value
-//             */
-//            @Override
-//            public void update(int index, BookmarkInfo trackInfo, Boolean value) {
-//                JSONObject jsonObject = trackInfo.getPayload();
-//                trackInfo.setVisible(value);
-//                if (value) {
-//                    jsonObject.put("command", new JSONString("show"));
-//                } else {
-//                    jsonObject.put("command", new JSONString("hide"));
-//                }
-//
-//                MainPanel.executeFunction("handleBookmarkVisibility", jsonObject.getJavaScriptObject());
-//            }
-//        });
-//        showColumn.setSortable(true);
-
         TextColumn<BookmarkInfo> nameColumn = new TextColumn<BookmarkInfo>() {
             @Override
             public String getValue(BookmarkInfo track) {
@@ -172,20 +127,14 @@ public class BookmarkPanel extends Composite {
         nameColumn.setSortable(true);
 
 
-//        dataGrid.addColumn(showColumn, "Show");
         dataGrid.addColumn(nameColumn, "Name");
-//        dataGrid.setColumnWidth(0, "10%");
         dataGrid.setSelectionModel(selectionModel);
         selectionModel.addSelectionChangeHandler(new SelectionChangeEvent.Handler() {
             @Override
             public void onSelectionChange(SelectionChangeEvent event) {
                 setBookmarkInfo(selectionModel.getSelectedSet());
-//                if (!trackSelectionFix) setBookmarkInfo(selectionModel.getSelectedSet());
-//                trackSelectionFix = true;
             }
         });
-
-        reload();
 
         dataProvider.addDataDisplay(dataGrid);
 
@@ -193,18 +142,6 @@ public class BookmarkPanel extends Composite {
         ColumnSortEvent.ListHandler<BookmarkInfo> sortHandler = new ColumnSortEvent.ListHandler<BookmarkInfo>(bookmarkInfoList);
         dataGrid.addColumnSortHandler(sortHandler);
 
-//        sortHandler.setComparator(showColumn, new Comparator<BookmarkInfo>() {
-//            @Override
-//            public int compare(BookmarkInfo o1, BookmarkInfo o2) {
-//                if (o1.getVisible() == o2.getVisible()) {
-//                    return 0;
-//                }  else if (o1.getVisible()) {
-//                    return 1;
-//                } else {
-//                    return -1;
-//                }
-//            }
-//        });
         sortHandler.setComparator(nameColumn, new Comparator<BookmarkInfo>() {
             @Override
             public int compare(BookmarkInfo o1, BookmarkInfo o2) {
@@ -218,7 +155,7 @@ public class BookmarkPanel extends Composite {
             public void onOrganismChanged(OrganismChangeEvent authenticationEvent) {
                 dataGrid.setLoadingIndicator(new Label("Loading..."));
                 dataGrid.setEmptyTableWidget(new Label("Loading..."));
-                bookmarkInfoList.clear();
+//                bookmarkInfoList.clear();
                 Scheduler.get().scheduleFixedDelay(new Scheduler.RepeatingCommand() {
                     @Override
                     public boolean execute() {
@@ -230,6 +167,54 @@ public class BookmarkPanel extends Composite {
             }
         });
 
+        stubBackingData(10);
+        reload();
+
+    }
+
+    @UiHandler("removeButton")
+    public void remove(ClickEvent clickEvent) {
+        bookmarkInfoList.removeAll(selectionModel.getSelectedSet());
+        dragAndDropPanel.clear();
+    }
+
+    @UiHandler("mergeButton")
+    public void merge(ClickEvent clickEvent) {
+        BookmarkInfo bookmarkInfo = new BookmarkInfo();
+        Set<BookmarkInfo> bookmarkInfoSet = selectionModel.getSelectedSet();
+        // merge rule 1 . . . take largest padding
+        // merge rule 2 . . . take exon -> transcript -> none
+        for (BookmarkInfo bookmarkInfo1 : bookmarkInfoSet) {
+            Integer padding = bookmarkInfo.getPadding();
+            String type = bookmarkInfo.getType();
+            bookmarkInfo.setPadding(padding == null || bookmarkInfo1.getPadding() > padding ? bookmarkInfo1.getPadding() : padding);
+            bookmarkInfo.setType(type == null ? bookmarkInfo1.getType() : type);
+
+            // combine the JSONArray now
+//            JSONArray jsonArray = bookmarkInfo.getSequenceList();
+//            if(jsonArray==null){
+//                bookmarkInfo.setSequenceList(jsonArray);
+//            }
+//            else{
+            JSONArray sequence1 = bookmarkInfo.getSequenceList();
+            JSONArray sequence2 = bookmarkInfo1.getSequenceList();
+            if(sequence1==null){
+                sequence1 = sequence2 ;
+            }
+            else{
+                // add all fo the elements between 1 and 2 and put back into 1
+                for (int i = 0; i < sequence2.size(); i++) {
+                    sequence1.set(sequence1.size(), sequence2.get(i));
+                }
+            }
+
+            bookmarkInfo.setSequenceList(sequence1);
+//            }
+
+        }
+        bookmarkInfoList.removeAll(bookmarkInfoSet);
+        bookmarkInfoList.add(bookmarkInfo);
+//        reload();
     }
 
     private void setBookmarkInfo(Set<BookmarkInfo> selectedObject) {
@@ -263,111 +248,46 @@ public class BookmarkPanel extends Composite {
 
         dragAndDropPanel.clear();
 
-//        AbsolutePanel boundaryPanel = new AbsolutePanel();
-//        boundaryPanel.addStyleName(CSS_DEMO_MAIN_BOUNDARY_PANEL);
-//        boundaryPanel.setPixelSize(900, 600);
 
-        PickupDragController dragController = new PickupDragController(absolutePanel,true);
-//        flowPanelDropTarget = new FlowPanel();
-//        flowPanelDropTarget.setWidth("400px");
-//        setWidget(flowPanelDropTarget);
-        FlowPanelDropController flowPanelDropController = new FlowPanelDropController(
-                dragAndDropPanel);
-        dragController.registerDropController(flowPanelDropController);
-
-        DropController pickupDropController = new FlowPanelDropController(dragAndDropPanel);
+//        DropController pickupDropController = new FlowPanelDropController(dragAndDropPanel);
         for (BookmarkInfo bookmarkInfo : selectedObject) {
-            FocusPanel focusPanel = new FocusPanel();
-//                focusPanel.setStyleName(CSS_DEMO_FLOW_PANEL_EXAMPLE_DRAGGABLE);
-
-            FlowPanel flowPanel = new FlowPanel();
-            focusPanel.setStyleName("demo-FlowPanelExample-draggable");
-            focusPanel.add(flowPanel);
 
 //            HTML label = new HTML("Draggable&nbsp;#" + i);
-            HTML label = new HTML(bookmarkInfo.getName());
-            label.setStyleName("demo-FlowPanelExample-label");
-            HTML spacer = new HTML(" ");
+            JSONArray sequenceArray = bookmarkInfo.getSequenceList();
+            for(int i = 0 ; i < sequenceArray.size() ; i++){
+                JSONObject sequenceObject = sequenceArray.get(i).isObject();
+                String name = sequenceObject.get(FeatureStringEnum.NAME.getValue()).isString().stringValue();
+                if(sequenceObject.containsKey(FeatureStringEnum.FEATURES.getValue())){
+                    JSONArray featureArray = sequenceObject.get(FeatureStringEnum.FEATURES.getValue()).isArray();
+                    for(int j = 0 ; j < featureArray.size() ; j++){
+                        name += "("+featureArray.get(j).isObject().get(FeatureStringEnum.NAME.getValue()).isString().stringValue()+")";
+                    }
+                }
+                FocusPanel focusPanel = new FocusPanel();
+//                focusPanel.setStyleName(CSS_DEMO_FLOW_PANEL_EXAMPLE_DRAGGABLE);
+
+                FlowPanel flowPanel = new FlowPanel();
+                focusPanel.setStyleName("demo-FlowPanelExample-draggable");
+                focusPanel.add(flowPanel);
+
+//                HTML label = new HTML(bookmarkInfo.getName());
+                HTML label = new HTML(name);
+                label.setStyleName("demo-FlowPanelExample-label");
+                HTML spacer = new HTML(" ");
 //                label.addStyleName(CSS_DEMO_FLOW_PANEL_EXAMPLE_LABEL);
-            flowPanel.add(label);
-            flowPanel.add(spacer);
-            dragController.makeDraggable(focusPanel);
-            dragAndDropPanel.add(focusPanel);
-        }
+                flowPanel.add(label);
+                flowPanel.add(spacer);
 
-    }
-
-    private void setOptionDetails(JSONObject jsonObject) {
-        for (String key : jsonObject.keySet()) {
-            TreeItem treeItem = new TreeItem();
-            treeItem.setHTML(generateHtmlFromObject(jsonObject, key));
-            if (jsonObject.get(key).isObject() != null) {
-                treeItem.addItem(generateTreeItem(jsonObject.get(key).isObject()));
-            }
-//            optionTree.addItem(treeItem);
-        }
-    }
-
-    private String generateHtmlFromObject(JSONObject jsonObject, String key) {
-        if (jsonObject.get(key) == null) {
-            return key;
-        } else if (jsonObject.get(key).isObject() != null) {
-            return key;
-        } else {
-            return "<b>" + key + "</b>: " + jsonObject.get(key).toString().replace("\\", "");
-        }
-    }
-
-    private TreeItem generateTreeItem(JSONObject jsonObject) {
-        TreeItem treeItem = new TreeItem();
-
-        for (String key : jsonObject.keySet()) {
-            treeItem.setHTML(generateHtmlFromObject(jsonObject, key));
-            if (jsonObject.get(key).isObject() != null) {
-                treeItem.addItem(generateTreeItem(jsonObject.get(key).isObject()));
+                dragController.makeDraggable(focusPanel);
+                dragAndDropPanel.add(focusPanel);
             }
         }
 
-
-        return treeItem;
     }
 
-
-//    @UiHandler("nameSearchBox")
-//    public void doSearch(KeyUpEvent keyUpEvent) {
-//
-//        filterList();
-//    }
-
-    static void filterList() {
-//        String text = nameSearchBox.getText();
-//        filteredBookmarkInfoList.clear();
-//        for (BookmarkInfo trackInfo : trackInfoList) {
-//            if (trackInfo.getName().toLowerCase().contains(text.toLowerCase()) &&
-//                    trackInfo.getName().toLowerCase().contains(text.toLowerCase()) &&
-//                    !isReferenceSequence(trackInfo) &&
-//                    !isAnnotationBookmark(trackInfo)) {
-//                filteredBookmarkInfoList.add(trackInfo);
-//            }
-//        }
-    }
-
-    private static boolean isAnnotationBookmark(BookmarkInfo trackInfo) {
-        return trackInfo.getName().equalsIgnoreCase("User-created Annotations");
-    }
-
-    private static boolean isReferenceSequence(BookmarkInfo trackInfo) {
-        return trackInfo.getName().equalsIgnoreCase("Reference sequence");
-    }
-
-//    public static native void exportStaticMethod() /*-{
-//        $wnd.loadBookmarks = $entry(@org.bbop.apollo.gwt.client.BookmarkPanel::updateBookmarks(Ljava/lang/String;));
-//    }-*/;
-
-    public void reload() {
-
-        bookmarkInfoList.clear();
-        for (int i = 0; i < 50; i++) {
+    private void stubBackingData(int number){
+//        bookmarkInfoList.clear();
+        for (int i = 0; i < number; i++) {
             BookmarkInfo bookmarkInfo = new BookmarkInfo();
             JSONArray jsonArray = new JSONArray();
             JSONObject sequenceObject = new JSONObject();
@@ -388,7 +308,12 @@ public class BookmarkPanel extends Composite {
 
             bookmarkInfoList.add(bookmarkInfo);
         }
+        reload();
+    }
 
+    public void reload() {
+        dataGrid.setVisibleRangeAndClearData(dataGrid.getVisibleRange(), true);
+        dataGrid.redraw();
     }
 
 }
