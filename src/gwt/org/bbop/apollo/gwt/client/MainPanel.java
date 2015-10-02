000ef0985ab17283cd2821d110449fdd1a85bcf1
Nathan Dunn <ndunn@me.com>
2015-10-02 11:45:26 -0700

    #525 - fixed compilation error

--- src/gwt/org/bbop/apollo/gwt/client/MainPanel.java
+++ src/gwt/org/bbop/apollo/gwt/client/MainPanel.java
@@ -18,7 +18,6 @@ import com.google.gwt.user.client.Command;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.*;
 import com.google.gwt.user.client.ui.ListBox;
-import org.bbop.apollo.UserBookmark;
 import org.bbop.apollo.gwt.client.dto.*;
 import org.bbop.apollo.gwt.client.dto.bookmark.BookmarkInfo;
 import org.bbop.apollo.gwt.client.event.*;
