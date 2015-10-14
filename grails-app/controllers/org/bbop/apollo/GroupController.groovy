f0c4f863ee300e45a5459e97de3cfd9655b4b2b3
Nathan Dunn <ndunn@me.com>
2015-10-14 13:45:48 -0700

    altered user group inclusion with user email now

--- grails-app/controllers/org/bbop/apollo/GroupController.groovy
+++ grails-app/controllers/org/bbop/apollo/GroupController.groovy
@@ -256,12 +256,10 @@ class GroupController {
         UserGroup group
         if(dataObject.groupId){
             group = UserGroup.findById(dataObject.groupId)
-            println "have a group ID . . . ${group}"
         }
 
         if(!group){
             group = UserGroup.findByName(dataObject.name)
-            println "have a group name ${dataObject.name} -> . . . ${group}"
         }
         if(!group){
             JSONObject jsonObject = new JSONObject()
