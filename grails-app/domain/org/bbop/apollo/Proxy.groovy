23081e40194f4bd8de096e5eea628e010383522f
Nathan Dunn <ndunn@me.com>
2015-10-17 11:35:03 -0700

    stubbed in code need for fallbacks

--- grails-app/domain/org/bbop/apollo/Proxy.groovy
+++ grails-app/domain/org/bbop/apollo/Proxy.groovy
@@ -3,8 +3,19 @@ package org.bbop.apollo
 class Proxy {
 
     static constraints = {
+        order nullable: true
+        lastSuccess nullable: true
+        lastFail nullable: true
+    }
+
+    static mapping= {
+        active defaultValue: true
     }
 
     String referenceUrl
     String targetUrl
+    Boolean active
+    Integer order
+    Date lastSuccess
+    Date lastFail
 }
