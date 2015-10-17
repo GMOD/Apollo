e616384f2ae1b631ccfdedac459dd300842c3b88
Colin <colin.diesh@gmail.com>
2015-10-16 23:11:52 -0500

    Update the markdown parser for readthedocs

--- docs/conf.py
+++ docs/conf.py
@@ -264,6 +264,6 @@ from recommonmark.parser import CommonMarkParser
 # The suffix of source filenames.
 source_suffix = ['.rst', '.md']
 
-parsers = {
+source_parsers = {
     '.md': CommonMarkParser,
 }
