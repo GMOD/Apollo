d64cd8b7abda062064ed92ae8c74ba1c678bea11
Colin <colin.diesh@gmail.com>
2015-10-20 15:11:09 -0500

    Revert "reverted patch"
    This reverts commit 38c2c66223103a1d35ad161f3165081df5647d1b.
    
    Conflicts:
    	src/gwt/org/bbop/apollo/gwt/client/LoginDialog.java

--- src/gwt/org/bbop/apollo/gwt/client/LoginDialog.java
+++ src/gwt/org/bbop/apollo/gwt/client/LoginDialog.java
@@ -1,109 +1,57 @@
 package org.bbop.apollo.gwt.client;
-
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.uibinder.client.UiHandler;
 import com.google.gwt.core.client.Scheduler;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.event.dom.client.KeyCodes;
 import com.google.gwt.dom.client.NativeEvent;
 import com.google.gwt.user.client.Event.*;
-import com.google.gwt.user.client.ui.*;
-import org.bbop.apollo.gwt.client.rest.UserRestService;
-import org.gwtbootstrap3.client.ui.*;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.user.client.ui.DialogBox;
+import com.google.gwt.user.client.ui.Widget;
+import com.google.gwt.uibinder.client.UiBinder;
+import org.gwtbootstrap3.client.ui.Button;
+import org.gwtbootstrap3.client.ui.Input;
+import org.gwtbootstrap3.client.ui.Icon;
+import org.gwtbootstrap3.client.ui.CheckBox;
+import org.gwtbootstrap3.client.ui.html.Paragraph;
+import org.gwtbootstrap3.client.ui.TextBox;
 import org.gwtbootstrap3.client.ui.constants.*;
+import org.bbop.apollo.gwt.client.rest.UserRestService;
 
-/**
- * Created by ndunn on 3/17/15.
- */
-//  TODO: this needs to be moved into UIBinder into its own class
 public class LoginDialog extends DialogBox {
+    private static final Binder binder = GWT.create(Binder.class);
+    interface Binder extends UiBinder<Widget, LoginDialog> {
+    }
 
-    // TODO: move to UIBinder
-    private VerticalPanel panel = new VerticalPanel();
-    private org.gwtbootstrap3.client.ui.Button okButton = new org.gwtbootstrap3.client.ui.Button("Login");
-    private org.gwtbootstrap3.client.ui.TextBox username = new org.gwtbootstrap3.client.ui.TextBox();
-    private Input passwordTextBox = new Input();
-    private HorizontalPanel horizontalPanel = new HorizontalPanel();
-    private org.gwtbootstrap3.client.ui.CheckBox rememberMeCheckBox = new org.gwtbootstrap3.client.ui.CheckBox("Remember me");
-    private Heading errorHtml = new Heading(HeadingSize.H4);
+
+    @UiField
+    Paragraph errorHtml;
+
+
+
+    @UiField
+    Button loginButton;
+
+    @UiField
+    TextBox userBox;
+
+    @UiField
+    Input passBox;
+
+    @UiField
+    CheckBox rememberBox;
+
 
 
     public LoginDialog() {
-        // Set the dialog box's caption.
         setText("Login");
-        // Enable animation.
         setAnimationEnabled(true);
         // Enable glass background.
         setGlassEnabled(true);
-
-        okButton.setType(ButtonType.PRIMARY);
-        errorHtml.setEmphasis(Emphasis.DANGER);
-//        errorHtml.setEmphasis(Emphasis.DANGER);
-//        errorHtml.setSi(Emphasis.DANGER);
-//        errorHtml.setContextualBackground(ContextualBackground.DANGER);
-        passwordTextBox.setType(InputType.PASSWORD);
-        Icon icon = new Icon(IconType.WARNING);
-        errorHtml.add(icon);
-
-        Form form = new Form();
-        FieldSet fieldSet = new FieldSet();
-        form.add(fieldSet);
-
-        FormGroup usernameFormGroup = new FormGroup();
-        fieldSet.add(usernameFormGroup);
-        FormLabel usernameLabel = new FormLabel();
-        usernameLabel.setText("Username");
-        usernameFormGroup.add(usernameLabel);
-        usernameFormGroup.add(username);
-        fieldSet.add(usernameFormGroup);
-
-        FormGroup passwordFormGroup = new FormGroup();
-        fieldSet.add(passwordFormGroup);
-        FormLabel passwordLabel = new FormLabel();
-        passwordLabel.setText("Password");
-        passwordFormGroup.add(passwordLabel);
-        passwordFormGroup.add(passwordTextBox);
-
-//        grid.setHTML(0, 0, "Username");
-//        grid.setWidget(0, 1, username);
-//        grid.setHTML(1, 0, "Password");
-//        grid.setWidget(1, 1, passwordTextBox);
-//        panel.add(grid);
-        panel.add(form);
-
-        Scheduler.get().scheduleDeferred(new Scheduler.ScheduledCommand() {
-            @Override
-            public void execute() {
-                username.setFocus(true);
-            }
-        });
-
-        horizontalPanel.add(rememberMeCheckBox);
-        horizontalPanel.add(new HTML("&nbsp;&nbsp;&nbsp;&nbsp;"));
-        horizontalPanel.add(okButton);
-        panel.add(horizontalPanel);
-        panel.add(errorHtml);
-        // DialogBox is a SimplePanel, so you have to set its widget property to
-        // whatever you want its contents to be.
-        okButton.addClickHandler(new ClickHandler() {
-            @Override
-            public void onClick(ClickEvent event) {
-                doLogin(username.getText().trim(),passwordTextBox.getText(),rememberMeCheckBox.getValue());
-            }
-        });
-        setWidget(panel);
-    }
-    @Override
-    public void onPreviewNativeEvent(NativePreviewEvent e) {
-        NativeEvent nativeEvent = e.getNativeEvent();
-        if ("keydown".equals(nativeEvent.getType())) {
-            if (nativeEvent.getKeyCode() == KeyCodes.KEY_ENTER) {
-                doLogin(username.getText().trim(),passwordTextBox.getText(),rememberMeCheckBox.getValue());
-            }
-        }
-    }
-
-    public void doLogin(String username,String password,Boolean rememberMe){
-        UserRestService.login(username, password,rememberMe,this);
+        setWidget(binder.createAndBindUi(this));
     }
 
     public void showLogin() {
@@ -113,14 +61,34 @@ public class LoginDialog extends DialogBox {
     }
 
     public void setError(String errorMessage){
+        Icon icon = new Icon(IconType.WARNING);
+        errorHtml.add(icon);
+        errorHtml.setEmphasis(Emphasis.DANGER);
         errorHtml.setText(errorMessage);
         errorHtml.setVisible(true);
     }
-
+ 
     public void clearErrors(){
         errorHtml.setText("");
         errorHtml.setVisible(false);
     }
 
-}
+    @Override
+    public void onPreviewNativeEvent(NativePreviewEvent e) {
+        NativeEvent nativeEvent = e.getNativeEvent();
+        if ("keydown".equals(nativeEvent.getType())) {
+            if (nativeEvent.getKeyCode() == KeyCodes.KEY_ENTER) {
+                doLogin(userBox.getText().trim(), passBox.getText(), rememberBox.getValue());
+            }
+        }
+    }
 
+    @UiHandler("loginButton")
+    public void submitAction(ClickEvent e) {
+        doLogin(userBox.getText().trim(),passBox.getText(),rememberBox.getValue());
+    }
+
+    public void doLogin(String username,String password,Boolean rememberMe){
+        UserRestService.login(username, password,rememberMe,this);
+    }
+}
