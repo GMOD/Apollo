604b53a31978edeed7bc030548642468f52a2a71
Colin <colin.diesh@gmail.com>
2015-11-06 17:24:04 -0600

    Constrain security on organism controller

--- grails-app/conf/org/bbop/apollo/SecurityFilters.groovy
+++ grails-app/conf/org/bbop/apollo/SecurityFilters.groovy
@@ -10,28 +10,20 @@ import grails.converters.JSON
 class SecurityFilters {
     def filters = {
 
-        all(uri: "/**") {
+        all(controller: 'organism', action: '*') {
             before = {
                 log.debug "apollo filter ${controllerName}::${actionName}"
                 if (!controllerName) return true
-                if(controllerName=="jbrowse") return true
-                if(controllerName=="assets") return true
-                if(controllerName=="organism"&&actionName=="chooseOrganismForJbrowse") return true
-                if(controllerName=="annotator" && actionName=="index") return true
-                if(controllerName=="user" && actionName=="checkLogin") return true
-                if(controllerName!="login") {
-                    log.debug "here"
-                    Subject subject = SecurityUtils.getSubject();
-                    if (!subject.isAuthenticated()) {
-                        def req = request.JSON
-                        if (req.username && req.password) {
-                            def authToken = new UsernamePasswordToken(req.username, req.password)
-                            authenticate(authToken)
-                        } else {
-                            log.warn "username/password not submitted"
-                            render text: ([error: "Not authorized"] as JSON), status: HttpStatus.UNAUTHORIZED
-                            return false
-                        }
+                Subject subject = SecurityUtils.getSubject();
+                if (!subject.isAuthenticated()) {
+                    def req = request.JSON
+                    if (req.username && req.password) {
+                        def authToken = new UsernamePasswordToken(req.username, req.password)
+                        authenticate(authToken)
+                    } else {
+                        log.warn "username/password not submitted"
+                        render text: ([error: "Not authorized"] as JSON), status: HttpStatus.UNAUTHORIZED
+                        return false
                     }
                 }
             }
