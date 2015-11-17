fc9b452cb4c084d68aa087acc9b6620fa17ae770
Colin <colin.diesh@gmail.com>
2015-11-17 16:35:37 -0600

    Use a faster sequencequerying mechanism

--- grails-app/services/org/bbop/apollo/SequenceService.groovy
+++ grails-app/services/org/bbop/apollo/SequenceService.groovy
@@ -26,6 +26,15 @@ class SequenceService {
     def cdsService
     def gff3HandlerService
     def overlapperService
+    def sessionFactory
+
+    def cleanUpGorm() {
+        def session = sessionFactory.currentSession
+        session.flush()
+        session.clear()
+   //     propertyInstanceMap.get().clear()
+    }
+
 
     List<FeatureLocation> getFeatureLocations(Sequence sequence){
         FeatureLocation.findAllBySequence(sequence)
@@ -204,9 +213,23 @@ class SequenceService {
         int startChunkNumber = fmin / sequence.seqChunkSize;
         int endChunkNumber = (fmax - 1 ) / sequence.seqChunkSize;
 
-        for(int i = startChunkNumber ; i<= endChunkNumber ; i++){
-            SequenceChunk sequenceChunk = getSequenceChunkForChunk(sequence,i)
-            sequenceString.append(sequenceChunk.residue)
+        def c=SequenceChunk.createCriteria()
+        def chunks=c.list() {
+            eq('sequence',sequence)
+            'in'('chunkNumber',[startChunkNumber,endChunkNumber])
+        }
+        log.debug("${chunks.size()} ${endChunkNumber-startChunkNumber+1}")
+        if(chunks.size()==endChunkNumber-startChunkNumber+1) {
+            chunks.each {
+                sequenceString.append(it.residue)
+            }
+        }
+        else {
+            for(int i = startChunkNumber ; i<= endChunkNumber ; i++){
+                SequenceChunk sequenceChunk = getSequenceChunkForChunk(sequence,i)
+                sequenceString.append(sequenceChunk.residue)
+            }
+            cleanUpGorm()
         }
 
 
