6d32a3d0c4335c7a60f6a635850f492156861a0a
Colin <colin.diesh@gmail.com>
2015-11-19 12:32:01 -0600

    Revert cleanUpGorm

--- grails-app/services/org/bbop/apollo/SequenceService.groovy
+++ grails-app/services/org/bbop/apollo/SequenceService.groovy
@@ -26,14 +26,7 @@ class SequenceService {
     def cdsService
     def gff3HandlerService
     def overlapperService
-    def sessionFactory
 
-    def cleanUpGorm() {
-        def session = sessionFactory.currentSession
-        session.flush()
-        session.clear()
-   //     propertyInstanceMap.get().clear()
-    }
 
 
     List<FeatureLocation> getFeatureLocations(Sequence sequence){
@@ -229,7 +222,6 @@ class SequenceService {
                 SequenceChunk sequenceChunk = getSequenceChunkForChunk(sequence,i)
                 sequenceString.append(sequenceChunk.residue)
             }
-            cleanUpGorm()
         }
 
 
