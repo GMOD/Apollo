908ea2adabcb090c7e10a813d893b09fdd0421b5
Nathan Dunn <ndunn@me.com>
2015-11-19 11:33:15 -0800

    fixed golr url

--- client/apollo/js/View/Track/AnnotTrack.js
+++ client/apollo/js/View/Track/AnnotTrack.js
@@ -2814,8 +2814,8 @@ var AnnotTrack = declare( DraggableFeatureTrack,
                                         }
                                     });
                                     // replaced with valid GOLR URL in build.xml file during copy process #148
-                                    //var gserv = 'http://golr.berkeleybop.org/';
-                                    var gserv = 'http://golr.geneontology.org/solr/';
+                                    var gserv = 'http://golr.berkeleybop.org/solr/';
+                                    //var gserv = 'http://golr.geneontology.org/solr/';
                                     //var gserv = 'http://a2-proxy2.stanford.edu:8080/solr/';
                                     //var gserv = 'http://a2-proxy1.stanford.edu:8080/solr/';
 
