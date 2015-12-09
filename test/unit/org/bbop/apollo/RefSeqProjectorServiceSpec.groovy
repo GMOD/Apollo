aeaced1ec4ebedd9bf91b76181589b0710cf2db2
Nathan Dunn <ndunn@me.com>
2015-12-08 18:04:13 -0800

    #680 - added sequence service

---
+++ test/unit/org/bbop/apollo/RefSeqProjectorServiceSpec.groovy
@@ -0,0 +1,31 @@
+package org.bbop.apollo
+
+import grails.test.mixin.TestFor
+import spock.lang.Specification
+
+/**
+ * See the API for {@link grails.test.mixin.services.ServiceUnitTestMixin} for usage instructions
+ */
+@TestFor(RefSeqProjectorService)
+class RefSeqProjectorServiceSpec extends Specification {
+
+    def refSeqProjectorService
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "get unprojected single"() {
+    }
+
+    void "get projected single"() {
+    }
+
+    void "get unprojected contiguous"() {
+    }
+
+    void "get projected contiguous"() {
+    }
+}
