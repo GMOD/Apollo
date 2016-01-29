69b674c969bacb3db0622de721ffd418c65a5a70
Colin <colin.diesh@gmail.com>
2016-01-29 15:59:56 -0600

    Add exportSubFeaturesAttr

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -205,6 +205,7 @@ apollo {
     feature_has_go_ids = true
     feature_has_comments = true
     feature_has_status = true
+    export_subfeatures_attr = false
     user_pure_memory_store = true
     is_partial_translation_allowed = false // unused so far
     // this is the default
