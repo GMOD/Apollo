db6065869287191c74f08933eee0c946f352a9bd
Colin <colin.diesh@gmail.com>
2016-01-29 14:39:22 -0600

    Add a pseudogene to the gff3 integration spec

--- test/integration/org/bbop/apollo/Gff3HandlerServiceIntegrationSpec.groovy
+++ test/integration/org/bbop/apollo/Gff3HandlerServiceIntegrationSpec.groovy
@@ -31,18 +31,22 @@ class Gff3HandlerServiceIntegrationSpec extends IntegrationSpec {
         given: "we create a new gene"
         String json=' { "track": "Group1.10", "features": [{"location":{"fmin":1216824,"fmax":1235616,"strand":1},"type":{"cv":{"name":"sequence"},"name":"mRNA"},"name":"GB40856-RA","children":[{"location":{"fmin":1235534,"fmax":1235616,"strand":1},"type":{"cv":{"name":"sequence"},"name":"exon"}},{"location":{"fmin":1216824,"fmax":1216850,"strand":1},"type":{"cv":{"name":"sequence"},"name":"exon"}},{"location":{"fmin":1224676,"fmax":1224823,"strand":1},"type":{"cv":{"name":"sequence"},"name":"exon"}},{"location":{"fmin":1228682,"fmax":1228825,"strand":1},"type":{"cv":{"name":"sequence"},"name":"exon"}},{"location":{"fmin":1235237,"fmax":1235396,"strand":1},"type":{"cv":{"name":"sequence"},"name":"exon"}},{"location":{"fmin":1235487,"fmax":1235616,"strand":1},"type":{"cv":{"name":"sequence"},"name":"exon"}},{"location":{"fmin":1216824,"fmax":1235534,"strand":1},"type":{"cv":{"name":"sequence"},"name":"CDS"}}]}], "operation": "add_transcript" }'
         String addInsertionString = '{"operation":"add_sequence_alteration","username":"deepak.unni3@gmail.com","features":[{"non_reserved_properties": [{"tag": "justification", "value":"Sanger sequencing"}], "residues":"GGG","location":{"fmin":208499,"strand":1,"fmax":208499},"type":{"name":"insertion","cv":{"name":"sequence"}}}],"track":"Group1.10"}'
-        
-        
+        String pseudogene = '{ "track": "Group1.10", "features": [{"location":{"fmin":433518,"fmax":437436,"strand":1},"type":{"cv":{"name":"sequence"},"name":"pseudogene"},"children":[{"location":{"fmin":433518,"fmax":437436,"strand":1},"type":{"cv":{"name":"sequence"},"name":"transcript"},"name":"GB40815-RA","children":[{"location":{"fmin":433518,"fmax":433570,"strand":1},"type":{"cv":{"name":"sequence"},"name":"exon"}},{"location":{"fmin":436576,"fmax":436641,"strand":1},"type":{"cv":{"name":"sequence"},"name":"exon"}},{"location":{"fmin":437424,"fmax":437436,"strand":1},"type":{"cv":{"name":"sequence"},"name":"exon"}},{"location":{"fmin":433518,"fmax":437436,"strand":1},"type":{"cv":{"name":"sequence"},"name":"CDS"}}]}]}], "operation": "add_feature", "username": "colin.diesh@gmail.com" }'
+       
         when: "we parse the json"
         requestHandlingService.addTranscript(JSON.parse(json))
         requestHandlingService.addSequenceAlteration(JSON.parse(addInsertionString))
+        requestHandlingService.addFeature(JSON.parse(pseudogene))
         
 
         then: "We should have at least one new gene"
-        assert Gene.count == 1
+
+        log.debug "${Gene.findAll()}"
+        assert Gene.count == 2
         assert MRNA.count == 1
-        assert Exon.count == 5
-        assert CDS.count == 1
+        assert Pseudogene.count == 1
+        assert Exon.count == 8
+        assert CDS.count == 2
 
 
         when: "we write the feature to test"
@@ -62,8 +66,9 @@ class Gff3HandlerServiceIntegrationSpec extends IntegrationSpec {
         assert lines[2].split("\t")[2]=="gene"
         assert lines[2].split("\t")[8].indexOf("Name=GB40856-RA")!=-1
         assert lines[3].split("\t")[2]=="mRNA"
-        assert lines[15].split("\t")[2]=="insertion"
-        assert lines[15].split("\t")[8].indexOf("justification=Sanger sequencing")!=-1
+        assert lines[15].split("\t")[2]=="pseudogene"
+        assert lines[21].split("\t")[2]=="insertion"
+        assert lines[21].split("\t")[8].indexOf("justification=Sanger sequencing")!=-1
         assert tempFileText.length() > 0
     }
 }
