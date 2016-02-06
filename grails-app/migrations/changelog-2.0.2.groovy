bb801b70e03cf30aec82fd65a5c5084373c59132
Nathan Dunn <ndunn@me.com>
2016-02-05 17:16:59 -0800

    fixed log

--- grails-app/migrations/changelog-2.0.2.groovy
+++ grails-app/migrations/changelog-2.0.2.groovy
@@ -2,11 +2,11 @@ databaseChangeLog = {
 	changeSet(author: "cmdcolin (generated)", id: "1454711582784-1") {
         preConditions(onFail: 'MARK_RAN', onError: "HALT") {
             not {
-                indexExists("indexName","feature_uniqueName",tableName: "feature_event")
+                indexExists(indexName:"feature_uniqueName")
             }
-            createIndex(indexName: "feature_uniqueName", tableName: "feature_event", unique: "false") {
-                column(name: "unique_name")
-            }
+        }
+        createIndex(indexName: "feature_uniqueName", tableName: "feature_event", unique: "false") {
+            column(name: "unique_name")
         }
     }
 }
