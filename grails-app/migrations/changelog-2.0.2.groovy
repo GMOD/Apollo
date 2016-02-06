e90f344530957d813fcb774ab642f4597870282b
Nathan Dunn <ndunn@me.com>
2016-02-05 16:32:09 -0800

    added preconditions for this table

--- grails-app/migrations/changelog-2.0.2.groovy
+++ grails-app/migrations/changelog-2.0.2.groovy
@@ -1,7 +1,12 @@
 databaseChangeLog = {
 	changeSet(author: "cmdcolin (generated)", id: "1454711582784-1") {
-        createIndex( indexName: "feature_event_s123", tableName: "feature_event", unique: "false" ) {
-            column( name: "unique_name" )
+        preConditions(onFail: 'MARK_RAN', onError: "HALT") {
+            not {
+                indexExists("indexName","feature_uniqueName",tableName: "feature_event")
+            }
+            createIndex(indexName: "feature_uniqueName", tableName: "feature_event", unique: "false") {
+                column(name: "unique_name")
+            }
         }
     }
 }
