43bb8d6f2bdb5318f8c144cd5ff7b654ef7d247b
Colin <colin.diesh@gmail.com>
2016-02-15 12:47:32 -0600

    Add tRNA back to list of WebApolloCanvasFeatures. See #868

--- client/apollo/js/View/Track/WebApolloCanvasFeatures.js
+++ client/apollo/js/View/Track/WebApolloCanvasFeatures.js
@@ -8,8 +8,8 @@ define( [
             'dijit/MenuSeparator',
             'dijit/PopupMenuItem',
             'dijit/Dialog',
-            'JBrowse/Util', 
-            'JBrowse/Model/SimpleFeature', 
+            'JBrowse/Util',
+            'JBrowse/Model/SimpleFeature',
             'WebApollo/SequenceOntologyUtils'
         ],
         function( declare,
@@ -17,12 +17,12 @@ define( [
             CanvasFeaturesTrack,
             FeatureSelectionManager,
             dijitMenu,
-            dijitMenuItem, 
+            dijitMenuItem,
             dijitCheckedMenuItem,
             dijitMenuSeparator,
             dijitPopupMenuItem,
             dijitDialog,
-            Util, 
+            Util,
             SimpleFeature,
             SeqOnto )
 {
@@ -62,35 +62,42 @@ return declare( CanvasFeaturesTrack,
                      var atrack=thisB.webapollo.getAnnotTrack();
                      atrack.createGenericAnnotations([this.feature], "repeat_region", null, "gene");
                    }
-                },               
+                },
                 {
                   "label" : "transposable element",
                   "action" : function() {
                      var atrack=thisB.webapollo.getAnnotTrack();
                      atrack.createGenericAnnotations([this.feature], "transposable_element", null, "gene");
                    }
-                },               
+                },
+                {
+                  "label" : "tRNA",
+                  "action" : function() {
+                     var atrack=thisB.webapollo.getAnnotTrack();
+                     atrack.createGenericAnnotations([this.feature], "tRNA", null, "gene");
+                   }
+                },
                 {
                   "label" : "snRNA",
                   "action" : function() {
                      var atrack=thisB.webapollo.getAnnotTrack();
                      atrack.createGenericAnnotations([this.feature], "snRNA", null, "gene");
                    }
-                },               
+                },
                 {
                   "label" : "miRNA",
                   "action" : function() {
                      var atrack=thisB.webapollo.getAnnotTrack();
                      atrack.createGenericAnnotations([this.feature], "miRNA", null, "gene");
                    }
-                },               
+                },
                 {
                   "label" : "rRNA",
                   "action" : function() {
                      var atrack=thisB.webapollo.getAnnotTrack();
                      atrack.createGenericAnnotations([this.feature], "rRNA", null, "gene");
                    }
-                },               
+                },
                 {
                   "label" : "snoRNA",
                   "action" : function() {
@@ -103,7 +110,7 @@ return declare( CanvasFeaturesTrack,
         );
         return config;
     }
-                    
+
 
 });
 
