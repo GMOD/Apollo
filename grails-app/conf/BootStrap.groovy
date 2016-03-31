218ea7c29da397a90a71a288222faab8f7b35152
Deepak Unni <deepak.unni3@gmail.com>
2016-03-31 16:10:13 -0500

    Remove unwanted import statements from Bootstrap.groovy

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,17 +1,8 @@
-import grails.converters.JSON
 import grails.util.Environment
 import org.bbop.apollo.FeatureType
-import org.bbop.apollo.Gene
-import org.bbop.apollo.MRNA
-import org.bbop.apollo.SnRNA
-import org.bbop.apollo.Organism
-import org.bbop.apollo.Pseudogene
 import org.bbop.apollo.Role
-import org.bbop.apollo.Transcript
-import org.bbop.apollo.User
 import org.bbop.apollo.UserService
 import org.bbop.apollo.sequence.SequenceTranslationHandler
-import org.codehaus.groovy.grails.commons.GrailsApplication
 
 class BootStrap {
 
