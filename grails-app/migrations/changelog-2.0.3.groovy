79c71bcaeeb5d5f27e7464d7e7e1085f196a9204
Deepak Unni <deepak.unni3@gmail.com>
2016-04-04 12:01:36 -0500

    create a changelog for database migration

---
+++ grails-app/migrations/changelog-2.0.3.groovy
@@ -0,0 +1,7 @@
+databaseChangeLog = {
+
+	changeSet(author: "deepak.unni3 (generated)", id: "1459788030174-8") {
+		modifyDataType(columnName: "value", newDataType: "text", tableName: "feature_property")
+	}
+
+}
