b47b79b1d9d6f715f76bac2226ccfce323438023
Nathan Dunn <ndunn@me.com>
2016-04-08 14:14:02 -0700

    added stubbed out fill blocks

--- client/apollo/js/View/Track/LegendTrack.js
+++ client/apollo/js/View/Track/LegendTrack.js
@@ -275,6 +275,46 @@ define([
 
                 },
 
+                //fillBlock: function( args ) {
+                //    var blockIndex = args.blockIndex;
+                //    var block = args.block;
+                //    var leftBase = args.leftBase;
+                //    var scale = args.scale;
+                //    var thisB = this;
+                //
+                //    // find the number that is within 2 px of the left boundary of
+                //    // the block that ends with the most zeroes, or a 5 if no
+                //    // zeroes
+                //    var labelNumber = this.chooseLabel( args );
+                //    var labelOffset = (leftBase+1-labelNumber)*scale/10;
+                //    // console.log( leftBase+1, labelNumber, labelOffset );
+                //
+                //    var posLabel = document.createElement("div");
+                //    var numtext = Util.addCommas( labelNumber );
+                //    posLabel.className = this.labelClass;
+                //
+                //    // give the position label a negative left offset in ex's to
+                //    // more-or-less center it over the left boundary of the block
+                //    posLabel.style.left = "-" + Number(numtext.length)/1.7 + labelOffset + "ex";
+                //
+                //    posLabel.appendChild( document.createTextNode( numtext ) );
+                //    block.domNode.appendChild(posLabel);
+                //
+                //    var highlight = this.browser.getHighlight();
+                //    if( highlight && highlight.ref == this.refSeq.name ) {
+                //        this.renderRegionHighlight( args, highlight );
+                //    }
+                //
+                //
+                //    var bookmarks = this.browser.getBookmarks();
+                //    if( bookmarks ) {
+                //        this.renderRegionBookmark( args, bookmarks, this.refSeq.name, true );
+                //    }
+                //
+                //    this.heightUpdate( Math.round( this.posHeight*1.2 ), blockIndex);
+                //    args.finishCallback();
+                //},
+
                 renderRegionRight: function(context, fRect){
                     var thisB = this;
                     // create svg element new
