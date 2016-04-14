4231d2a88c5581e1b85be2890f3340d11986e0dc
Nathan Dunn <nathandunn@users.noreply.github.com>
2016-04-14 13:28:48 -0700

    Update to 2 5 4 (#980)
    * Add fix for using organism=organismName
    
    * trying to get the 25 build
    
    * had to rename the migration scripts
    
    * removed AnnotTrack . . still getting a single error in relationship serfice
    
    * feature relationship delete seems to work
    
    * fixed non-canonical splite site update isseus
    
    * fixed the rest of the the flusing errors
    
    * remove transcript / gene
    
    * Add fix for using organism=organismName (#973)
    
    * added coveralls
    
    * added coveralls ;
    
    * added coveralls plugin
    
    * updated apollo / jbrowse plugin doc
    
    * added the login for non-annotator pages (#974)
    
    * trying to get the 25 build
    
    * had to rename the migration scripts
    
    * Fix setting translation startend intron (#976)
    
    * added the login for non-annotator pages
    
    * setting translation start or end in  intron gives more predictable behavior
    
    * removed AnnotTrack . . still getting a single error in relationship serfice
    
    * feature relationship delete seems to work
    
    * fixed non-canonical splite site update isseus
    
    * fixed the rest of the the flusing errors
    
    * remove transcript / gene
    
    * changed to make grailsw work
    
    * updated for grails 2.5.4
    
    * forgot to add the warpper code
    
    * forgot to install jbrowse it looked like
    
    * touched to retrigger a buidl
    
    * some minor changes to fix caching issues
    
    * added some datasource output to bootstrap
    
    * fixed double merge
    
    * seemed to fix caching issues
    
    * added some flushes

--- test/integration/org/bbop/apollo/FeatureRelationshipServiceIntegrationSpec.groovy
+++ test/integration/org/bbop/apollo/FeatureRelationshipServiceIntegrationSpec.groovy
@@ -35,6 +35,35 @@ class FeatureRelationshipServiceIntegrationSpec extends IntegrationSpec {
         FeatureRelationship.count ==0
     }
 
+    void "delete all parents and features on multiple levels"(){
+
+        given: "a gene and a transcript"
+        Gene gene = new Gene(name: "asdf",uniqueName: "asdf").save()
+        MRNA mrna = new MRNA(name: "rman",uniqueName: "mrna").save()
+        Exon exon = new Exon(name: "exonname",uniqueName: "exon-unique").save()
+
+        when: "we attach the two"
+        featureRelationshipService.addChildFeature(gene,mrna,false)
+        featureRelationshipService.addChildFeature(mrna,exon,false)
+
+        then: "they should be attached"
+        Gene.count ==1
+        MRNA.count ==1
+        Exon.count ==1
+        FeatureRelationship.count ==2
+        Feature.count ==3
+
+        when: "we delete from the gene"
+        featureRelationshipService.deleteFeatureAndChildren(gene)
+
+        then: "there should be nothing left"
+        Gene.count ==0
+        MRNA.count ==0
+        Exon.count ==0
+        FeatureRelationship.count ==0
+        Feature.count ==0
+    }
+
     void "delete all parents and features for multiple children"(){
 
         given: "a gene and a transcript"
