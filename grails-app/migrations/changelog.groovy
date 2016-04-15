7c3f5fa0a7066511739e7df20ebd79985bc83b5b
Colin Diesh <colin.diesh@gmail.com>
2016-04-15 00:33:26 -0500

    Update release build rules to fix #941 (#989)
    * Update build.xml for release build
    
    * Change to use handleJBrowse for release target

--- grails-app/migrations/changelog.groovy
+++ grails-app/migrations/changelog.groovy
@@ -1,5 +1,5 @@
 databaseChangeLog = {
     include file: 'changelog-2_0_1.groovy'
     include file: 'changelog-2_0_2.groovy'
-	include file: 'changelog-2_0_3.groovy'
+    include file: 'changelog-2_0_3.groovy'
 }
