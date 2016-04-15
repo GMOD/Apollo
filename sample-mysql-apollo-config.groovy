a6fd5d512637074d9a0eb4f2c80bf63dee2a955a
Deepak Unni <deepak.unni3@gmail.com>
2016-04-15 09:42:37 -0500

    Remove Chado data source configuration from sample apollo config for H2 and MySQL

--- sample-mysql-apollo-config.groovy
+++ sample-mysql-apollo-config.groovy
@@ -17,14 +17,6 @@ environments {
             dialect = org.hibernate.dialect.MySQL5InnoDBDialect
             url = "jdbc:mysql://localhost/apollo"
         }
-        dataSource_chado{
-            dbCreate = "update" // one of 'create', 'create-drop', 'update', 'validate', ''
-            username = "<CHANGEME>"
-            password = "<CHANGEME>"
-            driverClassName = "com.mysql.jdbc.Driver"
-            dialect = org.hibernate.dialect.MySQL5InnoDBDialect
-            url = "jdbc:mysql://localhost/apollo-chado"
-        }
     }
     test {
         dataSource{
@@ -35,14 +27,6 @@ environments {
             dialect = org.hibernate.dialect.MySQL5InnoDBDialect
             url = "jdbc:mysql://localhost/apollo-test"
         }
-        dataSource_chado{
-            dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
-            username = "<CHANGEME>"
-            password = "<CHANGEME>"
-            driverClassName = "com.mysql.jdbc.Driver"
-            dialect = org.hibernate.dialect.MySQL5InnoDBDialect
-            url = "jdbc:mysql://localhost/apollo-test-chado"
-        }
     }
     production {
         dataSource{
@@ -52,34 +36,6 @@ environments {
             driverClassName = "com.mysql.jdbc.Driver"
             dialect = org.hibernate.dialect.MySQL5InnoDBDialect
             url = "jdbc:mysql://localhost/apollo-production"
-            properties {
-                // See http://grails.org/doc/latest/guide/conf.html#dataSource for documentation
-                jmxEnabled = true
-                initialSize = 5
-                maxActive = 50
-                minIdle = 5
-                maxIdle = 25
-                maxWait = 10000
-                maxAge = 10 * 60000
-                timeBetweenEvictionRunsMillis = 5000
-                minEvictableIdleTimeMillis = 60000
-                validationQuery = "SELECT 1"
-                validationQueryTimeout = 3
-                validationInterval = 15000
-                testOnBorrow = true
-                testWhileIdle = true
-                testOnReturn = false
-                jdbcInterceptors = "ConnectionState"
-                defaultTransactionIsolation = java.sql.Connection.TRANSACTION_READ_COMMITTED
-            }
-        }
-        dataSource_chado{
-            dbCreate = "update" // one of 'create', 'create-drop', 'update', 'validate', ''
-            username = "<CHANGEME>"
-            password = "<CHANGEME>"
-            driverClassName = "com.mysql.jdbc.Driver"
-            dialect = org.hibernate.dialect.MySQL5InnoDBDialect
-            url = "jdbc:mysql://localhost/apollo-production-chado"
             properties {
                 // See http://grails.org/doc/latest/guide/conf.html#dataSource for documentation
                 jmxEnabled = true
