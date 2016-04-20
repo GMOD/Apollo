ce22c92b0e170d71d3c0eb6d9820f7b449b07539
Nathan Dunn <ndunn@me.com>
2016-04-20 10:08:31 -0700

    a small refactor

--- client/apollo/js/View/Track/ProjectionCoordinates.js
+++ client/apollo/js/View/Track/ProjectionCoordinates.js
@@ -92,7 +92,105 @@ return declare(
         return bp ;
     },
 
-    showRange: function(first, last, startBase, bpPerBlock, scale, containerStart, containerEnd) {
+    removeLabels: function(){
+        for (var bpCoord in this.svgCoords.middleCoord) {
+            this.svgCoords.middleCoord[bpCoord].setAttribute("display","none");
+            this.svgCoords.topCoord[bpCoord].setAttribute("display","none");
+            this.svgCoords.bottomCoord[bpCoord].setAttribute("display","none");
+            this.svgCoords.shadedCoord[bpCoord].setAttribute("display","none");
+        }
+    },
+
+    addGridLabel: function(bpCoord){
+        var coordinateLabel = this.calculateBpForSequence(bpCoord+1);
+        var x = this.bp2Native(bpCoord);
+        var svgCoord;
+        if (bpCoord in this.svgCoords.middleCoord) {
+            svgCoord = this.svgCoords.middleCoord[bpCoord];
+        }
+        else {
+            svgCoord = document.createElementNS('http://www.w3.org/2000/svg', 'text');
+            this.svgCoords.middleCoord[bpCoord] = svgCoord;
+        }
+        var xlength = 5; // for 0 case only
+        var formattedLabel = numberWithCommas(coordinateLabel);
+        var offsetMultiplier = 4;
+        if (bpCoord +1 != 0) {
+            xlength = -(formattedLabel.length - 1) * offsetMultiplier;
+        }
+        svgCoord.setAttribute('x', x + xlength);
+        svgCoord.setAttribute('y', 30);
+        svgCoord.setAttribute('fill', 'black');
+        svgCoord.setAttribute('fill-opacity', '0.5');
+        svgCoord.setAttribute('display', 'block');
+        svgCoord.innerHTML = formattedLabel ;
+        this.coordGroup.appendChild(svgCoord);
+    },
+
+    addTopTick: function(bpCoord){
+        var label = this.getSequenceForBp(bpCoord);
+        //if(label=='') continue ;
+        var topTick;
+        if (bpCoord in this.svgCoords.topCoord) {
+            topTick= this.svgCoords.topCoord[bpCoord];
+        }
+        else {
+            topTick = document.createElementNS('http://www.w3.org/2000/svg', 'text');
+            this.svgCoords.topCoord[bpCoord] = topTick;
+        }
+
+        x = this.bp2Native(bpCoord);
+        // draw stems
+        var xlength = -((label.length - 1) * 3);
+        topTick.setAttribute('x', x+xlength);
+        topTick.setAttribute('y', 10);
+        topTick.setAttribute('stroke-width', 0.5);
+        topTick.setAttribute('stroke', 'black');
+        topTick.setAttribute('display', 'block');
+        topTick.innerHTML = label ;
+        this.coordGroup.appendChild(topTick);
+    },
+
+    addBottomTick: function(bpCoord){
+        var x = this.bp2Native(bpCoord);
+        var bottomTick;
+        if (bpCoord in this.svgCoords.bottomCoord) {
+            bottomTick = this.svgCoords.bottomCoord[bpCoord];
+        }
+        else {
+            bottomTick = document.createElementNS('http://www.w3.org/2000/svg', 'line');
+            this.svgCoords.bottomCoord[bpCoord] = bottomTick;
+        }
+
+        bottomTick.setAttribute('x1', x);
+        bottomTick.setAttribute('y1', 40);
+        bottomTick.setAttribute('x2', x);
+        bottomTick.setAttribute('y2', 50);
+        bottomTick.setAttribute('stroke', 'rgba(0,0,0,.5)');
+        bottomTick.setAttribute('stroke-width', 2);
+        bottomTick.setAttribute('stroke-linecap', 'round');
+        bottomTick.setAttribute('display', 'block');
+        this.coordGroup.appendChild(bottomTick);
+    },
+
+        addShadedTick: function (bpCoord) {
+            var x = this.bp2Native(bpCoord);
+            var tick;
+            if (bpCoord in this.svgCoords.shadedCoord) {
+                tick = this.svgCoords.shadedCoord[bpCoord];
+            }
+            else {
+                tick = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+                this.svgCoords.shadedCoord[bpCoord] = tick;
+            }
+            tick.setAttribute('d', 'M'+x+' 30 L'+(x-50)+' 0 L'+(x+50)+' 0 Z');
+            tick.setAttribute('fill', this.getColorForBp(bpCoord));
+            tick.setAttribute('fill-opacity', 0.1);
+            tick.setAttribute('display', 'block');
+            this.coordGroup.appendChild(tick);
+        },
+
+        showRange: function(first, last, startBase, bpPerBlock, scale, containerStart, containerEnd) {
         console.log("SVGLayerCoords::showRange");
 
         this.inherited( arguments );
@@ -105,108 +203,18 @@ return declare(
         this.svgCoords.setAttribute('style', 'left:'+left+'%;width:'+width+'%;height:100%;position:absolute;z-index:15');
         this.coordGroup.setAttribute('style', 'width:100%;height:100%;position:absolute;');
 
+        // TODO: there is a better way to erase
         // erase test coordinates
-        for (var bpCoord in this.svgCoords.middleCoord) {
-            this.svgCoords.middleCoord[bpCoord].setAttribute("display","none");
-            this.svgCoords.topCoord[bpCoord].setAttribute("display","none");
-            this.svgCoords.bottomCoord[bpCoord].setAttribute("display","none");
-            this.svgCoords.shadedCoord[bpCoord].setAttribute("display","none");
-        }
+        this.removeLabels();
 
+        // TODO: refactor for a single loop
         // draw test coordinates
         for(i=first;i < last;i++) {
             var bpCoord = this.svgParent.blocks[i].startBase;
-            var coordinateLabel = this.calculateBpForSequence(bpCoord+1);
-            var x = this.bp2Native(bpCoord);
-            var svgCoord;
-            if (bpCoord in this.svgCoords.middleCoord) {
-                svgCoord = this.svgCoords.middleCoord[bpCoord];
-            }
-            else {
-                svgCoord = document.createElementNS('http://www.w3.org/2000/svg', 'text');
-                this.svgCoords.middleCoord[bpCoord] = svgCoord;
-            }
-            var xlength = 5; // for 0 case only
-            var formattedLabel = numberWithCommas(coordinateLabel);
-            var offsetMultiplier = 4;
-            if (bpCoord +1 != 0) {
-                xlength = -(formattedLabel.length - 1) * offsetMultiplier;
-            }
-            svgCoord.setAttribute('x', x + xlength);
-            svgCoord.setAttribute('y', 30);
-            svgCoord.setAttribute('fill', 'black');
-            svgCoord.setAttribute('fill-opacity', '0.5');
-            //svgCoord.setAttribute('transform','rotate(90 '+x+' 20)');
-            svgCoord.setAttribute('display', 'block');
-            svgCoord.innerHTML = formattedLabel ;
-            this.coordGroup.appendChild(svgCoord);
-        }
-
-        for(var i=first;i < last;i++) {
-            bpCoord = this.svgParent.blocks[i].startBase;
-            var label = this.getSequenceForBp(bpCoord);
-            //if(label=='') continue ;
-            var topTick;
-            if (bpCoord in this.svgCoords.topCoord) {
-                topTick= this.svgCoords.topCoord[bpCoord];
-            }
-            else {
-                topTick = document.createElementNS('http://www.w3.org/2000/svg', 'text');
-                this.svgCoords.topCoord[bpCoord] = topTick;
-            }
-
-            x = this.bp2Native(bpCoord);
-            // draw stems
-            var xlength = -((label.length - 1) * 3);
-            topTick.setAttribute('x', x+xlength);
-            topTick.setAttribute('y', 10);
-            topTick.setAttribute('stroke-width', 0.5);
-            topTick.setAttribute('stroke', 'black');
-            topTick.setAttribute('display', 'block');
-            topTick.innerHTML = label ;
-            this.coordGroup.appendChild(topTick);
-        }
-
-        for(var i=first;i < last;i++) {
-            var bpCoord = this.svgParent.blocks[i].startBase;
-            var x = this.bp2Native(bpCoord);
-            var bottomTick;
-            if (bpCoord in this.svgCoords.bottomCoord) {
-                bottomTick = this.svgCoords.bottomCoord[bpCoord];
-            }
-            else {
-                bottomTick = document.createElementNS('http://www.w3.org/2000/svg', 'line');
-                this.svgCoords.bottomCoord[bpCoord] = bottomTick;
-            }
-
-            bottomTick.setAttribute('x1', x);
-            bottomTick.setAttribute('y1', 40);
-            bottomTick.setAttribute('x2', x);
-            bottomTick.setAttribute('y2', 50);
-            bottomTick.setAttribute('stroke', 'rgba(0,0,0,.5)');
-            bottomTick.setAttribute('stroke-width', 2);
-            bottomTick.setAttribute('stroke-linecap', 'round');
-            bottomTick.setAttribute('display', 'block');
-            this.coordGroup.appendChild(bottomTick);
-        }
-
-        for(var i=first;i < last;i++) {
-            var bpCoord = this.svgParent.blocks[i].startBase;
-            var x = this.bp2Native(bpCoord);
-            var tick;
-            if (bpCoord in this.svgCoords.shadedCoord) {
-                tick = this.svgCoords.shadedCoord[bpCoord];
-            }
-            else {
-                tick = document.createElementNS('http://www.w3.org/2000/svg', 'path');
-                this.svgCoords.shadedCoord[bpCoord] = tick;
-            }
-            tick.setAttribute('d', 'M'+x+' 30 L'+(x-50)+' 0 L'+(x+50)+' 0 Z');
-            tick.setAttribute('fill', this.getColorForBp(bpCoord));
-            //tick.setAttribute('color', 'black');
-            tick.setAttribute('fill-opacity', 0.1);
-            tick.setAttribute('display', 'block');
-            this.coordGroup.appendChild(tick);
+            this.addGridLabel(bpCoord);
+            this.addTopTick(bpCoord);
+            this.addBottomTick(bpCoord);
+            this.addShadedTick(bpCoord);
         }
     },
     bp2Native: function(val) {
