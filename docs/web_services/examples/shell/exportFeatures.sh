4bde73572620ca9c716cbff5dc9c246ba7ca3588
Nathan Dunn <ndunn@me.com>
2016-05-24 14:06:38 -0700

    #1045 - cleaned up API description and doc

---
+++ docs/web_services/examples/shell/exportFeatures.sh
@@ -0,0 +1,7 @@
+#!/bin/sh 
+seq='';
+
+curl --silent 'http://localhost:8080/apollo/IOService/write'  \
+	-H 'Content-Type: application/json' \
+	--data "{\"type\":\"GFF3\", \"seqType\":\"genomic\", \"exportAllSequences\":\"false\", \"exportGff3Fasta\":\"true\", \"output\":\"text\", \"format\":\"text\", \"sequences\":[], \"username\":\"demo@user.com\", \"password\":\"password\",\"organism\":\"Merlin\" }" ;
+#    --data "{\"type\":\"FASTA\", \"seqType\":\"genomic\", \"exportAllSequences\":\"true\", \"exportGff3Fasta\":\"true\", \"output\":\"text\", \"format\":\"gff3\", \"sequences\":[], \"username\":\"ndunn@me.com\", \"password\":\"demo\",\"organism\":\"Volvox\" }" ;
