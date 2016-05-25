41c3bc5e19553a7939adbe2883225630a8e7a1f3
Nathan Dunn <ndunn@me.com>
2016-05-25 13:47:31 -0700

    added the client Token

--- client/apollo/js/View/Track/AnnotTrack.js
+++ client/apollo/js/View/Track/AnnotTrack.js
@@ -380,13 +380,16 @@ define([
                     });
                     console.log('connection established');
                 }
-                else {
+                else
+                // TODO: note this code will likely be removed with an error that it has to be wrapped
+                {
                     console.log('No embedded server is present.');
                     client.connect({}, function () {
 
                         var request = {
                             "name": track.refSeq.name,
-                            "organism": track.webapollo.organism
+                            "organism": track.webapollo.organism,
+                            "clientToken": track.getClientToken()
                         };
 
                         xhr.post(context_path + "/sequence/lookupSequenceByNameAndOrganism/", {
