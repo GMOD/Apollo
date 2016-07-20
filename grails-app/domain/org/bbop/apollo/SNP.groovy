6649909eccd550cb2391278b58622a31ab85f17d
Deepak Unni <deepak.unni3@gmail.com>
2016-07-20 09:07:37 -0500

    Add SNV and SNP domain objects to org.bbop.apollo

---
+++ grails-app/domain/org/bbop/apollo/SNP.groovy
@@ -0,0 +1,11 @@
+package org.bbop.apollo
+
+class SNP extends SNV {
+
+    static constraints = {
+    }
+
+    static String cvTerm = "SNV"
+    static String ontologyId = "SO:0000694"
+    static String alternateCvTerm = "single nucleotide polymorphism"
+}
---
+++ grails-app/domain/org/bbop/apollo/SNV.groovy
@@ -0,0 +1,11 @@
+package org.bbop.apollo
+
+class SNV extends Substitution {
+
+    static constraints = {
+    }
+
+    static String cvTerm = "SNV"
+    static String ontologyId = "SO:0001483"
+    static String alternateCvTerm = "single nucleotide variant"
+}
