d6a6c0007c304bd33d6e8168e345089327cde669
Nathan Dunn <ndunn@me.com>
2016-07-21 16:09:00 -0700

    fixed missing import

--- src/gwt/org/bbop/apollo/gwt/client/MainPanel.java
+++ src/gwt/org/bbop/apollo/gwt/client/MainPanel.java
@@ -14,6 +14,7 @@ import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.uibinder.client.UiHandler;
 import com.google.gwt.user.client.Command;
+import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.*;
 import com.google.gwt.user.client.ui.ListBox;
 import org.bbop.apollo.gwt.client.dto.*;
