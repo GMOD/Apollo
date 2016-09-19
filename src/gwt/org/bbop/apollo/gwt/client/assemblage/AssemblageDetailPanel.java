c914d6c97842070f2860441cfe1b53ab15d60e63
Nathan Dunn <ndunn@me.com>
2016-09-19 15:47:53 -0700

    everything is draggable now

--- src/gwt/org/bbop/apollo/gwt/client/assemblage/AssemblageDetailPanel.java
+++ src/gwt/org/bbop/apollo/gwt/client/assemblage/AssemblageDetailPanel.java
@@ -6,7 +6,6 @@ import com.allen_sauer.gwt.dnd.client.drop.VerticalPanelDropController;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
-import com.google.gwt.user.client.Random;
 import com.google.gwt.user.client.ui.*;
 import org.bbop.apollo.gwt.client.dto.assemblage.AssemblageInfo;
 import org.bbop.apollo.gwt.client.dto.assemblage.AssemblageSequence;
@@ -64,46 +63,6 @@ public class AssemblageDetailPanel extends Composite {
         HorizontalPanelDropController columnDropController = new HorizontalPanelDropController(
                 horizontalPanel);
         columnDragController.registerDropController(columnDropController);
-
-        if(true) return ;
-        for (int col = 1; col <= 3 ; col++) {
-            // initialize a vertical panel to hold the heading and a second vertical
-            // panel
-            VerticalPanel columnCompositePanel = new VerticalPanel();
-//            columnCompositePanel.addStyleName(CSS_DEMO_INSERT_PANEL_EXAMPLE_COLUMN_COMPOSITE);
-
-            // initialize inner vertical panel to hold individual widgets
-            VerticalPanel verticalPanel = new VerticalPanelWithSpacer();
-//            verticalPanel.addStyleName(CSS_DEMO_INSERT_PANEL_EXAMPLE_CONTAINER);
-            verticalPanel.setSpacing(SPACING);
-            horizontalPanel.add(columnCompositePanel);
-
-            // initialize a widget drop controller for the current column
-            VerticalPanelDropController widgetDropController = new VerticalPanelDropController(
-                    verticalPanel);
-            widgetDragController.registerDropController(widgetDropController);
-
-            // Put together the column pieces
-            Label heading = new Label("Column " + col);
-//            heading.addStyleName(CSS_DEMO_INSERT_PANEL_EXAMPLE_HEADING);
-            columnCompositePanel.add(heading);
-            columnCompositePanel.add(verticalPanel);
-
-            // make the column draggable by its heading
-            columnDragController.makeDraggable(columnCompositePanel, heading);
-
-            int count = 0 ;
-//            for (int row = 1; row <= 5 ; row++) {
-//                // initialize a widget
-//                HTML widget = new HTML("Draggable&nbsp;#" + ++count);
-////                widget.addStyleName(CSS_DEMO_INSERT_PANEL_EXAMPLE_WIDGET);
-//                widget.setHeight(Random.nextInt(4) + 2 + "em");
-//                verticalPanel.add(widget);
-//
-//                // make the widget draggable
-//                widgetDragController.makeDraggable(widget);
-//            }
-        }
     }
 
     public AssemblageInfo getAssemblageInfo() {
@@ -111,14 +70,93 @@ public class AssemblageDetailPanel extends Composite {
     }
 
     public void setAssemblageInfo(Set<AssemblageInfo> selectedObjects) {
-//        if(true) return ;
 
-        Map<String, VerticalPanel> sequenceColumnMap = new HashMap<>();
 
-        // a map of the features?
-        Map<String, VerticalPanel> sequenceFeatureMap = new HashMap<>();
+        widgetDragController.unregisterDropControllers();
 
         horizontalPanel.clear();
+
+        // we have to munge this to Sequence,List<Feature>
+        // assemblageInfo will only have a single feature
+//        Map<String, List<AssemblageSequence>> assemblageSequenceListMap = new HashMap<>();
+//        TreeMap<Integer, String> orderMap = new TreeMap<>();
+//
+//        int counter = 0;
+//        for (AssemblageInfo assemblageInfo : selectedObjects) {
+//            AssemblageSequenceList sequenceArray = assemblageInfo.getSequenceList();
+//            for (int i = 0; i < sequenceArray.size(); i++) {
+//                AssemblageSequence assemblageSequence = sequenceArray.getSequence(i);
+//
+//                List<AssemblageSequence> assemblageInfoList = assemblageSequenceListMap.get(assemblageSequence.getName());
+//                SequenceFeatureInfo sequenceFeatureInfo = assemblageSequence.getFeature();
+////                if (sequenceFeatureInfo != null) {
+//                    if (assemblageInfoList == null) {
+//                        assemblageInfoList = new ArrayList<>();
+//                    }
+//                    assemblageInfoList.add(assemblageSequence);
+////                }
+//                assemblageSequenceListMap.put(assemblageSequence.getName(), assemblageInfoList);
+//                if (!orderMap.containsValue(assemblageSequence.getName())) {
+//                    orderMap.put(counter, assemblageSequence.getName());
+//                }
+//                ++counter;
+//            }
+//        }
+//        GWT.log("assemblies: " + assemblageSequenceListMap.size());
+//
+//
+//        // we simplify the ordering to do it all at once
+//        for (String sequenceName : orderMap.values()) {
+//            GWT.log("trying to "+ sequenceName + " read for: "+orderMap.values());
+//            List<AssemblageSequence> assemblageSequences = assemblageSequenceListMap.get(sequenceName);
+//            GWT.log("assemblies: " + assemblageSequences.size() + " for "+sequenceName);
+//            VerticalPanel sequenceColumnPanel = new VerticalPanel();
+//
+//            VerticalPanel featurePanel = new VerticalPanelWithSpacer();
+//            featurePanel.setSpacing(SPACING);
+//            horizontalPanel.add(sequenceColumnPanel);
+//
+//            VerticalPanelDropController widgetDropController = new VerticalPanelDropController(featurePanel);
+//            widgetDragController.registerDropController(widgetDropController);
+//
+//
+////            HorizontalPanel headingPanel = new HorizontalPanel();
+//            HTML headingHtml = new HTML(sequenceName);
+//
+////            if (sequenceObject.getReverse()) {
+////                headingPanel.add(new Icon(IconType.LONG_ARROW_LEFT));
+////            }
+////            headingPanel.add(headingHtml);
+////            if (!sequenceObject.getReverse()) {
+////                headingPanel.add(new Icon(IconType.LONG_ARROW_RIGHT));
+////            }
+//
+//            sequenceColumnPanel.add(headingHtml);
+////                    sequenceColumnPanel.add(headingPanel);
+//            sequenceColumnPanel.add(featurePanel);
+//
+////                    columnDragController.makeDraggable(sequenceColumnPanel, headingPanel);
+//            columnDragController.makeDraggable(sequenceColumnPanel, headingHtml);
+//
+////            sequenceColumnMap.put(sequenceName, sequenceColumnPanel);
+////                    sequenceFeatureMap.put(sequenceName, featurePanel);
+//
+//            // extract the feature
+//            for(AssemblageSequence assemblageSequence : assemblageSequences){
+//                SequenceFeatureInfo sequenceFeatureInfo = assemblageSequence.getFeature();
+//                if (sequenceFeatureInfo != null) {
+////                    VerticalPanel thisFeaturePanel = sequenceFeatureMap.get(sequenceName);
+//                    String name = sequenceFeatureInfo.getName();
+//                    HTML widget = new HTML(name);
+//                    featurePanel.add(widget);
+//////                // make the widget draggable
+//                    widgetDragController.makeDraggable(widget);
+//                }
+//            }
+//        }
+        Map<String, VerticalPanel> sequenceColumnMap = new HashMap<>();
+        Map<String, VerticalPanel> sequenceFeatureMap = new HashMap<>();
+
         for (AssemblageInfo assemblageInfo : selectedObjects) {
 
             AssemblageSequenceList sequenceArray = assemblageInfo.getSequenceList();
@@ -142,11 +180,11 @@ public class AssemblageDetailPanel extends Composite {
                     HorizontalPanel headingPanel = new HorizontalPanel();
                     HTML headingHtml = new HTML(sequenceName);
 
-                    if(sequenceObject.getReverse()){
+                    if (sequenceObject.getReverse()) {
                         headingPanel.add(new Icon(IconType.LONG_ARROW_LEFT));
                     }
                     headingPanel.add(headingHtml);
-                    if(!sequenceObject.getReverse()){
+                    if (!sequenceObject.getReverse()) {
                         headingPanel.add(new Icon(IconType.LONG_ARROW_RIGHT));
                     }
 
@@ -160,7 +198,6 @@ public class AssemblageDetailPanel extends Composite {
                     sequenceColumnMap.put(sequenceName, sequenceColumnPanel);
                     sequenceFeatureMap.put(sequenceName, featurePanel);
                 }
-                // else, nothing to do, we have to process the feature isntead
 
                 // extract the feature
                 SequenceFeatureInfo sequenceFeatureInfo = sequenceObject.getFeature();
@@ -172,6 +209,8 @@ public class AssemblageDetailPanel extends Composite {
 ////                // make the widget draggable
                     widgetDragController.makeDraggable(widget);
                 }
+                // else, nothing to do, we have to process the feature isntead
+
 
             }
         }
