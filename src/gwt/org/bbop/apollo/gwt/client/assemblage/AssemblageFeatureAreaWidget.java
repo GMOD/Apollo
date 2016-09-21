3d83f67f9262b059109dfff351f5476f60ad303b
Nathan Dunn <ndunn@me.com>
2016-09-21 13:00:57 -0700

    no longer listing duplicates

--- src/gwt/org/bbop/apollo/gwt/client/assemblage/AssemblageFeatureAreaWidget.java
+++ src/gwt/org/bbop/apollo/gwt/client/assemblage/AssemblageFeatureAreaWidget.java
@@ -2,7 +2,12 @@ package org.bbop.apollo.gwt.client.assemblage;
 
 import com.allen_sauer.gwt.dnd.client.PickupDragController;
 import com.allen_sauer.gwt.dnd.client.drop.VerticalPanelDropController;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.user.client.ui.Widget;
 import org.bbop.apollo.gwt.client.dto.assemblage.SequenceFeatureInfo;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * This creates the area that contains feature widgets.
@@ -21,9 +26,32 @@ public class AssemblageFeatureAreaWidget extends VerticalPanelWithSpacer{
     }
 
     public void addSequenceFeature(SequenceFeatureInfo sequenceFeatureInfo,PickupDragController featureDragController) {
-        AssemblageFeatureWidget assemblageFeatureWidget = new AssemblageFeatureWidget(sequenceFeatureInfo);
-        assemblageFeatureWidget.render(featureDragController);
-        add(assemblageFeatureWidget);
+        if(!containsFeature(sequenceFeatureInfo)){
+            AssemblageFeatureWidget assemblageFeatureWidget = new AssemblageFeatureWidget(sequenceFeatureInfo);
+            assemblageFeatureWidget.render(featureDragController);
+            add(assemblageFeatureWidget);
+        }
+    }
+
+    private boolean containsFeature(SequenceFeatureInfo sequenceFeatureInfo) {
+        for(AssemblageFeatureWidget assemblageFeatureWidget : getAssembledFeatureWidgets()){
+            if(sequenceFeatureInfo.getName().equals(assemblageFeatureWidget.getSequenceFeatureInfo().getName())){
+//                GWT.log("not adding a duplicate feature "+sequenceFeatureInfo.getName());
+                return true ;
+            }
+        }
+        return false ;
+    }
+
+    private List<AssemblageFeatureWidget> getAssembledFeatureWidgets() {
+        List<AssemblageFeatureWidget> assemblageFeatureWidgets = new ArrayList<>();
+        for(int i = 0 ; i < getWidgetCount() ; i++){
+            if(getWidget(i) instanceof AssemblageFeatureWidget){
+                assemblageFeatureWidgets.add( (AssemblageFeatureWidget) getWidget(i));
+            }
+        }
+
+        return  assemblageFeatureWidgets ;
     }
 
 }
--- src/gwt/org/bbop/apollo/gwt/client/assemblage/AssemblageFeatureWidget.java
+++ src/gwt/org/bbop/apollo/gwt/client/assemblage/AssemblageFeatureWidget.java
@@ -3,27 +3,23 @@ package org.bbop.apollo.gwt.client.assemblage;
 import com.allen_sauer.gwt.dnd.client.PickupDragController;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.user.client.Random;
-import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import org.bbop.apollo.gwt.client.dto.assemblage.SequenceFeatureInfo;
 import org.gwtbootstrap3.client.ui.Button;
-import org.gwtbootstrap3.client.ui.Icon;
 import org.gwtbootstrap3.client.ui.Image;
 import org.gwtbootstrap3.client.ui.constants.ButtonType;
-import org.gwtbootstrap3.client.ui.constants.IconType;
 import org.gwtbootstrap3.client.ui.constants.ImageType;
 import org.gwtbootstrap3.client.ui.constants.Pull;
 
 /**
  * Created by nathandunn on 9/20/16.
  */
-public class AssemblageFeatureWidget extends HorizontalPanel{
+public class AssemblageFeatureWidget extends HorizontalPanel {
 
-    private SequenceFeatureInfo sequenceFeatureInfo ;
+    private SequenceFeatureInfo sequenceFeatureInfo;
 
     private Button featureButton = new Button();
-//    private Icon actionIcon = new Icon();
+    //    private Icon actionIcon = new Icon();
     private Image expandImage = new Image();
     private Image collapseImage = new Image();
 
@@ -39,18 +35,17 @@ public class AssemblageFeatureWidget extends HorizontalPanel{
     }
 
     private void handleCollapsed() {
-        if(sequenceFeatureInfo.isCollapsed()){
+        if (sequenceFeatureInfo.isCollapsed()) {
             collapseImage.setVisible(false);
             expandImage.setVisible(true);
-        }
-        else{
+        } else {
             collapseImage.setVisible(true);
             expandImage.setVisible(false);
         }
     }
 
-    public AssemblageFeatureWidget(final SequenceFeatureInfo sequenceFeatureInfo){
-        this.sequenceFeatureInfo = sequenceFeatureInfo ;
+    public AssemblageFeatureWidget(final SequenceFeatureInfo sequenceFeatureInfo) {
+        this.sequenceFeatureInfo = sequenceFeatureInfo;
         featureButton.setType(ButtonType.INFO);
         String name = sequenceFeatureInfo.getName();
         featureButton.setText(name);
@@ -74,7 +69,7 @@ public class AssemblageFeatureWidget extends HorizontalPanel{
         collapseImage.setVisible(!sequenceFeatureInfo.isCollapsed());
         collapseImage.setUrl("../images/fold.png");
         collapseImage.setType(ImageType.DEFAULT);
-        collapseImage.setSize("20px","20px");
+        collapseImage.setSize("20px", "20px");
         collapseImage.setPull(Pull.RIGHT);
         featureButton.add(collapseImage);
 
@@ -90,7 +85,7 @@ public class AssemblageFeatureWidget extends HorizontalPanel{
         expandImage.setVisible(sequenceFeatureInfo.isCollapsed());
         expandImage.setUrl("../images/unfold.png");
         expandImage.setType(ImageType.DEFAULT);
-        expandImage.setSize("20px","20px");
+        expandImage.setSize("20px", "20px");
         expandImage.setPull(Pull.RIGHT);
         featureButton.add(expandImage);
 
@@ -107,8 +102,8 @@ public class AssemblageFeatureWidget extends HorizontalPanel{
     }
 
 
-    public void render(PickupDragController featureDragController){
-        featureDragController.makeDraggable(this,featureButton);
+    public void render(PickupDragController featureDragController) {
+        featureDragController.makeDraggable(this, featureButton);
     }
 
     public SequenceFeatureInfo getSequenceFeatureInfo() {
