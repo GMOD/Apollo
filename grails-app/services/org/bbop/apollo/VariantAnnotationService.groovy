884d539be1e358b53d83270308a684dbd149b654
Deepak Unni <deepak.unni3@gmail.com>
2016-10-03 12:45:04 -0500

    Add a new service called VariantAnnotationService which will be responsible for predicting effect of variants on genomic features

---
+++ grails-app/services/org/bbop/apollo/VariantAnnotationService.groovy
@@ -0,0 +1,57 @@
+package org.bbop.apollo
+
+import grails.transaction.Transactional
+import org.bbop.apollo.gwt.shared.FeatureStringEnum
+import org.bbop.apollo.sequence.SequenceTranslationHandler
+import org.bbop.apollo.sequence.Strand
+
+@Transactional
+class VariantAnnotationService {
+
+    def featureService
+    def transcriptService
+    def cdsService
+    def exonService
+    def sequenceService
+    def featureRelationshipService
+
+    def calculateVariantEffects(SequenceAlteration variant) {
+        println "@calculateVariantEffects"
+        Set<Feature> variants = new HashSet<>()
+        variants.add(variant)
+        def overlappingTranscripts = featureService.getOverlappingTranscripts(variant.featureLocation)
+        for (Transcript transcript : overlappingTranscripts) {
+            variants.addAll(getOverlappingVariants(transcript))
+        }
+        if (overlappingTranscripts.size() > 0) {
+            calculateEffectOfVariantsOnTranscripts(variants, overlappingTranscripts)
+        }
+    }
+
+    def getOverlappingVariants(Transcript transcript) {
+        println "@getOverlappingVariants"
+        Set<Feature> variantsList = new HashSet<>()
+        def overlappingFeaturesList = featureService.getOverlappingFeatures(transcript.featureLocation, false)
+
+        for (Feature feature : overlappingFeaturesList) {
+            if (feature instanceof Variant) {
+                // perform instanceof Variant check to avoid SequenceAlterations that are Assembly Error Corrections
+                variantsList.add(feature)
+            }
+        }
+        return variantsList
+    }
+
+    def calculateEffectOfVariantsOnTranscripts(def variants, def transcripts) {
+        println "@calculateEffectOfVariantsOnTranscripts"
+        println "Variants: ${variants}"
+        println "Transcripts: ${transcripts}"
+        for (Transcript transcript : transcripts) {
+            String original = featureService.getResiduesWithAlterationsAndFrameshifts(transcript, [FeatureStringEnum.ASSEMBLY_ERROR_CORRECTION.value])
+            String altered = featureService.getResiduesWithAlterationsAndFrameshifts(transcript, [FeatureStringEnum.ASSEMBLY_ERROR_CORRECTION.value, FeatureStringEnum.VARIANT.value])
+            println "Original String: ${original}"
+            println "Altered String: ${altered}"
+        }
+    }
+
+}
