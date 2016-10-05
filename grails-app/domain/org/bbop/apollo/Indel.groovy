af6989eab9fe18d216fdb7cd73020d04ab6d1255
Deepak Unni <deepak.unni3@gmail.com>
2016-10-05 15:57:40 -0500

    Move over variant properties like referenceBases and alternateAlleles from SNV domain to SequenceAlteration domain

--- grails-app/domain/org/bbop/apollo/Indel.groovy
+++ grails-app/domain/org/bbop/apollo/Indel.groovy
@@ -6,21 +6,4 @@ class Indel extends SequenceAlteration implements Variant {
     static String ontologyId = "SO:1000032"
     static String alternateCvTerm = "indel"
 
-    String referenceBases
-
-    static hasMany = [
-            alternateAlleles: Allele,
-            variantInfo     : FeatureProperty,
-            variantCalls    : Call
-    ]
-
-//    static mappedBy = [
-//            alternateAlleles: "variant"
-//    ]
-
-    static mapping = {
-        alternateAlleles cascade: 'all-delete-orphan'
-        variantInfo cascade: 'all-delete-orphan'
-        variantCalls cascade: 'all-delete-orphan'
-    }
 }
--- grails-app/domain/org/bbop/apollo/SNV.groovy
+++ grails-app/domain/org/bbop/apollo/SNV.groovy
@@ -6,23 +6,4 @@ class SNV extends Substitution implements Variant {
     static String ontologyId = "SO:0001483"
     static String alternateCvTerm = "single nucleotide variant"
 
-    String referenceBases
-
-    // TODO: Should variantInfo use a separate class than FeatureProperty
-
-    static hasMany = [
-            alternateAlleles: Allele,
-            variantInfo     : FeatureProperty,
-            variantCalls    : Call
-    ]
-
-//    static mappedBy = [
-//            alternateAlleles: "variant"
-//    ]
-
-    static mapping = {
-        alternateAlleles cascade: 'all-delete-orphan'
-        variantInfo cascade: 'all-delete-orphan'
-        variantCalls cascade: 'all-delete-orphan'
-    }
 }
