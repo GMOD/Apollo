f272ad13f4d550ba83afeea1ed1cb24a8516cd5b
Deepak Unni <deepak.unni3@gmail.com>
2016-11-15 14:40:29 -0600

    Add methods to transform source coordinates to local coordinates; add unit tests

--- grails-app/services/org/bbop/apollo/VariantAnnotationService.groovy
+++ grails-app/services/org/bbop/apollo/VariantAnnotationService.groovy
@@ -1,9 +1,13 @@
 package org.bbop.apollo
 
 import grails.transaction.Transactional
+import org.bbop.apollo.alteration.SequenceAlterationInContext
 import org.bbop.apollo.gwt.shared.FeatureStringEnum
 import org.bbop.apollo.sequence.SequenceTranslationHandler
+import org.bbop.apollo.sequence.StandardTranslationTable
 import org.bbop.apollo.sequence.Strand
+import org.bbop.apollo.sequence.TranslationTable
+import org.codehaus.groovy.grails.web.json.JSONObject
 
 @Transactional
 class VariantAnnotationService {
@@ -12,21 +16,10 @@ class VariantAnnotationService {
     def transcriptService
     def cdsService
     def exonService
+    def overlapperService
     def sequenceService
     def featureRelationshipService
-
-    def calculateVariantEffects(SequenceAlteration variant) {
-        log.info "@calculateVariantEffects"
-        Set<Feature> variants = new HashSet<>()
-        variants.add(variant)
-        def overlappingTranscripts = featureService.getOverlappingTranscripts(variant.featureLocation, false)
-        for (Transcript transcript : overlappingTranscripts) {
-            variants.addAll(getOverlappingVariants(transcript))
-        }
-        if (overlappingTranscripts.size() > 0) {
-            calculateEffectOfVariantsOnTranscripts(variants, overlappingTranscripts)
-        }
-    }
+    def configWrapperService
 
     def getOverlappingVariants(Transcript transcript) {
         println "@getOverlappingVariants"
@@ -42,16 +35,400 @@ class VariantAnnotationService {
         return variantsList
     }
 
-    def calculateEffectOfVariantsOnTranscripts(def variants, def transcripts) {
-        println "@calculateEffectOfVariantsOnTranscripts"
-        println "Variants: ${variants}"
-        println "Transcripts: ${transcripts}"
-        for (Transcript transcript : transcripts) {
-            String original = featureService.getResiduesWithAlterationsAndFrameshifts(transcript, [FeatureStringEnum.ASSEMBLY_ERROR_CORRECTION.value])
-            String altered = featureService.getResiduesWithAlterationsAndFrameshifts(transcript, [FeatureStringEnum.ASSEMBLY_ERROR_CORRECTION.value, FeatureStringEnum.VARIANT.value])
-            println "Original String: ${original}"
-            println "Altered String: ${altered}"
+    /**
+     * Calculate effect of a variant on all overlapping transcripts
+     * @param variant
+     */
+    def calculateEffectOfVariant(SequenceAlteration variant) {
+        log.info "calculate effect of variant ${variant.name}"
+        def overlappingTranscripts = featureService.getOverlappingTranscripts(variant.featureLocation, false)
+        log.info "Overlapping transcripts: ${overlappingTranscripts.name}"
+        for (Transcript transcript : overlappingTranscripts) {
+            calculateEffectOfVariantOnTranscript(variant, transcript)
         }
     }
 
+    /**
+     * Calculate effect of a variant on a given transcript
+     * @param variant
+     * @param transcript
+     * @return
+     */
+    def calculateEffectOfVariantOnTranscript(SequenceAlteration variant, Transcript transcript) {
+
+        // TODO: predicting the effect of variant on transcript
+        VariantEffect variantEffect = new VariantEffect(
+                cdnaPosition: -1,
+                cdsPosition: -1,
+                proteinPosition: -1,
+                referenceCodon: 'NNN',
+                alternateCodon: 'NNN',
+                referenceResidue: 'N',
+                alternateResidue: 'N'
+
+        )
+        variantEffect.variant = variant
+        // TODO: support multiple alternate alleles
+        variantEffect.alternateAllele = variant.alternateAlleles.iterator().next()
+        variantEffect.save()
+        assignSOTypeToVariantEffect(variant, variantEffect, transcript)
+    }
+
+    /**
+     * Assign SO Type to Variant Effect
+     * @param variant
+     * @param variantEffect
+     * @param transcript
+     */
+    def assignSOTypeToVariantEffect(SequenceAlteration variant, VariantEffect variantEffect, Transcript transcript) {
+
+        variantEffect.addToEffects(new SequenceVariant())
+        variantEffect.save()
+        // TODO: Get SO Terms based on variantEffect
+    }
+
+    /**
+     * Apply offsets of all the sequence alterations that are upstream of the given variant
+     * @param variant
+     * @param sequenceAlterationInContextList
+     * @return
+     */
+    def applyOffsetFromAssemblyErrorCorrections(SequenceAlteration variant, Collection<SequenceAlterationInContext> sequenceAlterationInContextList) {
+        int variantFmin = variant.fmin
+        int variantFmax = variant.fmax
+
+        for (SequenceAlterationInContext sa : sequenceAlterationInContextList) {
+            if (variant.fmin >= sa.fmin) {
+                // variant is downstream of the alteration
+                if (sa.instanceOf == Insertion.class.canonicalName) {
+                    variantFmin += sa.alterationResidue.length()
+                    variantFmax += sa.alterationResidue.length()
+                }
+                else if (sa.instanceOf == Deletion.class.canonicalName) {
+                    variantFmin -= sa.alterationResidue.length()
+                    variantFmax -= sa.alterationResidue.length()
+                }
+                else if (sa.instanceOf == Substitution.class.canonicalName) {
+                    // do nothing since AEC of tpye Substitution don't introduce any frameshifts
+                }
+            }
+        }
+        return [variantFmin, variantFmax]
+    }
+
+    /**
+     * Given a source coordinate, transform the coordinate in the context of a given feature.
+     * @param fmin
+     * @param fmax
+     * @param strand
+     * @param sourceCoordinate
+     * @return
+     */
+    def convertSourceCoordinateToLocalCoordinate(int fmin, int fmax, int strand, int sourceCoordinate) {
+        log.info "convertSourceCoordinateToLocalCoordinate: ${fmin} ${fmax} ${strand} ${sourceCoordinate}"
+        // fmin and fmax are in global context
+        if (sourceCoordinate < fmin || sourceCoordinate > fmax) {
+            // sourceCoordinate never falls within the feature
+            log.info "sourceCoordinate never falls within the feature; return -1"
+            return -1
+        }
+        if (strand == Strand.NEGATIVE.value) {
+            return fmax - 1 - sourceCoordinate
+        }
+        else {
+            return sourceCoordinate - fmin
+        }
+    }
+
+    /**
+     * Given a local coordinate, transform the coordinate into global context
+     * @param fmin
+     * @param fmax
+     * @param strand
+     * @param localCoordinate
+     * @return
+     */
+    def convertLocalCoordinateToSourceCoordinate(int fmin, int fmax, int strand, int localCoordinate) {
+        log.info "convertLocalCoordinateToSourceCoordinate: ${fmin} ${fmax} ${strand} ${localCoordinate}"
+        // fmin and fmax are in global context
+        if (localCoordinate < 0 || localCoordinate > (fmax - fmin)) {
+            log.info "localCoordinate never falls within the feature"
+            // local coordinate never falls within the feature
+            return -1
+        }
+        if (strand == Strand.NEGATIVE.value) {
+            return fmax - localCoordinate - 1
+        }
+        else {
+            return fmin + localCoordinate
+        }
+    }
+
+    /**
+     * Given a source coordinate, transform the coordinate in the context of a given transcript's exons
+     * @param exonFminArray
+     * @param exonFmaxArray
+     * @param strand
+     * @param sourceCoordinate
+     * @return
+     */
+    def convertSourceCoordinateToLocalCoordinateForTranscript(List exonFminArray, List exonFmaxArray, int strand, int sourceCoordinate) {
+        log.info "convertSourceCoordinateToLocalCoordinateForTranscript: ${exonFminArray} ${exonFmaxArray} ${strand} ${sourceCoordinate}"
+        // exon fmin and fmax array are in global context
+        int localCoordinate = -1
+        int currentCoordinate = 0
+
+        if (strand == Strand.NEGATIVE.value) {
+            exonFminArray.reverse(true)
+            exonFmaxArray.reverse(true)
+        }
+        for (int i = 0; i < exonFminArray.size(); i++) {
+            int exonFmin = exonFminArray.get(i)
+            int exonFmax = exonFmaxArray.get(i)
+
+            if (sourceCoordinate >= exonFmin && sourceCoordinate <= exonFmax) {
+                // sourceCoordinate falls within an exon
+                log.info "sourceCoordinate falls within exon ${exonFmin} - ${exonFmax}"
+                if (strand == Strand.NEGATIVE.value) {
+                    localCoordinate = currentCoordinate + (exonFmax - sourceCoordinate) - 1
+                }
+                else {
+                    localCoordinate = currentCoordinate + (sourceCoordinate - exonFmin)
+                }
+                break
+            }
+            else {
+                currentCoordinate += (exonFmax - exonFmin)
+            }
+
+        }
+        println "Returning with ${localCoordinate}"
+        return localCoordinate
+    }
+
+    /**
+     * Given a local coordinate, transforms the coordinate into global context, accounting for exons
+     * @param exonFminArray
+     * @param exonFmaxArray
+     * @param strand
+     * @param localCoordinate
+     * @return sourceCoordinate
+     */
+    def convertLocalCoordinateToSourceCoordinateForTranscript(List exonFminArray, List exonFmaxArray, int strand, int localCoordinate) {
+        log.info "convertLocalCoordinateToSourceCoordinateForTranscript: ${exonFminArray} ${exonFmaxArray} ${strand} ${localCoordinate}"
+        int sourceCoordinate = -1
+        int currentLength = 0
+        int currentCoordinate = localCoordinate
+
+        if (strand == Strand.NEGATIVE.value) {
+            exonFminArray.reverse(true)
+            exonFmaxArray.reverse(true)
+        }
+
+        for (int i = 0; i < exonFminArray.size(); i++) {
+            int exonFmin = exonFminArray.get(i)
+            int exonFmax = exonFmaxArray.get(i)
+            int exonLength = exonFmax - exonFmin
+
+            if (currentLength + exonLength >= localCoordinate) {
+                log.info "localCoordinate falls within exon ${exonFmin} - ${exonFmax}"
+                if (strand == Strand.NEGATIVE.value) {
+                    sourceCoordinate = (exonFmax - currentCoordinate) - 1
+                }
+                else {
+                    sourceCoordinate = exonFmin + currentCoordinate
+                }
+                break
+            }
+            else {
+                currentLength += exonLength
+                currentCoordinate -= exonLength
+            }
+        }
+        return sourceCoordinate
+    }
+
+    /**
+     * Given a source coordinate, transforms the coordinate into the context of a given transcript's CDS
+     * @param cdsFmin
+     * @param cdsFmax
+     * @param exonFminArray
+     * @param exonFmaxArray
+     * @param strand
+     * @param sourceCoordinate
+     * @return
+     */
+    def convertSourceCoordinateToLocalCoordinateForCDS(int cdsFmin, int cdsFmax, List exonFminArray, List exonFmaxArray, int strand, int sourceCoordinate) {
+        int localCoordinate = 0
+
+        if (!(cdsFmin <= sourceCoordinate && cdsFmax >= sourceCoordinate)) {
+            return -1
+        }
+
+        int x,y = 0
+
+        if (strand == Strand.POSITIVE.value) {
+            for (int i = 0; i < exonFminArray.size(); i++) {
+                int exonFmin = exonFminArray.get(i)
+                int exonFmax = exonFmaxArray.get(i)
+                if (overlaps(exonFmin, exonFmax, cdsFmin, cdsFmax) && exonFmin >= cdsFmin && exonFmax <= cdsFmax) {
+                    // complete overlap
+                    log.info "Complete overlap"
+                    x = exonFmin
+                    y = exonFmax
+                }
+                else if (overlaps(exonFmin, exonFmax, cdsFmin, cdsFmax)) {
+                    // partial overlap
+                    log.info "Partial overlap"
+                    if (exonFmin < cdsFmin && exonFmax < cdsFmax) {
+                        x = cdsFmin
+                        y = exonFmax
+                    }
+                    else {
+                        x = exonFmin
+                        y = cdsFmax
+                    }
+                }
+                else {
+                    // no overlap
+                    log.info "No overlap"
+                    continue
+                }
+
+                if (x <= sourceCoordinate && y >= sourceCoordinate) {
+                    localCoordinate += sourceCoordinate - x
+                    return localCoordinate
+                }
+                else {
+                    localCoordinate += y - x
+                }
+            }
+        }
+        else {
+            exonFminArray.reverse(true)
+            exonFmaxArray.reverse(true)
+            for (int i = 0; i < exonFminArray.size(); i++) {
+                int exonFmin = exonFminArray.get(i)
+                int exonFmax = exonFmaxArray.get(i)
+                if (overlaps(exonFmin, exonFmax, cdsFmin, cdsFmax) && exonFmin >= cdsFmin && exonFmax <= cdsFmax) {
+                    // complete overlap
+                    log.info "Complete overlap"
+                    x = exonFmax
+                    y = exonFmin
+                }
+                else if (overlaps(exonFmin, exonFmax, cdsFmin, cdsFmax)) {
+                    // partial overlap
+                    log.info "Partial overlap"
+                    if (exonFmin <= cdsFmin && exonFmax <= cdsFmax) {
+                        x = exonFmax
+                        y = cdsFmin
+                    }
+                    else {
+                        x = cdsFmax
+                        y = exonFmin
+                    }
+                }
+                else {
+                    log.info "No overlap"
+                    continue
+                }
+                if (y <= sourceCoordinate && x >= sourceCoordinate) {
+                    localCoordinate += (x - sourceCoordinate) - 1
+                    return localCoordinate
+                }
+                else {
+                    localCoordinate += (x - y)
+                }
+            }
+        }
+        // if it gets here, that means the coordinate was in an intron
+        // TODO: better way to do this
+        return -1
+    }
+
+    /**
+     * Given a local coordinate, transforms the coordinate into global context, accounting for CDS
+     * @param cdsFmin
+     * @param cdsFmax
+     * @param exonFminArray
+     * @param exonFmaxArray
+     * @param strand
+     * @param localCoordinate
+     * @return
+     */
+    def convertLocalCoordinateToSourceCoordinateForCDS(int cdsFmin, int cdsFmax, List exonFminArray, List exonFmaxArray, int strand, int localCoordinate) {
+        int sourceCoordinate = -1
+        int currentLength = 0
+        int currentCoordinate = localCoordinate
+
+        if (strand == Strand.NEGATIVE.value) {
+            exonFminArray.reverse(true)
+            exonFmaxArray.reverse(true)
+        }
+
+        int x, y =  0
+        for (int i = 0; i < exonFminArray.size(); i++) {
+            int exonFmin = exonFminArray.get(i)
+            int exonFmax = exonFmaxArray.get(i)
+
+            x = Math.max(exonFmin, cdsFmin)
+            y = Math.min(exonFmax, cdsFmax)
+            int segmentLength = y - x
+            if (currentLength + segmentLength >= localCoordinate) {
+                log.info "LocalCoordinate falls within segment ${x} - ${y}"
+                if (strand == Strand.NEGATIVE.value) {
+                    sourceCoordinate = (y - currentCoordinate) - 1
+                }
+                else {
+                    sourceCoordinate = x + currentCoordinate
+                }
+                break
+            }
+            else {
+                currentLength += segmentLength
+                currentCoordinate -= segmentLength
+            }
+        }
+        return sourceCoordinate
+    }
+
+    /**
+     *
+     * @param pos
+     * @param sequence
+     * @return
+     */
+    def getCodonFromSequence(int pos, String sequence) {
+        String codon
+        int aaPosition = Math.ceil(pos / 3)
+        if (pos % 3 == 0) {
+            // [X]YZ
+            log.info "mod 0"
+            codon = sequence.substring(pos, pos + 3)
+        }
+        else if (pos % 3 == 1) {
+            // X[Y]Z
+            log.info "mod 1"
+            codon = sequence.substring(pos - 1, pos + 2)
+        }
+        else if (pos % 3 == 2) {
+            // XY[Z]
+            log.info "mod 2"
+            codon = sequence.substring(pos - 2, pos + 1)
+        }
+        return [codon, aaPosition]
+    }
+
+    /**
+     *
+     * @param leftFmin
+     * @param leftFmax
+     * @param rightFmin
+     * @param rightFmax
+     * @return
+     */
+    boolean overlaps(int leftFmin, int leftFmax,int rightFmin,int rightFmax) {
+        return (leftFmin <= rightFmin && leftFmax > rightFmin ||
+                leftFmin >= rightFmin && leftFmin < rightFmax)
+    }
 }
