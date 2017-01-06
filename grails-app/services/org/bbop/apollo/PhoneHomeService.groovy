4c8967c4e00da5eea493064c64fd9c1a17db09e7
Nathan Dunn <ndunn@me.com>
2017-01-06 14:15:11 -0800

    fixed async phonehome_Server

--- grails-app/services/org/bbop/apollo/PhoneHomeService.groovy
+++ grails-app/services/org/bbop/apollo/PhoneHomeService.groovy
@@ -1,5 +1,6 @@
 package org.bbop.apollo
 
+import grails.async.Promise
 import grails.transaction.Transactional
 import groovy.json.JsonSlurper
 
@@ -9,6 +10,23 @@ class PhoneHomeService {
     def configWrapperService
     def grailsApplication
 
+    def pingServerAsync(String message = null ,Map<String,String> argMap = [:]) {
+        try {
+            Promise p = task {
+                // Long running task
+                pingServer(org.bbop.apollo.PhoneHomeEnum.START.value)
+            }
+            p.onError { Throwable err ->
+                log.error "An error occured while phoning home ${err.message}"
+            }
+            p.onComplete { result ->
+                log.info "Phone home completed"
+            }
+            def result = p.get()
+        } catch (e) {
+            log.warn("Failed to phone home: "+e)
+        }
+    }
 
     /**
      * Only process args if there is a message
