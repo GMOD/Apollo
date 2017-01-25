d708136919a6a47c48b90d86c4866349af50f24a
Nathan Dunn <ndunn@me.com>
2017-01-25 11:41:51 -0800

    added toggle switch and synced for #1452

--- src/gwt/org/bbop/apollo/gwt/client/MainPanel.java
+++ src/gwt/org/bbop/apollo/gwt/client/MainPanel.java
@@ -975,7 +975,6 @@ public class MainPanel extends Composite {
     public void trackListToggleButtonHandler(ClickEvent event){
         useNativeTracklist = !trackListToggle.isActive();
         trackPanel.updateTrackToggle(useNativeTracklist);
-        trackPanel.trackListToggle(event);
     }
 
     public static native void exportStaticMethod() /*-{
