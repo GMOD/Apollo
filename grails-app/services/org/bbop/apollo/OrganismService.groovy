1ebda692c6c4116ce5ff6e1fc7a7c2ec03105df4
Nathan Dunn <ndunn@me.com>
2017-01-30 20:42:06 -0800

    removing features will work for larger transactions

--- grails-app/services/org/bbop/apollo/OrganismService.groovy
+++ grails-app/services/org/bbop/apollo/OrganismService.groovy
@@ -1,43 +1,54 @@
 package org.bbop.apollo
 
+import grails.transaction.NotTransactional
 import grails.transaction.Transactional
-import org.hibernate.Hibernate
-import org.hibernate.Session
 
 @Transactional
 class OrganismService {
     def featureService
 
+    int TRANSACTION_SIZE = 100
+
+    @NotTransactional
     def deleteAllFeaturesForOrganism(Organism organism) {
-        def list=Feature.withCriteria() {
-            featureLocations {
-                sequence {
-                    eq("organism",organism)
-                }
+
+        def featurePairs = Feature.executeQuery("select f.id,f.uniqueName from Feature f join f.featureLocations fl join fl.sequence s join s.organism o where o=:organism", [organism: organism])
+
+        // maximum transaction size  30
+
+        println "feature sublists created ${featurePairs.size()}"
+        def featureSubLists = featurePairs.collate(TRANSACTION_SIZE)
+        println "sublists size ${featureSubLists.size()}"
+        int count = 0
+        featureSubLists.each { featureList ->
+            def ids = featureList.collect() {
+                it[0]
             }
+            println "ids ${ids.size()}"
+            def uniqueNames = featureList.collect() {
+                it[1]
+            }
+            println "uniqueNames ${uniqueNames.size()}"
+            Feature.withTransaction{
+                def features = Feature.findAllByIdInList(ids)
+                features.each { f ->
+                    f.delete()
+                }
+                println "deleted features"
+//                FeatureEvent.withTransaction {
+                def featureEvents = FeatureEvent.findAllByUniqueNameInList(uniqueNames)
+                featureEvents.each { fe ->
+                    fe.delete()
+                }
+//                }
+                println "deleted feature events"
+                organism.save(flush: true)
+                count += TRANSACTION_SIZE
+                println "count ${count}"
+                println "${count} / ${featurePairs.size()}  =  ${count / featurePairs.size()}% "
+            }
+            println "fin"
+            organism.save(flush: true)
         }
-
-
-        def uniqueNames=list.collect {
-            it.uniqueName
-        }
-
-        list.each {
-            it.delete();
-        }
-
-
-        def events=FeatureEvent.withCriteria() {
-            'in'("uniqueName",uniqueNames)
-        }
-
-
-        events.each {
-            it.delete()
-        }
-
-
-        organism.save(flush: true )
-        return list.size()
     }
 }
