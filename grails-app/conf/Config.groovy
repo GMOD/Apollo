4c782bb57cf301093e07e7c9d26f5699ac24fddd
Monica Munoz-Torres <monicacecilia@users.noreply.github.com>
2017-02-01 12:46:05 -0800

    Added caution text (#1472)
    Added comment on use of NeatFeatures plugins in combination with Apollo

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -399,6 +399,9 @@ jbrowse {
 //        type ="zip"
 //        fileName = "JBrowse-1.12.0-dev"
 //    }
+//
+//	// Warning: We are still testing the performance of NeatFeatures plugins in combination with Apollo.
+//	// We advise caution if enabling these plugins with Apollo until this process is finalized.
     plugins {
         WebApollo{
             included = true
