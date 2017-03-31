85e183c03e0422a07b4268198976f27fdf98a4b3
Chris Childers <genetics.guy@gmail.com>
2017-03-31 16:11:11 -0400

    adding code to make comments mandatory for sequence modifications

--- client/apollo/js/View/Track/SequenceTrack.js
+++ client/apollo/js/View/Track/SequenceTrack.js
@@ -1035,6 +1035,10 @@ var SequenceTrack = declare( "SequenceTrack", DraggableFeatureTrack,
                 alert("Input cannot be empty for " + type);
                 ok = false;
             }
+	    if (commentFieldValue.length == 0) {
+		alert("Please provide a justification for the sequence modification (" + type + ") in the 'Comment' field.");
+                ok = false;
+	    }
             if (ok) {
                 var input = inputField.value.toUpperCase();
                 if (type == "deletion") {
