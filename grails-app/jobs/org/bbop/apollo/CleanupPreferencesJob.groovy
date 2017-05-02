6ac27830c9179900de9380a034a10b079912b20c
Nathan Dunn <ndunn@me.com>
2017-05-02 13:23:00 -0700

    added delay to strart

--- grails-app/jobs/org/bbop/apollo/CleanupPreferencesJob.groovy
+++ grails-app/jobs/org/bbop/apollo/CleanupPreferencesJob.groovy
@@ -7,7 +7,7 @@ class CleanupPreferencesJob {
 
     static triggers = {
 //      simple repeatInterval: 30000l // execute job every 30 seconds for testing
-        simple repeatInterval: 24 * 60 * 60 * 1000l // execute job once a day
+        simple repeatInterval: 24 * 60 * 60 * 1000l ,startDelay: 5 * 60 * 1000l // execute job once a day, delays 5 minutes
     }
 
     def execute() {
