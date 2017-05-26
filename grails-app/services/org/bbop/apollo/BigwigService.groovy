bf6dbfcaf457daa60b8a552985fbbbb719a6a226
Nathan Dunn <ndunn@me.com>
2017-05-26 13:14:24 -0700

    changed min score to 0

--- grails-app/services/org/bbop/apollo/BigwigService.groovy
+++ grails-app/services/org/bbop/apollo/BigwigService.groovy
@@ -1,12 +1,12 @@
 package org.bbop.apollo
 
+import edu.unc.genomics.Interval
 import edu.unc.genomics.io.BigWigFileReader
 import grails.transaction.NotTransactional
 import grails.transaction.Transactional
 import org.bbop.apollo.gwt.shared.projection.MultiSequenceProjection
 import org.bbop.apollo.gwt.shared.projection.ProjectionSequence
 import org.codehaus.groovy.grails.web.json.JSONArray
-import edu.unc.genomics.Interval
 import org.codehaus.groovy.grails.web.json.JSONObject
 
 @Transactional
@@ -53,9 +53,9 @@ class BigwigService {
         int maxInView = 1000
         Integer realEnd = 0
 
-        Map<Integer,Integer> lengthMap = new TreeMap<>()
+        Map<Integer, Integer> lengthMap = new TreeMap<>()
         for (ProjectionSequence projectionSequence in projection.sequenceDiscontinuousProjectionMap.keySet().sort() { a, b -> a.order <=> b.order }) {
-            lengthMap.put(projectionSequence.order,projection.sequenceDiscontinuousProjectionMap.get(projectionSequence).length)
+            lengthMap.put(projectionSequence.order, projection.sequenceDiscontinuousProjectionMap.get(projectionSequence).length)
             realEnd += bigWigFileReader.getChrStop(projectionSequence.name)
         }
 
@@ -64,10 +64,10 @@ class BigwigService {
         for (ProjectionSequence projectionSequence in projection.sequenceDiscontinuousProjectionMap.keySet().sort() { a, b -> a.order <=> b.order }) {
             // recalculate start and stop for each sequence
             Integer calculatedStart = start < offset ? 0 : start + offset
-            Integer calculatedStop = end > projectionSequence.length + offset ?  lengthMap.get(order) : end
-            Integer stepSize = maxInView < (calculatedStop - calculatedStart) ? (calculatedStop- calculatedStart) / maxInView : 1
+            Integer calculatedStop = end > projectionSequence.length + offset ? lengthMap.get(order) : end
+            Integer stepSize = maxInView < (calculatedStop - calculatedStart) ? (calculatedStop - calculatedStart) / maxInView : 1
             calculateFeatureArray(featuresArray, calculatedStart, calculatedStop, stepSize, bigWigFileReader, projection, projectionSequence)
-            offset = lengthMap.get(order)+1
+            offset = lengthMap.get(order) + 1
             ++order
         }
 //        }
@@ -79,19 +79,14 @@ class BigwigService {
         for (Integer i = actualStart; i < actualStop; i += stepSize) {
             JSONObject globalFeature = new JSONObject()
             Integer endStep = i + stepSize
-            globalFeature.put("start", i+projectionSequence.projectedOffset)
-            globalFeature.put("end", endStep+projectionSequence.projectedOffset)
+            globalFeature.put("start", i + projectionSequence.projectedOffset)
+            globalFeature.put("end", endStep + projectionSequence.projectedOffset)
             Integer originalStart = projection.unProjectValue(i)
             Integer originalEnd = projection.unProjectValue(endStep)
             edu.unc.genomics.Contig innerContig = bigWigFileReader.query(projectionSequence.name, originalStart, originalEnd)
             Integer value = innerContig.mean()
 
-            if (value >= 0) {
-//                        // TODO: this should be th mean value
-                globalFeature.put("score", value)
-            } else {
-                globalFeature.put("score", 0)
-            }
+            globalFeature.put("score", value >= 1 ?: 1)
             featuresArray.add(globalFeature)
         }
     }
