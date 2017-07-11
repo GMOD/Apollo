91443c1f8535f9520dd8f8e166193cb0a9e86f75
Nathan Dunn <nathandunn@users.noreply.github.com>
2017-07-11 15:35:36 -0700

    Fix track order 1629 (#1688)
    * updated
    
    * updated
    
    * fixed defaults
    
    * fixed defaults
    
    * fixed in the sequence track
    
    * prior CDS length calculated
    
    * fixed in the case of no cds length
    
    * removed excessive logging
    
    * removed extra comment, etc.
    
    * fixed last problem with #1688 knock on wood

--- client/apollo/js/View/Track/DraggableHTMLFeatures.js
+++ client/apollo/js/View/Track/DraggableHTMLFeatures.js
@@ -31,7 +31,7 @@ define( [
         SimpleFeature,
         SeqOnto ) {
 
-var debugFrame = false;
+var debugFrame = true ;
 
 var draggableTrack = declare( HTMLFeatureTrack,
 
@@ -634,12 +634,12 @@ var draggableTrack = declare( HTMLFeatureTrack,
         // whole exon is translated
         else if (cdsMin <= subStart && cdsMax >= subEnd) {
             var overhang = priorCdsLength % 3;  // number of bases overhanging from previous CDS
+            var absFrame, cdsFrame, initFrame
             var relFrame = (3 - (priorCdsLength % 3)) % 3;
-            var absFrame, cdsFrame, initFrame;
             if (reverse)  {
-                initFrame = (cdsMax - 1) % 3;
-                absFrame = (subEnd - 1) % 3;
-                cdsFrame = (3 + absFrame - relFrame) % 3;
+                initFrame = (cdsMax  ) % 3;
+                absFrame = (subEnd ) % 3;
+                cdsFrame = ( (absFrame - relFrame) + 3 ) % 3;
             }
             else  {
                 initFrame = cdsMin % 3;
@@ -676,9 +676,8 @@ var draggableTrack = declare( HTMLFeatureTrack,
             if (priorCdsLength > 0)  {
                 var relFrame = (3 - (priorCdsLength % 3)) % 3;
                 if (reverse)  {
-                    //      cdsFrame = ((subEnd-1) + ((3 - (priorCdsLength % 3)) % 3)) % 3; }
-                    initFrame = (cdsMax - 1) % 3;
-                    absFrame = (subEnd - 1) % 3;
+                    initFrame = (cdsMax) % 3;
+                    absFrame = (subEnd ) % 3;
                     cdsFrame = (3 + absFrame - relFrame) % 3;
                 }
                 else  {
@@ -693,7 +692,7 @@ var draggableTrack = declare( HTMLFeatureTrack,
             }
             else  {  // actually shouldn't need this? -- if priorCdsLength = 0, then above conditional collapses down to same calc...
                 if (reverse) {
-                    cdsFrame = (cdsMax-1) % 3; // console.log("rendering reverse frame");
+                    cdsFrame = (cdsMax) % 3;
                 }
                 else  {
                     cdsFrame = cdsMin % 3;
