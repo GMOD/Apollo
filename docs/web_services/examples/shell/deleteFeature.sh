c60089f7d0eb2f8d360e14e7b40b2cd04aa633ba
Deepak <deepakunni3@users.noreply.github.com>
2017-09-01 13:51:26 -0500

    Fix web services examples (#1746)

--- docs/web_services/examples/shell/deleteFeature.sh
+++ docs/web_services/examples/shell/deleteFeature.sh
@@ -6,11 +6,9 @@ password=$3
 uniqueName=$4
 
 usage() {
-    echo "Sample script for adding status to a feature via web services"
+    echo "Sample script for deleting a feature via web services"
     echo "Usage:    ./deleteFeature.sh <complete_apollo_URL> <username> <password> <unique_name_for_feature>"
     echo "Example:  ./deleteFeature.sh http://localhost:8080/apollo demo@demo.com demo f5f9fb2d-5b40-48fb-bf02-b67a87cfb82a"
-    echo ""
-    echo "Note: Be sure to create the status beforehand in Apollo from the Admin tab in Annotator Panel"
 }
 
 
@@ -19,5 +17,5 @@ if [[ ! -n "$url" || ! -n "$username" || ! -n "$password" || ! -n "$uniqueName" 
     exit
 fi
 
-echo curl -i -H \"Content-type: application/json\" -X POST ${url}/annotationEditor/deleteFeature -d \"{\"username\":\"${username}\", \"password\":\"${password}\", \"features\":[{\"uniquename\":\"${uniqueName}\", \"status\":\'${status}\'}], \"track\":\"${trackName}\", \"organism\":\"${organism}\"}\"
-curl -i -H "Content-type: application/json" -X POST ${url}/annotationEditor/deleteFeature -d "{"username":"${username}", "password":"${password}", "features":[{"uniquename":"${uniqueName}"}]}"
+echo curl -i -H 'Content-type: application/json' -X POST ${url}/annotationEditor/deleteFeature -d "{'username':'${username}', 'password':'${password}', 'features':[{'uniquename':'${uniqueName}'}], 'track':'${trackName}', 'organism':'${organism}'}"
+curl -i -H 'Content-type: application/json' -X POST ${url}/annotationEditor/deleteFeature -d "{'username':'${username}', 'password':'${password}', 'features':[{'uniquename':'${uniqueName}'}], 'track':'${trackName}', 'organism':'${organism}'}"
