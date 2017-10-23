35e3e005e1931a4e2cf6023ff0d29b0b35c590b0
Deepak Unni <deepak.unni3@gmail.com>
2017-10-23 13:12:35 -0500

    Add database migration changelog

--- grails-app/migrations/changelog.groovy
+++ grails-app/migrations/changelog.groovy
@@ -3,4 +3,5 @@ databaseChangeLog = {
     include file: 'changelog-2_0_2.groovy'
     include file: 'changelog-2_0_3.groovy'
     include file: 'changelog-2_0_7.groovy'
+    include file: 'changelog-2_0_8.groovy'
 }
