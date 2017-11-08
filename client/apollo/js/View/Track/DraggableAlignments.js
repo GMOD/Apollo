bab690b481fa906c2058bc9980193c3f09411446
Deepak Unni <deepak.unni3@gmail.com>
2017-11-08 11:08:04 -0600

    project features before displaying them in DraggableAlignments

--- client/apollo/js/View/Track/DraggableAlignments.js
+++ client/apollo/js/View/Track/DraggableAlignments.js
@@ -1,13 +1,15 @@
 define([
            'dojo/_base/declare', 
            'dojo/_base/array',
+           'dojo/_base/lang',
            'JBrowse/View/Track/Alignments',
            'WebApollo/View/Track/DraggableHTMLFeatures', 
            'JBrowse/Util'
        ],
        function(
            declare,
-           array, 
+           array,
+           lang,
            AlignmentsTrack,
            DraggableTrack, 
            Util
@@ -120,6 +122,93 @@ return declare([ DraggableTrack, AlignmentsTrack ], {
                 }
             }, this );
         }
+    },
+
+    /**
+     * Override
+     * @param args
+     */
+    fillFeatures: function( args ) {
+        var blockIndex = args.blockIndex;
+        var block = args.block;
+        var leftBase = args.leftBase;
+        var rightBase = args.rightBase;
+        var scale = args.scale;
+        var stats = args.stats;
+        var containerStart = args.containerStart;
+        var containerEnd = args.containerEnd;
+        var finishCallback = args.finishCallback;
+        var browser = this.browser;
+        this.scale = scale;
+        block.featureNodes = {};
+
+        //determine the glyph height, arrowhead width, label text dimensions, etc.
+        if( !this.haveMeasurements ) {
+            this.measureStyles();
+            this.haveMeasurements = true;
+        }
+
+        var labelScale       = this.config.style.labelScale       || stats.featureDensity * this.config.style._defaultLabelScale;
+        var descriptionScale = this.config.style.descriptionScale || stats.featureDensity * this.config.style._defaultDescriptionScale;
+        var curTrack = this;
+
+        var featCallback = dojo.hitch(this,function( feature ) {
+            var uniqueId = feature.id();
+            // project feature after it is fetched from the store
+            if ( !feature.isProjected ) {
+                feature = JSONUtils.projectJSONFeature(feature, this.refSeq.name);
+            }
+
+            if( ! this._featureIsRendered( uniqueId ) ) {
+                if( this.filterFeature( feature ) )  {
+                    var render = 1;
+                    if (typeof this.renderFilter === 'function')
+                        render = this.renderFilter(feature);
+
+                    if (render === 1) {
+                        this.addFeatureToBlock( feature, uniqueId, block, scale, labelScale, descriptionScale, containerStart, containerEnd );
+                    }
+                }
+            }
+        });
+
+        this.store.getFeatures( {
+                ref: this.refSeq.name,
+                start: leftBase,
+                end: rightBase
+            },
+            featCallback,
+            function ( args ) {
+                curTrack.heightUpdate(curTrack._getLayout(scale).getTotalHeight(),
+                    blockIndex);
+                if ( args && args.maskingSpans ) {
+                    //note: spans have to be inverted
+                    var invSpan = [];
+                    invSpan[0] = { start: leftBase };
+                    var i = 0;
+                    for ( var span in args.maskingSpans) {
+                        if (args.maskingSpans.hasOwnProperty(span)) {
+                            span = args.maskingSpans[span];
+                            invSpan[i].end = span.start;
+                            i++;
+                            invSpan[i] = { start: span.end };
+                        }
+                    }
+                    invSpan[i].end = rightBase;
+                    if (invSpan[i].end <= invSpan[i].start) {
+                        invSpan.splice(i,1); }
+                    if (invSpan[0].end <= invSpan[0].start) {
+                        invSpan.splice(0,1); }
+                    curTrack.maskBySpans( invSpan, args.maskingSpans );
+                }
+                finishCallback();
+            },
+            function( error ) {
+                console.error( error, error.stack );
+                curTrack.fillBlockError( blockIndex, block, error );
+                finishCallback();
+            }
+        );
     }
 
 } );
