7c270ec454edb1b6add04b9586ec5e150e19dd37
Nathan Dunn <ndunn@me.com>
2017-11-29 19:00:05 -0800

    updated

--- docs/web_services/examples/shell/getSequences.sh
+++ docs/web_services/examples/shell/getSequences.sh
@@ -9,12 +9,14 @@ features=$5
 usage() {
     echo "Sample script to get all features from a sequence of an organism via web services"
     echo "Usage:    ./getSequences.sh <complete_apollo_URL> <username> <password> <organism> <feature>"
-    echo "Example:  ./getSequences.sh http://localhost:8080/apollo ndunn@me.com demo Honeybee ['Group1.10']"
+    echo "Example:  ./getSequences.sh http://localhost:8080/apollo ndunn@me.com demo Honeybee "
 }
 
-if [[ ! -n "$url" || ! -n "$username" || ! -n "$password" || ! -n "$organism" || -n "$features" ]]; then
+if [[ ! -n "$url" || ! -n "$username" || ! -n "$password" || ! -n "$organism" || ! -n "$features" ]]; then
     usage
     exit
 fi
 
-curl  --header "Content-type: application/json" --request POST --data "{'username': '${username}', 'password': '${password}', 'organism': '${organism}',{ 'uniquename':'${features}'}" "${url}/annotationEditorController/getSequences"
+ echo curl  --header "Content-type: application/json" --request POST --data "{'username': '${username}', 'password': '${password}', 'organism': '${organism}', 'uniquename':'${features}'}" "${url}/annotationEditor/getSequences"
+ curl  --header "Content-type: application/json" --request POST --data "{'username': '${username}', 'password': '${password}', 'organism': '${organism}', 'uniquename':'${features}'}" "${url}/annotationEditor/getSequences"
+#curl  --header "Content-type: application/json" --request POST --data "{'username': '${username}', 'password': '${password}', 'organism': '${organism}',{ 'uniquename':'${features}'}" "${url}/${organism}/${sequence}/getSequences"
