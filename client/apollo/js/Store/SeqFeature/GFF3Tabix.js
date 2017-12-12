469b478252a26593e17cab6e13a87d373ee2dcc7
Nathan Dunn <ndunn@me.com>
2017-12-11 20:18:12 -0800

    made some small changes to the tabix code

--- client/apollo/js/Store/SeqFeature/GFF3Tabix.js
+++ client/apollo/js/Store/SeqFeature/GFF3Tabix.js
@@ -26,50 +26,9 @@ define([
        ) {
 
 
-return declare( [ GlobalStatsEstimationMixin , GFF3Tabix ],
+return declare( [  GFF3Tabix , GlobalStatsEstimationMixin ],
 {
 
-    constructor: function( args ) {
-        var thisB = this;
-
-        var tbiBlob = args.tbi ||
-            new XHRBlob(
-                this.resolveUrl(
-                    this.getConf('tbiUrlTemplate',[]) || this.getConf('urlTemplate',[])+'.tbi'
-                )
-            );
-
-        var fileBlob = args.file ||
-            new XHRBlob(
-                this.resolveUrl( this.getConf('urlTemplate',[]) )
-            );
-
-        this.indexedData = new TabixIndexedFile(
-            {
-                tbi: tbiBlob,
-                file: fileBlob,
-                browser: this.browser,
-                chunkSizeLimit: args.chunkSizeLimit || 1000000
-            });
-
-
-
-
-        this.getHeader()
-            .then( function( header ) {
-                    thisB._deferred.features.resolve({success:true});
-                    thisB._estimateGlobalStats()
-                        .then(
-                            function( stats ) {
-                                thisB.globalStats = stats;
-                                thisB._deferred.stats.resolve( stats );
-                            },
-                            lang.hitch( thisB, '_failAllDeferred' )
-                        );
-                },
-                lang.hitch( thisB, '_failAllDeferred' )
-            );
-    },
 
     _getFeatures: function( query, featureCallback, finishedCallback, errorCallback ) {
         var thisB = this;
@@ -88,27 +47,46 @@ return declare( [ GlobalStatsEstimationMixin , GFF3Tabix ],
             });
 
         thisB.getHeader().then( function() {
-            var refSeqName = query.ref || thisB.refSeq.name ;
+            // var refSeqName = query.ref || thisB.refSeq.name ;
+            var refSeqName = thisB.refSeq.name ;
             var min, max ;
+            var reverse = false ;
+            var chrName ;
             if(ProjectionUtils.isSequenceList(refSeqName)){
-                // var sequenceListObject = ProjectionUtils.parseSequenceList(refSeqName);
-                var unprojectedArray = ProjectionUtils.unProjectCoordinates(refSeqName,query.start,query.end);
-                min = unprojectedArray[0];
-                max = unprojectedArray[1];
+                var sequenceListObject = ProjectionUtils.parseSequenceList(refSeqName);
+                chrName = sequenceListObject[0].name ;
+                reverse = sequenceListObject[0].reverse;
             }
             else{
-                min = query.start ;
-                max = query.end ;
-                // chrName = query.ref ;
+                chrName = query.ref ;
             }
+            min = query.start ;
+            max = query.end ;
 
-            console.log(min + ' ' + max);
+            // console.log(min + ' ' + max);
 
             thisB.indexedData.getLines(
-                refSeqName,
+                chrName,
                 min,
                 max,
                 function( line ) {
+                    if(ProjectionUtils.isSequenceList(refSeqName)) {
+                        console.log(refSeqName);
+                        console.log(line);
+                        var unprojectedArray = ProjectionUtils.unProjectCoordinates(refSeqName, line.start, line.end);
+                        min = unprojectedArray[0];
+                        max = unprojectedArray[1];
+                        line.start = min;
+                        line.end = max;
+                        line.fields[3] = min;
+                        line.fields[4] = max;
+                        if (reverse) {
+                            line.fields[6] = ProjectionUtils.flipStrand(line.fields[6]);
+                        }
+                        console.log('- VS -');
+                        console.log(line);
+                    }
+
                     parser._buffer_feature( thisB.lineToFeature(line) );
                 },
                 function() {
@@ -133,27 +111,30 @@ return declare( [ GlobalStatsEstimationMixin , GFF3Tabix ],
 
         var refSeqName = this.refSeq.name ;
         var min, max ;
+        var chrName;
         if(ProjectionUtils.isSequenceList(refSeqName)){
             var sequenceListObject = ProjectionUtils.parseSequenceList(refSeqName);
-            if(sequenceListObject.reverse){
-                strand = -1 * strand ;
+            chrName = sequenceListObject[0].name ;
+            if(sequenceListObject[0].reverse){
+                strand = ProjectionUtils.flipStrand(strand);
+                // strand = -1 * strand ;
             }
-            var unprojectedArray = ProjectionUtils.unProjectCoordinates(refSeqName,line.start,line.end);
-            min = unprojectedArray[0];
-            max = unprojectedArray[1];
+            // var unprojectedArray = ProjectionUtils.unProjectCoordinates(refSeqName,line.start,line.end);
+            // min = unprojectedArray[0];
+            // max = unprojectedArray[1];
         }
         else{
-            min = line.start;
-            max = line.end ;
-            // chrName = query.ref ;
+            chrName = query.ref ;
         }
+        min = line.start;
+        max = line.end ;
 
         var featureData = {
             start:  min,
             end:    max,
             strand: strand,
             child_features: [],
-            seq_id: line.ref,
+            seq_id: chrName,
             attributes: attributes,
             type:   type,
             source: source,
