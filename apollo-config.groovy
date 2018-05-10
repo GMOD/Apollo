b154ccdbebb7c4e419dea6f089cfe1ad1282746d
Childers <genetics.guy@gmail.com>
2018-05-10 14:00:39 -0400

    updateing with upstream

--- apollo-config.groovy
+++ apollo-config.groovy
@@ -1,3 +1,53 @@
+environments {
+    development {
+        // sample config to turn on debug logging in development e.g. for apollo run-local
+        log4j.main = {
+            debug "grails.app"
+        }
+        // sample config to edit apollo specific configs in development mode
+        apollo {
+            gff3.source = "testing"
+        }
+        dataSource{
+            dbCreate = "update" // one of 'create', 'create-drop', 'update', 'validate', ''
+            username = "webapollo"
+            password = "web2apollo"
+            driverClassName = "org.postgresql.Driver"
+            dialect = org.hibernate.dialect.PostgresPlusDialect
+            url = "jdbc:postgresql://localhost/webapollo_2"
+        }
+   }
+    production {
+        dataSource{
+            dbCreate = "update" // one of 'create', 'create-drop', 'update', 'validate', ''
+            username = "webapollo"
+            password = "web2apollo"
+            driverClassName = "org.postgresql.Driver"
+            dialect = org.hibernate.dialect.PostgresPlusDialect
+            url = "jdbc:postgresql://localhost/webapollo_2"
+            properties {
+                // See http://grails.org/doc/latest/guide/conf.html#dataSource for documentation
+                jmxEnabled = true
+                initialSize = 5
+                maxActive = 50
+                minIdle = 5
+                maxIdle = 25
+                maxWait = 10000
+                maxAge = 10 * 60000
+                timeBetweenEvictionRunsMillis = 5000
+                minEvictableIdleTimeMillis = 60000
+                validationQuery = "SELECT 1"
+                validationQueryTimeout = 3
+                validationInterval = 15000
+                testOnBorrow = true
+                testWhileIdle = true
+                testOnReturn = false
+                jdbcInterceptors = "ConnectionState"
+                defaultTransactionIsolation = java.sql.Connection.TRANSACTION_READ_COMMITTED
+            }
+        }
+   }
+}
 // environments {
 //     development {
 //         // sample config to turn on debug logging in development e.g. for apollo run-local
@@ -110,7 +160,9 @@
 jbrowse {
    git {
        url= "https://github.com/NAL-i5K/jbrowse"
-       branch = "master"
+        branch = "master"
+//url= "https://github.com/NAL-i5K/jbrowse#master"
+//       branch = "master#db01bc0c65f339d7fdacfd13973937b41b109c84"
        //tag = "974a414338807964d66bf8ed9503207e30cea293"
        alwaysPull = true
        alwaysRecheck = true
@@ -151,12 +203,12 @@ jbrowse {
            alwaysPull = true
      	   alwaysRecheck = true
        }
-       GCContent {
-           git = 'https://github.com/elsiklab/gccontent'
-   	       branch = 'master'
-           alwaysPull = true
-    	   alwaysRecheck = true
-       }
+//        Header {
+//           git = 'https://github.com/NAL-i5K/workspace_header_footer'
+//               branch = 'master'
+//           alwaysPull = true
+//           alwaysRecheck = true
+//       }
    }
 }
 
