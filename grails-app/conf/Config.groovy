da540c314f2016ce5890d7af7666bc9b1b171e22
Nathan Dunn <ndunn@me.com>
2018-06-13 14:32:50 -0700

    fixes pulled jbrowse version

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -408,8 +408,8 @@ jbrowse {
     git {
         url = "https://github.com/gmod/jbrowse"
 //        branch = "master"
-//        tag = "1.12.5-release"
-        tag = "aa6127349b6cf7b59619154e72d025cc7567a0ef"
+        tag = "maint/1.12.5.0-apollo"
+//        tag = "27ec453"
         alwaysPull = false
         alwaysRecheck = false
     }
