57c667adb1e28f60151bd1a3b10ebcbf62a64048
childers <genetics.guy@gmail.com>
2018-07-12 10:40:41 -0400

    removing replce tag feature

--- client/apollo/js/View/Track/AnnotTrack.js
+++ client/apollo/js/View/Track/AnnotTrack.js
@@ -4497,7 +4497,7 @@ define([
                 var attributesDiv = dojo.create("div", {'class': "annotation_info_editor_section"}, content);
                 var attributesLabel = dojo.create("div", {
                     'class': "annotation_info_editor_section_header",
-                    innerHTML: "Replaced Model"
+                    innerHTML: "Attributes"
                 }, attributesDiv);
                 var attributesTable = dojo.create("div", {
                     'class': "attributes",
@@ -4513,13 +4513,6 @@ define([
                     innerHTML: "Delete",
                     'class': "annotation_info_editor_button"
                 }, attributeButtons);
-		var attributess = "Use the Replaced Models field to specify the gene models your annotation(s) will supercede in any official gene set generated from these annotations.";
-                new Tooltip({
-			connectId: attributesDiv,
-			    label: attributess,
-			    position: ["above"],
-			    showDelay: 600
-			    });
 
                 var pubmedIdsDiv = dojo.create("div", {'class': "annotation_info_editor_section"}, content);
                 var pubmedIdsLabel = dojo.create("div", {
@@ -4920,7 +4913,7 @@ define([
                         });
                         for (var i = 0; i < feature.non_reserved_properties.length; ++i) {
                             var attribute = feature.non_reserved_properties[i];
-                            attributes.newItem({tag: "replace", value: attribute.value});
+                            attributes.newItem({tag: attribute.tag, value: attribute.value});
                         }
 
 
@@ -4935,9 +4928,9 @@ define([
                                     options: cannedKeys,
                                     formatter: function (tag) {
                                         if (!tag) {
-                                            return "replace";
+                                            return "Enter new tag";
                                         }
-                                        return "replace";
+                                        return tag;
                                     },
                                     editable: hasWritePermission
                                 },
@@ -4985,7 +4978,7 @@ define([
                         });
 
                         dojo.connect(attributeTable, "onCancelEdit", function (inRowIndex) {
-                            attributeTable.store.setValue(attributeTable.getItem(inRowIndex), "tag", "replace");
+                            attributeTable.store.setValue(attributeTable.getItem(inRowIndex), "tag", oldTag);
                             attributeTable.store.setValue(attributeTable.getItem(inRowIndex), "value", oldValue);
                             dirty = false;
                         });
@@ -4996,18 +4989,18 @@ define([
                             if (!newTag || !newValue) {
                             }
                             else if (!oldTag || !oldValue) {
-                                addAttribute("replace", newValue);
+                                addAttribute(newTag, newValue);
                             }
                             else {
                                 if (newTag != oldTag || newValue != oldValue) {
-                                    updateAttribute(oldTag, oldValue, "replace", newValue);
+                                    updateAttribute(oldTag, oldValue, newTag, newValue);
                                 }
                             }
                             dirty = false;
                         });
 
                         dojo.connect(addAttributeButton, "onclick", function () {
-                            attributeTable.store.newItem({tag: "replace", value: ""});
+                            attributeTable.store.newItem({tag: "", value: ""});
                             attributeTable.scrollToRow(attributeTable.rowCount);
                         });
 
