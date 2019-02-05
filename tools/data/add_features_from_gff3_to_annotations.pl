35bf5deb0a6e621f205e6a679d713326676b033e
Nathan Dunn <ndunn@me.com>
2019-02-04 19:43:11 -0800

    reverted a change so it will build

--- tools/data/add_features_from_gff3_to_annotations.pl
+++ tools/data/add_features_from_gff3_to_annotations.pl
@@ -4,7 +4,7 @@ use strict;
 use warnings;
 
 use FindBin qw($RealBin);
-use lib "$RealBin/../../jbrowse/src/perl5/lib/perl5";
+use lib "$RealBin/../../jbrowse/src/perl5";
 
 use File::Basename;
 use lib dirname($0);
