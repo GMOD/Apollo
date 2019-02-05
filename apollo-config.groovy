8e903c4be02f3ae7a41b13592738c4245b18004e
Childers <genetics.guy@gmail.com>
2019-02-05 11:59:07 -0500

    savign updated config file

--- apollo-config.groovy
+++ apollo-config.groovy
@@ -100,7 +100,7 @@ jbrowse {
 //       url= "https://github.com/NAL-i5K/jbrowse"
        url= "https://github.com/GMOD/jbrowse"
 //        branch = "master"
-	  tag = "1.15.4-release"
+//	  tag = "1.15.4-release"
 //	  tag = "e9a005cf86a40ad4b2a4aaebcbf914a866ff7f3b"
        alwaysPull = true
        alwaysRecheck = true
