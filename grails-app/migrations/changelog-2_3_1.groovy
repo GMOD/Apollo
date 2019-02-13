810db7b4e227a57bbfc81212b7d671495a5048c4
Nathan Dunn <ndunn@me.com>
2019-02-13 15:41:11 -0800

    Fix 2046 (#2047)
    * initial add of adding the constraint
    
    * set boolean value

---
+++ grails-app/migrations/changelog-2_3_1.groovy
@@ -0,0 +1,14 @@
+databaseChangeLog = {
+
+    changeSet(author: "nathandunn", id: "1459788030178-1") {
+
+        preConditions(onFail: 'MARK_RAN', onError: "HALT") {
+            not {
+                columnExists(columnName: "obsolete", tableName: "organism")
+            }
+        }
+        addColumn(tableName: "organism"){
+            column(name:"obsolete",type:"BOOLEAN",defaultValueBoolean:false)
+        }
+    }
+}
