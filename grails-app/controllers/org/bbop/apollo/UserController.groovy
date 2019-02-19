67aa504ea48b2c10bb272843dc53dccb068a3b8b
Nathan Dunn <ndunn@me.com>
2019-02-18 17:36:44 -0800

    updated message

--- grails-app/controllers/org/bbop/apollo/UserController.groovy
+++ grails-app/controllers/org/bbop/apollo/UserController.groovy
@@ -464,7 +464,7 @@ class UserController {
         } catch (e) {
             log.error(e.fillInStackTrace())
             JSONObject jsonObject = new JSONObject()
-            jsonObject.put(FeatureStringEnum.ERROR.value, "Failed to inactivate the user " + e.message)
+            jsonObject.put(FeatureStringEnum.ERROR.value, "Failed to inactivate the user " + e.message+". Remove users and groups first.")
             render jsonObject as JSON
         }
     }
