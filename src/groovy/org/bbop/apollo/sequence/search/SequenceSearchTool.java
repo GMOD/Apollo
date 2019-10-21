f8f46c44461f32b64b24087f729ff793b5e3b6e9
Nathan Dunn <nathandunn@lbl.gov>
2019-10-21 09:40:06 -0700

    Fix 2270 (#2273)
    * fixed search hit strand
    
    * added strand to interface

--- src/groovy/org/bbop/apollo/sequence/search/SequenceSearchTool.java
+++ src/groovy/org/bbop/apollo/sequence/search/SequenceSearchTool.java
@@ -5,18 +5,16 @@ import java.util.Collection;
 import org.bbop.apollo.sequence.search.blast.BlastAlignment;
 import org.codehaus.groovy.grails.web.json.JSONObject;
 
-import org.bbop.apollo.Match;
-
 public abstract class SequenceSearchTool {
 
     public abstract void parseConfiguration(JSONObject config) throws SequenceSearchToolException;
 
     public abstract Collection<BlastAlignment> search(String uniqueToken, String query, String databaseId) throws SequenceSearchToolException;
-    
+
     public Collection<BlastAlignment> search(String uniqueToken, String query) throws SequenceSearchToolException {
         return search(uniqueToken, query, null);
     }
 
 
-    
+
 }
