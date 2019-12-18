6240f159cea72aef4df0b2f8b0f5f0dab10592ae
Nathan Dunn <ndunn@me.com>
2019-12-18 13:36:42 -0800

    setting there as well

--- src/gwt/org/bbop/apollo/gwt/client/GeneDetailPanel.java
+++ src/gwt/org/bbop/apollo/gwt/client/GeneDetailPanel.java
@@ -21,7 +21,6 @@ import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.SuggestOracle;
 import com.google.gwt.user.client.ui.Widget;
 import org.bbop.apollo.gwt.client.dto.AnnotationInfo;
-import org.bbop.apollo.gwt.client.event.AnnotationInfoChangeEvent;
 import org.bbop.apollo.gwt.client.rest.AnnotationRestService;
 import org.bbop.apollo.gwt.client.rest.AvailableStatusRestService;
 import org.bbop.apollo.gwt.client.rest.RestService;
@@ -102,6 +101,39 @@ public class GeneDetailPanel extends Composite {
         String updatedName = symbolField.getText();
         internalAnnotationInfo.setSymbol(updatedName);
         updateGene();
+    }
+
+    @UiHandler("synonymsField")
+    void handleSynonymsChange(ChangeEvent e) {
+        final AnnotationInfo updateAnnotationInfo = this.internalAnnotationInfo;
+        final String updatedName = synonymsField.getText().trim();
+        String[] synonyms = updatedName.split("\\|");
+        String infoString = "";
+        for(String s : synonyms){
+            infoString += "'"+ s.trim() + "' ";
+        }
+        infoString = infoString.trim();
+        Bootbox.confirm(synonyms.length + " synonyms: " + infoString, new ConfirmCallback() {
+            @Override
+            public void callback(boolean result) {
+                if(result){
+                    updateAnnotationInfo.setSynonyms(updatedName);
+                    synonymsField.setText(updatedName);
+                    updateGene();
+                }
+                else{
+                    synonymsField.setText(updateAnnotationInfo.getSynonyms());
+                }
+            }
+        });
+
+
+
+    }
+
+    private boolean validateSynonyms(String updatedName) {
+
+        return false;
     }
 
     @UiHandler("descriptionField")
