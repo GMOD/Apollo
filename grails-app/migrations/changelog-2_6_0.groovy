c648a61d430d8c53d9a942142781c1eadb7e5a0e
Nathan Dunn <ndunn@me.com>
2020-03-30 16:03:19 -0700

    added a quick stash

--- grails-app/migrations/changelog-2_6_0.groovy
+++ grails-app/migrations/changelog-2_6_0.groovy
@@ -14,4 +14,26 @@ databaseChangeLog = {
         dropNotNullConstraint(tableName: "feature_synonym",columnName:"is_internal", columnDataType:"bigint")
         dropNotNullConstraint(tableName: "feature_synonym",columnName:"is_current", columnDataType:"bigint")
     }
+
+    changeSet(author: "nathandunn", id: "1459788030180-3") {
+        preConditions(onFail: 'MARK_RAN', onError: "HALT") {
+            not {
+                indexExists(indexName:"feature_event_name")
+            }
+        }
+        createIndex(indexName: "feature_event_name", tableName: "feature_event", unique: "false") {
+            column(name: "name")
+        }
+    }
+
+    changeSet(author: "nathandunn", id: "1459788030180-4") {
+        preConditions(onFail: 'MARK_RAN', onError: "HALT") {
+            not {
+                indexExists(indexName:"feature_name")
+            }
+        }
+        createIndex(indexName: "feature_name", tableName: "feature", unique: "false") {
+            column(name: "name")
+        }
+    }
 }
