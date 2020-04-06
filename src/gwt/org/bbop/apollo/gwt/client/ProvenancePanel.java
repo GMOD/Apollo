361555c6a7300126b4152f28e9f875d6cdac2983
Nathan Dunn <nathandunn@lbl.gov>
2020-04-06 09:15:48 -0700

    refactor for default ECO codes (#2429)
    * did an initial refactor
    
    * refactored for a more centralized approach
    
    * closer to fixing ECO box
    
    * got the ECO suggest box to work properly
    
    * added info link
    
    * propataged to gene product and provenance
    
    * updated

--- src/gwt/org/bbop/apollo/gwt/client/ProvenancePanel.java
+++ src/gwt/org/bbop/apollo/gwt/client/ProvenancePanel.java
@@ -22,7 +22,9 @@ import com.google.gwt.view.client.SelectionChangeEvent;
 import com.google.gwt.view.client.SingleSelectionModel;
 import org.bbop.apollo.gwt.client.dto.AnnotationInfo;
 import org.bbop.apollo.gwt.client.dto.ProvenanceConverter;
+import org.bbop.apollo.gwt.client.oracles.BiolinkLookup;
 import org.bbop.apollo.gwt.client.oracles.BiolinkOntologyOracle;
+import org.bbop.apollo.gwt.client.oracles.BiolinkSuggestBox;
 import org.bbop.apollo.gwt.client.resources.TableResources;
 import org.bbop.apollo.gwt.client.rest.ProvenanceRestService;
 import org.bbop.apollo.gwt.shared.provenance.Provenance;
@@ -39,12 +41,12 @@ import org.gwtbootstrap3.extras.bootbox.client.callback.ConfirmCallback;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.bbop.apollo.gwt.client.oracles.BiolinkOntologyOracle.ECO_BASE;
+
 /**
  * Created by ndunn on 1/9/15.
  */
 public class ProvenancePanel extends Composite {
-
-  private final String ECO_BASE = "http://www.evidenceontology.org/term/";
 
   interface ProvenancePanelUiBinder extends UiBinder<Widget, ProvenancePanel> {
   }
@@ -59,7 +61,7 @@ public class ProvenancePanel extends Composite {
   @UiField
   ListBox provenanceField;
   @UiField(provided = true)
-  SuggestBox evidenceCodeField;
+  BiolinkSuggestBox evidenceCodeField;
   @UiField
   TextBox withFieldPrefix;
   @UiField
@@ -96,10 +98,15 @@ public class ProvenancePanel extends Composite {
 //    Button referenceValidateButton;
   @UiField
   HTML provenanceTitle;
+  @UiField
+  org.gwtbootstrap3.client.ui.CheckBox allEcoCheckBox;
+  @UiField
+  Anchor helpLink;
 
   private static ListDataProvider<Provenance> dataProvider = new ListDataProvider<>();
   private static List<Provenance> annotationInfoList = dataProvider.getList();
   private SingleSelectionModel<Provenance> selectionModel = new SingleSelectionModel<>();
+  private BiolinkOntologyOracle ecoLookup = new BiolinkOntologyOracle(BiolinkLookup.ECO);
 
   private AnnotationInfo annotationInfo;
 
@@ -113,6 +120,7 @@ public class ProvenancePanel extends Composite {
 
     initWidget(ourUiBinder.createAndBindUi(this));
     initFields();
+    allEcoCheckBox(null);
 
 
     selectionModel.addSelectionChangeHandler(new SelectionChangeEvent.Handler() {
@@ -183,23 +191,7 @@ public class ProvenancePanel extends Composite {
   }
 
   private void initLookups() {
-    // most from here: http://geneontology.org/docs/guide-go-evidence-codes/
-    BiolinkOntologyOracle ecoLookup = new BiolinkOntologyOracle("ECO");
-    ecoLookup.addPreferredSuggestion("experimental evidence used in manual assertion (EXP)", "http://www.evidenceontology.org/term/ECO:0000269/", "ECO:0000269");
-    ecoLookup.addPreferredSuggestion("direct assay evidence used in manual assertion (IDA)", "http://www.evidenceontology.org/term/ECO:0000314/", "ECO:0000314");
-    ecoLookup.addPreferredSuggestion("physical interaction evidence used in manual assertion (IPI)", "http://www.evidenceontology.org/term/ECO:0000353/", "ECO:0000353");
-    ecoLookup.addPreferredSuggestion("mutant phenotype evidence used in manual assertion (IMP)", "http://www.evidenceontology.org/term/ECO:0000315/", "ECO:0000315");
-    ecoLookup.addPreferredSuggestion("genetic interaction evidence used in manual assertion (IGI)", "http://www.evidenceontology.org/term/ECO:0000316/", "ECO:0000316");
-    ecoLookup.addPreferredSuggestion("biological aspect of ancestor evidence used in manual assertion (IBA)", "http://www.evidenceontology.org/term/ECO:0000318/", "ECO:0000318");
-    ecoLookup.addPreferredSuggestion("biological aspect of descendant evidence used in manual assertion (IBD)", "http://www.evidenceontology.org/term/ECO:0000319/", "ECO:0000319");
-    ecoLookup.addPreferredSuggestion("sequence similarity evidence used in manual assertion (ISS)", "http://www.evidenceontology.org/term/ECO:0000250/", "ECO:0000250");
-    ecoLookup.addPreferredSuggestion("sequence orthology evidence used in manual assertion (ISO)", "http://www.evidenceontology.org/term/ECO:0000266/", "ECO:0000266");
-    ecoLookup.addPreferredSuggestion("sequence alignment evidence used in manual assertion (ISA)", "http://www.evidenceontology.org/term/ECO:0000247/", "ECO:0000247");
-    ecoLookup.addPreferredSuggestion("no biological data found used in manual assertion (ND)", "http://www.evidenceontology.org/term/ECO:0000307/", "ECO:0000307");
-    ecoLookup.addPreferredSuggestion("curator inference used in manual assertion (IC)", "http://www.evidenceontology.org/term/ECO:0000305/", "ECO:0000305");
-    ecoLookup.addPreferredSuggestion("evidence used in automatic assertion (IEA)", "http://www.evidenceontology.org/term/ECO:0000501/", "ECO:0000501");
-    ecoLookup.addPreferredSuggestion("high throughput direct assay evidence used in manual assertion (HDA)", "http://www.evidenceontology.org/term/ECO:0007005/", "ECO:0007005");
-    evidenceCodeField = new SuggestBox(ecoLookup);
+    evidenceCodeField = new BiolinkSuggestBox(ecoLookup);
   }
 
   private void loadData() {
@@ -371,6 +363,17 @@ public class ProvenancePanel extends Composite {
     for (int i = 0; i < annotationsArray.size(); i++) {
       Provenance provenanceInstance = ProvenanceConverter.convertFromJson(annotationsArray.get(i).isObject());
       annotationInfoList.add(provenanceInstance);
+    }
+  }
+
+  @UiHandler("allEcoCheckBox")
+  public void allEcoCheckBox(ClickEvent e) {
+    ecoLookup.setUseAllEco(allEcoCheckBox.getValue());
+    if(allEcoCheckBox.getValue()){
+      helpLink.setHref("https://www.ebi.ac.uk/ols/ontologies/eco");
+    }
+    else {
+      helpLink.setHref("http://geneontology.org/docs/guide-go-evidence-codes/");
     }
   }
 
