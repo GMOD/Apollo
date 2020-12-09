c98327506f5dd92fe8252353a001d0327edb3907
Nathan Dunn <ndunn@me.com>
2020-12-09 13:35:31 -0800

    added get recent

--- docs/web_services/examples/shell/getRecent.sh
+++ docs/web_services/examples/shell/getRecent.sh
@@ -4,10 +4,13 @@ url=$1
 username=$2
 password=$3
 days=$4
+status=$5
 
 usage() {
     echo "Sample script for exporting features from organism as GFF3 via web services"
-    echo "Usage:    ./getRecent.sh <complete_apollo_URL> <username> <password> <days>"
+    echo "Usage:    ./getRecent.sh <complete_apollo_URL> <username> <password> <days> <status>"
+    echo "Example:  ./getRecent.sh http://localhost:8080/apollo demo demo Finished|InProgress"
+    echo "Example:  ./getRecent.sh http://localhost:8080/apollo demo demo None"
     echo "Example:  ./getRecent.sh http://localhost:8080/apollo demo demo "
 }
 
@@ -16,6 +19,6 @@ if [[ ! -n "$url" || ! -n "$username" || ! -n "$password" || ! -n "$days" ]]; th
     exit
 fi
 
-echo curl "${url}/annotationEditor/getRecentAnnotations" -H 'Content-Type: application/json' --data "{'days':${days},  'username': '${username}', 'password': '${password}'}"
-curl "${url}/annotationEditor/getRecentAnnotations" -H 'Content-Type: application/json' --data "{'days':${days}, 'username': '${username}', 'password': '${password}'}"
+echo curl "${url}/annotationEditor/getRecentAnnotations" -H 'Content-Type: application/json' --data "{'days':${days},  'username': '${username}', 'password': '${password}', 'status':'${status}'}"
+curl "${url}/annotationEditor/getRecentAnnotations" -H 'Content-Type: application/json' --data "{'days':${days}, 'username': '${username}', 'password': '${password}', 'status':'${status}'}"
 
