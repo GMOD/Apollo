279e9a6d5a04fe7d0c78e4cd815872ad1550c5d9
Nathan Dunn <ndunn@me.com>
2021-01-05 09:23:30 -0800

    triggering github actions

--- client/apollo/js/JSONUtils.js
+++ client/apollo/js/JSONUtils.js
@@ -377,6 +377,8 @@ function copyOfficialData(fromFeature,toFeature){
 */
 JSONUtils.createApolloFeature = function( jfeature, specified_type, useName, specified_subtype ,is_official)   {
     var diagnose =  (JSONUtils.verbose_conversion && jfeature.children() && jfeature.children().length > 0);
+
+    
     if (diagnose)  {
         console.log("converting JBrowse feature to Apollo feture, specified type: " + specified_type + " " + specified_subtype);
         console.log(jfeature,JSON.stringify(jfeature));
