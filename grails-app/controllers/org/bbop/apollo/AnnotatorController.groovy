99188e583e13a463ec6c15cb90d19850947e173c
Nathan Dunn <nathandunn@lbl.gov>
2021-01-27 16:20:18 -0800

    fixes #2556 (#2557)
    * fixes #2556
    
    * slight cleanup

--- grails-app/controllers/org/bbop/apollo/AnnotatorController.groovy
+++ grails-app/controllers/org/bbop/apollo/AnnotatorController.groovy
@@ -724,12 +724,21 @@ class AnnotatorController {
 /**
  * This is a public passthrough to version
  */
-    def version() {
-        println "version "
-    }
+  @RestApiMethod(description = "Get system info", path = "/system", verb = RestApiVerb.GET)
+  @RestApiParams(params = [])
+  def system() {
+    def jsonObject = new JSONObject()
+    jsonObject.apollo_version = grails.util.Metadata.current['app.version']
+    jsonObject.grails_version = GroovySystem.getVersion()
+    jsonObject.jvm_version = System.getProperty("java.version")
+    jsonObject.serverInfo = servletContext.getServerInfo()
+    jsonObject.jbrowse_branch = grailsApplication.config?.jbrowse?.git
+    jsonObject.jbrowse_url = grailsApplication.config?.jbrowse?.git?.url
+    render jsonObject as JSON
+  }
 
+  // used in the view
     def about() {
-        println "about . . . . "
     }
 /**
  * This is a very specific method for the GWT interface.
